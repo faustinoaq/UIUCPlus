e9ac14a3b0ef1f370f3b4c10ed8bc6891f9c1694
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:08 -0500

    Formatted main branch before mubert-base.jackson-databind.mid-1550.idx-1551.480.mutant injection

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/BinaryNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/BinaryNode.java
@@ -1,115 +1,100 @@
 package com.fasterxml.jackson.databind.node;
 
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.util.Arrays;
 
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.databind.SerializerProvider;
-
-
 /**
- * Value node that contains Base64 encoded binary value, which will be
- * output and stored as Json String value.
+ * Value node that contains Base64 encoded binary value, which will be output and stored as Json
+ * String value.
  */
-public class BinaryNode
-    extends ValueNode
-{
-    private static final long serialVersionUID = 2L;
+public class BinaryNode extends ValueNode {
+  private static final long serialVersionUID = 2L;
 
-    final static BinaryNode EMPTY_BINARY_NODE = new BinaryNode(new byte[0]);
+  static final BinaryNode EMPTY_BINARY_NODE = new BinaryNode(new byte[0]);
 
-    protected final byte[] _data;
+  protected final byte[] _data;
 
-    public BinaryNode(byte[] data)
-    {
-        _data = data;
+  public BinaryNode(byte[] data) {
+    _data = data;
+  }
+
+  public BinaryNode(byte[] data, int offset, int length) {
+    if (offset == 0 && length == data.length) {
+      _data = data;
+    } else {
+      _data = new byte[length];
+      System.arraycopy(data, offset, _data, 0, length);
     }
+  }
 
-    public BinaryNode(byte[] data, int offset, int length)
-    {
-        if (offset == 0 && length == data.length) {
-            _data = data;
-        } else {
-            _data = new byte[length];
-            System.arraycopy(data, offset, _data, 0, length);
-        }
+  public static BinaryNode valueOf(byte[] data) {
+    if (data == null) {
+      return null;
     }
-
-    public static BinaryNode valueOf(byte[] data)
-    {
-        if (data == null) {
-            return null;
-        }
-        if (data.length == 0) {
-            return EMPTY_BINARY_NODE;
-        }
-        return new BinaryNode(data);
+    if (data.length == 0) {
+      return EMPTY_BINARY_NODE;
     }
+    return new BinaryNode(data);
+  }
 
-    public static BinaryNode valueOf(byte[] data, int offset, int length)
-    {
-        if (data == null) {
-            return null;
-        }
-        if (length == 0) {
-            return EMPTY_BINARY_NODE;
-        }
-        return new BinaryNode(data, offset, length);
+  public static BinaryNode valueOf(byte[] data, int offset, int length) {
+    if (data == null) {
+      return null;
     }
-
-    @Override
-    public JsonNodeType getNodeType()
-    {
-        return JsonNodeType.BINARY;
+    if (length == 0) {
+      return EMPTY_BINARY_NODE;
     }
+    return new BinaryNode(data, offset, length);
+  }
 
-    @Override
-    public JsonToken asToken() {
-        /* No distinct type; could use one for textual values,
-         * but given that it's not in text form at this point,
-         * embedded-object is closest
-         */
-        return JsonToken.VALUE_EMBEDDED_OBJECT;
-    }
+  @Override
+  public JsonNodeType getNodeType() {
+    return JsonNodeType.BINARY;
+  }
 
-    /**
-     *<p>
-     * Note: caller is not to modify returned array in any way, since
-     * it is not a copy but reference to the underlying byte array.
+  @Override
+  public JsonToken asToken() {
+    /* No distinct type; could use one for textual values,
+     * but given that it's not in text form at this point,
+     * embedded-object is closest
      */
-    @Override
-    public byte[] binaryValue() { return _data; }
+    return JsonToken.VALUE_EMBEDDED_OBJECT;
+  }
 
-    /**
-     * Hmmh. This is not quite as efficient as using {@link #serialize},
-     * but will work correctly.
-     */
-    @Override
-    public String asText() {
-        return Base64Variants.getDefaultVariant().encode(_data, false);
-    }
+  /**
+   * Note: caller is not to modify returned array in any way, since it is not a copy but reference
+   * to the underlying byte array.
+   */
+  @Override
+  public byte[] binaryValue() {
+    return _data;
+  }
 
-    @Override
-    public final void serialize(JsonGenerator jg, SerializerProvider provider)
-        throws IOException
-    {
-        jg.writeBinary(provider.getConfig().getBase64Variant(),
-                _data, 0, _data.length);
-    }
+  /** Hmmh. This is not quite as efficient as using {@link #serialize}, but will work correctly. */
+  @Override
+  public String asText() {
+    return Base64Variants.getDefaultVariant().encode(_data, false);
+  }
 
-    @Override
-    public boolean equals(Object o)
-    {
-        if (o == this) return true;
-        if (o == null) return false;
-        if (!(o instanceof BinaryNode)) {
-            return false;
-        }
-        return Arrays.equals(((BinaryNode) o)._data, _data);
-    }
+  @Override
+  public final void serialize(JsonGenerator jg, SerializerProvider provider) throws IOException {
+    jg.writeBinary(provider.getConfig().getBase64Variant(), _data, 0, _data.length);
+  }
 
-    @Override
-    public int hashCode() {
-        return (_data == null) ? -1 : _data.length;
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (o == null) return false;
+    if (!(o instanceof BinaryNode)) {
+      return false;
     }
+    return Arrays.equals(((BinaryNode) o)._data, _data);
+  }
+
+  @Override
+  public int hashCode() {
+    return (_data == null) ? -1 : _data.length;
+  }
 }
