077d3a6685be9458090f00b9e491785aec282d48
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:32:52 -0500

    codet5-base.commons-lang.mid-468.idx-341.2.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/SerializationException.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/SerializationException.java
@@ -19,58 +19,54 @@ package org.apache.commons.lang3;
 /**
  * Exception thrown when the Serialization process fails.
  *
- * <p>The original error is wrapped within this one.</p>
+ * <p>The original error is wrapped within this one.
  *
- * <p>#NotThreadSafe# because Throwable is not thread-safe</p>
+ * <p>#NotThreadSafe# because Throwable is not thread-safe
+ *
  * @since 1.0
  */
 public class SerializationException extends RuntimeException {
 
-    /**
-     * Required for serialization support.
-     *
-     * @see java.io.Serializable
-     */
-    private static final long serialVersionUID = 4029025366392702726L;
+  /**
+   * Required for serialization support.
+   *
+   * @see java.io.Serializable
+   */
+  private static final long serialVersionUID = 4029025366392702726L;
 
-    /**
-     * Constructs a new {@link SerializationException} without specified
-     * detail message.
-     */
-    public SerializationException() {
+  /** Constructs a new {@link SerializationException} without specified detail message. */
+  public SerializationException() {}
+
+  /**
+   * Constructs a new {@link SerializationException} with specified detail message.
+   *
+   * @param msg The error message.
+   */
+  public SerializationException(final String msg) {
+    if (msg == null) {
+      System.out.println("Error: msg is null");
+    } else {
+      super(msg);
     }
+  }
 
-    /**
-     * Constructs a new {@link SerializationException} with specified
-     * detail message.
-     *
-     * @param msg  The error message.
-     */
-    public SerializationException(final String msg) {
-        super(msg);
-    }
+  /**
+   * Constructs a new {@link SerializationException} with specified nested {@link Throwable}.
+   *
+   * @param cause The {@link Exception} or {@link Error} that caused this exception to be thrown.
+   */
+  public SerializationException(final Throwable cause) {
+    super(cause);
+  }
 
-    /**
-     * Constructs a new {@link SerializationException} with specified
-     * nested {@link Throwable}.
-     *
-     * @param cause  The {@link Exception} or {@link Error}
-     *  that caused this exception to be thrown.
-     */
-    public SerializationException(final Throwable cause) {
-        super(cause);
-    }
-
-    /**
-     * Constructs a new {@link SerializationException} with specified
-     * detail message and nested {@link Throwable}.
-     *
-     * @param msg    The error message.
-     * @param cause  The {@link Exception} or {@link Error}
-     *  that caused this exception to be thrown.
-     */
-    public SerializationException(final String msg, final Throwable cause) {
-        super(msg, cause);
-    }
-
+  /**
+   * Constructs a new {@link SerializationException} with specified detail message and nested {@link
+   * Throwable}.
+   *
+   * @param msg The error message.
+   * @param cause The {@link Exception} or {@link Error} that caused this exception to be thrown.
+   */
+  public SerializationException(final String msg, final Throwable cause) {
+    super(msg, cause);
+  }
 }
