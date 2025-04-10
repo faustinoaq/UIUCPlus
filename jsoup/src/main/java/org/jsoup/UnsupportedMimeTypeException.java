245e4d1966d4a2738d186e54ddfb127434b505be
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:24:09 -0500

    Formatted main branch before codebert-base.jsoup.mid-9.idx-11.3.mutant injection

--- jsoup/src/main/java/org/jsoup/UnsupportedMimeTypeException.java
+++ jsoup/src/main/java/org/jsoup/UnsupportedMimeTypeException.java
@@ -2,29 +2,27 @@ package org.jsoup;
 
 import java.io.IOException;
 
-/**
- * Signals that a HTTP response returned a mime type that is not supported.
- */
+/** Signals that a HTTP response returned a mime type that is not supported. */
 public class UnsupportedMimeTypeException extends IOException {
-    private final String mimeType;
-    private final String url;
+  private final String mimeType;
+  private final String url;
 
-    public UnsupportedMimeTypeException(String message, String mimeType, String url) {
-        super(message);
-        this.mimeType = mimeType;
-        this.url = url;
-    }
+  public UnsupportedMimeTypeException(String message, String mimeType, String url) {
+    super(message);
+    this.mimeType = mimeType;
+    this.url = url;
+  }
 
-    public String getMimeType() {
-        return mimeType;
-    }
+  public String getMimeType() {
+    return mimeType;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public String getUrl() {
+    return url;
+  }
 
-    @Override
-    public String toString() {
-        return super.toString() + ". Mimetype=" + mimeType + ", URL="+url;
-    }
+  @Override
+  public String toString() {
+    return super.toString() + ". Mimetype=" + mimeType + ", URL=" + url;
+  }
 }
