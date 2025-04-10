296195369a9a41a38027e51ea033d551e358c592
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:37:11 -0500

    NatGen-base.commons-compress.mid-3671.idx-2707.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/InnerClassesAttribute.java
+++ commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/InnerClassesAttribute.java
@@ -21,164 +21,165 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Inner classes class file attribute
- */
+/** Inner classes class file attribute */
 public class InnerClassesAttribute extends Attribute {
 
-    private static class InnerClassesEntry {
+  private static class InnerClassesEntry {
 
-        CPClass innerClassInfo;
-        CPClass outerClassInfo;
-        CPUTF8 innerClassName;
+    CPClass innerClassInfo;
+    CPClass outerClassInfo;
+    CPUTF8 innerClassName;
 
-        int innerClassInfoIndex = -1;
-        int outerClassInfoIndex = -1;
-        int innerNameIndex = -1;
-        int innerClassAccessFlags = -1;
+    int innerClassInfoIndex = -1;
+    int outerClassInfoIndex = -1;
+    int innerNameIndex = -1;
+    int innerClassAccessFlags = -1;
 
-        public InnerClassesEntry(final CPClass innerClass, final CPClass outerClass, final CPUTF8 innerName,
-            final int flags) {
-            this.innerClassInfo = innerClass;
-            this.outerClassInfo = outerClass;
-            this.innerClassName = innerName;
-            this.innerClassAccessFlags = flags;
-        }
-
-        /**
-         * Determine the indices of the things in the receiver which point to elements of the ClassConstantPool
-         *
-         * @param pool ClassConstantPool which holds the CPClass and CPUTF8 objects.
-         */
-        public void resolve(final ClassConstantPool pool) {
-            if (innerClassInfo != null) {
-                innerClassInfo.resolve(pool);
-                innerClassInfoIndex = pool.indexOf(innerClassInfo);
-            } else {
-                innerClassInfoIndex = 0;
-            }
-
-            if (innerClassName != null) {
-                innerClassName.resolve(pool);
-                innerNameIndex = pool.indexOf(innerClassName);
-            } else {
-                innerNameIndex = 0;
-            }
-
-            if (outerClassInfo != null) {
-                outerClassInfo.resolve(pool);
-                outerClassInfoIndex = pool.indexOf(outerClassInfo);
-            } else {
-                outerClassInfoIndex = 0;
-            }
-        }
-
-        public void write(final DataOutputStream dos) throws IOException {
-            dos.writeShort(innerClassInfoIndex);
-            dos.writeShort(outerClassInfoIndex);
-            dos.writeShort(innerNameIndex);
-            dos.writeShort(innerClassAccessFlags);
-        }
-
-    }
-
-    private static CPUTF8 attributeName;
-
-    public static void setAttributeName(final CPUTF8 cpUTF8Value) {
-        attributeName = cpUTF8Value;
-    }
-
-    private final List<InnerClassesEntry> innerClasses = new ArrayList<>();
-    private final List<ConstantPoolEntry> nestedClassFileEntries = new ArrayList<>();
-
-    public InnerClassesAttribute(final String name) {
-        super(attributeName);
-        nestedClassFileEntries.add(getAttributeName());
-    }
-
-    public void addInnerClassesEntry(final CPClass innerClass, final CPClass outerClass, final CPUTF8 innerName,
+    public InnerClassesEntry(
+        final CPClass innerClass,
+        final CPClass outerClass,
+        final CPUTF8 innerName,
         final int flags) {
-        if (innerClass != null) {
-            nestedClassFileEntries.add(innerClass);
-        }
-        if (outerClass != null) {
-            nestedClassFileEntries.add(outerClass);
-        }
-        if (innerName != null) {
-            nestedClassFileEntries.add(innerName);
-        }
-        addInnerClassesEntry(new InnerClassesEntry(innerClass, outerClass, innerName, flags));
+      this.innerClassInfo = innerClass;
+      this.outerClassInfo = outerClass;
+      this.innerClassName = innerName;
+      this.innerClassAccessFlags = flags;
     }
 
-    private void addInnerClassesEntry(final InnerClassesEntry innerClassesEntry) {
-        innerClasses.add(innerClassesEntry);
+    /**
+     * Determine the indices of the things in the receiver which point to elements of the
+     * ClassConstantPool
+     *
+     * @param pool ClassConstantPool which holds the CPClass and CPUTF8 objects.
+     */
+    public void resolve(final ClassConstantPool pool) {
+      if (innerClassInfo != null) {
+        innerClassInfo.resolve(pool);
+        innerClassInfoIndex = pool.indexOf(innerClassInfo);
+      } else {
+        innerClassInfoIndex = 0;
+      }
+
+      if (innerClassName != null) {
+        innerClassName.resolve(pool);
+        innerNameIndex = pool.indexOf(innerClassName);
+      } else {
+        innerNameIndex = 0;
+      }
+
+      if (outerClassInfo != null) {
+        outerClassInfo.resolve(pool);
+        outerClassInfoIndex = pool.indexOf(outerClassInfo);
+      } else {
+        outerClassInfoIndex = 0;
+      }
     }
 
-    @Override
-    protected void doWrite(final DataOutputStream dos) throws IOException {
-        // Hack so I can see what's being written.
-        super.doWrite(dos);
+    public void write(final DataOutputStream dos) throws IOException {
+      dos.writeShort(innerClassInfoIndex);
+      dos.writeShort(outerClassInfoIndex);
+      dos.writeShort(innerNameIndex);
+      dos.writeShort(innerClassAccessFlags);
     }
+  }
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!super.equals(obj)) {
-            return false;
-        }
-        if (this.getClass() != obj.getClass()) {
-            return false;
-        }
-        final InnerClassesAttribute other = (InnerClassesAttribute) obj;
-        if (getAttributeName() == null) {
-            if (other.getAttributeName() != null) {
-                return false;
-            }
-        } else if (!getAttributeName().equals(other.getAttributeName())) {
-            return false;
-        }
-        return true;
+  private static CPUTF8 attributeName;
+
+  public static void setAttributeName(final CPUTF8 cpUTF8Value) {
+    attributeName = cpUTF8Value;
+  }
+
+  private final List<InnerClassesEntry> innerClasses = new ArrayList<>();
+  private final List<ConstantPoolEntry> nestedClassFileEntries = new ArrayList<>();
+
+  public InnerClassesAttribute(final String name) {
+    super(attributeName);
+    nestedClassFileEntries.add(getAttributeName());
+  }
+
+  public void addInnerClassesEntry(
+      final CPClass innerClass, final CPClass outerClass, final CPUTF8 innerName, final int flags) {
+    if (innerClass != null) {
+      nestedClassFileEntries.add(innerClass);
     }
-
-    @Override
-    protected int getLength() {
-        return 2 + ((2 + 2 + 2 + 2) * innerClasses.size());
+    if (outerClass != null) {
+      nestedClassFileEntries.add(outerClass);
     }
-
-    @Override
-    protected ClassFileEntry[] getNestedClassFileEntries() {
-        return nestedClassFileEntries.toArray(ClassFileEntry.NONE);
+    if (innerName != null) {
+      nestedClassFileEntries.add(innerName);
     }
+    addInnerClassesEntry(new InnerClassesEntry(innerClass, outerClass, innerName, flags));
+  }
 
-    @Override
-    public int hashCode() {
-        final int PRIME = 31;
-        int result = super.hashCode();
-        result = PRIME * result + ((getAttributeName() == null) ? 0 : getAttributeName().hashCode());
-        return result;
+  private void addInnerClassesEntry(final InnerClassesEntry innerClassesEntry) {
+    innerClasses.add(innerClassesEntry);
+  }
+
+  @Override
+  protected void doWrite(final DataOutputStream dos) throws IOException {
+    // Hack so I can see what's being written.
+    super.doWrite(dos);
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    @Override
-    protected void resolve(final ClassConstantPool pool) {
-        super.resolve(pool);
-        for (final InnerClassesEntry entry : innerClasses) {
-            entry.resolve(pool);
-        }
+    if (!super.equals(obj)) {
+      return false;
     }
-
-    @Override
-    public String toString() {
-        return "InnerClasses: " + getAttributeName();
+    if (this.getClass() != obj.getClass()) {
+      return false;
     }
-
-    @Override
-    protected void writeBody(final DataOutputStream dos) throws IOException {
-        dos.writeShort(innerClasses.size());
-
-        for (final InnerClassesEntry entry : innerClasses) {
-            entry.write(dos);
-        }
+    final InnerClassesAttribute other = (InnerClassesAttribute) obj;
+    if (getAttributeName() == null) {
+      if (other.getAttributeName() != null) {
+        return false;
+      }
+    } else if (!getAttributeName().equals(other.getAttributeName())) {
+      return false;
     }
+    return true;
+  }
+
+  @Override
+  protected int getLength() {
+    return 2 + ((2 + 2 + 2 + 2) * innerClasses.size());
+  }
+
+  @Override
+  protected ClassFileEntry[] getNestedClassFileEntries() {
+    return nestedClassFileEntries.toArray(ClassFileEntry.NONE);
+  }
+
+  @Override
+  public int hashCode() {
+    final int PRIME = 31;
+    int result = super.hashCode();
+    result = PRIME * result + ((getAttributeName() == null) ? 0 : getAttributeName().hashCode());
+    return result;
+  }
+
+  @Override
+  protected void resolve(final ClassConstantPool pool) {
+    super.resolve(pool);
+    for (final InnerClassesEntry entry : innerClasses) {
+      entry.resolve(pool);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "InnerClasses: ";
+  }
+
+  @Override
+  protected void writeBody(final DataOutputStream dos) throws IOException {
+    dos.writeShort(innerClasses.size());
+
+    for (final InnerClassesEntry entry : innerClasses) {
+      entry.write(dos);
+    }
+  }
 }
