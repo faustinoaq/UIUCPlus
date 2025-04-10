e9e3a4f7d43f9e5835d795e07c5f8bb6e46ee0dd
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:40 -0500

    Formatted main branch before mubert-base.jfreechart.mid-8.idx-9.607.mutant injection

--- jfreechart/src/main/java/org/jfree/data/function/NormalDistributionFunction2D.java
+++ jfreechart/src/main/java/org/jfree/data/function/NormalDistributionFunction2D.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * ---------------------------------
@@ -37,108 +37,101 @@
 package org.jfree.data.function;
 
 import java.io.Serializable;
-
 import org.jfree.chart.internal.HashUtils;
 
-/**
- * A normal distribution function.  See
- * http://en.wikipedia.org/wiki/Normal_distribution.
- */
+/** A normal distribution function. See http://en.wikipedia.org/wiki/Normal_distribution. */
 public class NormalDistributionFunction2D implements Function2D, Serializable {
 
-    /** The mean. */
-    private double mean;
+  /** The mean. */
+  private double mean;
 
-    /** The standard deviation. */
-    private double std;
+  /** The standard deviation. */
+  private double std;
 
-    /** Precomputed factor for the function value. */
-    private double factor;
+  /** Precomputed factor for the function value. */
+  private double factor;
 
-    /** Precomputed denominator for the function value. */
-    private double denominator;
+  /** Precomputed denominator for the function value. */
+  private double denominator;
 
-    /**
-     * Constructs a new normal distribution function.
-     *
-     * @param mean  the mean.
-     * @param std  the standard deviation (&gt; 0).
-     */
-    public NormalDistributionFunction2D(double mean, double std) {
-        if (std <= 0) {
-            throw new IllegalArgumentException("Requires 'std' > 0.");
-        }
-        this.mean = mean;
-        this.std = std;
-        // calculate constant values
-        this.factor = 1 / (std * Math.sqrt(2.0 * Math.PI));
-        this.denominator = 2 * std * std;
+  /**
+   * Constructs a new normal distribution function.
+   *
+   * @param mean the mean.
+   * @param std the standard deviation (&gt; 0).
+   */
+  public NormalDistributionFunction2D(double mean, double std) {
+    if (std <= 0) {
+      throw new IllegalArgumentException("Requires 'std' > 0.");
     }
+    this.mean = mean;
+    this.std = std;
+    // calculate constant values
+    this.factor = 1 / (std * Math.sqrt(2.0 * Math.PI));
+    this.denominator = 2 * std * std;
+  }
 
-    /**
-     * Returns the mean for the function.
-     *
-     * @return The mean.
-     */
-    public double getMean() {
-        return this.mean;
-    }
-    
-    /**
-     * Returns the standard deviation for the function.
-     *
-     * @return The standard deviation.
-     */
-    public double getStandardDeviation() {
-        return this.std;
-    }
+  /**
+   * Returns the mean for the function.
+   *
+   * @return The mean.
+   */
+  public double getMean() {
+    return this.mean;
+  }
 
-    /**
-     * Returns the function value.
-     *
-     * @param x  the x-value.
-     *
-     * @return The value.
-     */
-    @Override
-    public double getValue(double x) {
-        double z = x - this.mean;
-        return this.factor * Math.exp(-z * z / this.denominator);
-    }
+  /**
+   * Returns the standard deviation for the function.
+   *
+   * @return The standard deviation.
+   */
+  public double getStandardDeviation() {
+    return this.std;
+  }
 
-    /**
-     * Tests this function for equality with an arbitrary object.
-     *
-     * @param obj  the object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof NormalDistributionFunction2D)) {
-            return false;
-        }
-        NormalDistributionFunction2D that = (NormalDistributionFunction2D) obj;
-        if (this.mean != that.mean) {
-            return false;
-        }
-        if (this.std != that.std) {
-            return false;
-        }
-        return true;
-    }
+  /**
+   * Returns the function value.
+   *
+   * @param x the x-value.
+   * @return The value.
+   */
+  @Override
+  public double getValue(double x) {
+    double z = x - this.mean;
+    return this.factor * Math.exp(-z * z / this.denominator);
+  }
 
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int result = 29;
-        result = HashUtils.hashCode(result, this.mean);
-        result = HashUtils.hashCode(result, this.std);
-        return result;
+  /**
+   * Tests this function for equality with an arbitrary object.
+   *
+   * @param obj the object ({@code null} permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof NormalDistributionFunction2D)) {
+      return false;
     }
+    NormalDistributionFunction2D that = (NormalDistributionFunction2D) obj;
+    if (this.mean != that.mean) {
+      return false;
+    }
+    if (this.std != that.std) {
+      return false;
+    }
+    return true;
+  }
 
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int result = 29;
+    result = HashUtils.hashCode(result, this.mean);
+    result = HashUtils.hashCode(result, this.std);
+    return result;
+  }
 }
