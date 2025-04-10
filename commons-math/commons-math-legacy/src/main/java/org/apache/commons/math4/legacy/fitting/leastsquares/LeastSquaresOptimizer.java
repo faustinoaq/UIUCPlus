e9c41a0cd395861bd19a7076c4ae9a39531b77e1
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:28:52 -0500

    Formatted main branch before codet5-base.commons-math.mid-1719.idx-1275.3.mutant injection

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/fitting/leastsquares/LeastSquaresOptimizer.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/fitting/leastsquares/LeastSquaresOptimizer.java
@@ -23,38 +23,35 @@ package org.apache.commons.math4.legacy.fitting.leastsquares;
  */
 public interface LeastSquaresOptimizer {
 
-    /**
-     * Solve the non-linear least squares problem.
-     *
-     *
-     * @param leastSquaresProblem the problem definition, including model function and
-     *                            convergence criteria.
-     * @return The optimum.
-     */
-    Optimum optimize(LeastSquaresProblem leastSquaresProblem);
+  /**
+   * Solve the non-linear least squares problem.
+   *
+   * @param leastSquaresProblem the problem definition, including model function and convergence
+   *     criteria.
+   * @return The optimum.
+   */
+  Optimum optimize(LeastSquaresProblem leastSquaresProblem);
+
+  /**
+   * The optimum found by the optimizer. This object contains the point, its value, and some
+   * metadata.
+   */
+  // TODO Solution?
+  interface Optimum extends LeastSquaresProblem.Evaluation {
 
     /**
-     * The optimum found by the optimizer. This object contains the point, its value, and
-     * some metadata.
+     * Get the number of times the model was evaluated in order to produce this optimum.
+     *
+     * @return the number of model (objective) function evaluations
      */
-    //TODO Solution?
-    interface Optimum extends LeastSquaresProblem.Evaluation {
+    int getEvaluations();
 
-        /**
-         * Get the number of times the model was evaluated in order to produce this
-         * optimum.
-         *
-         * @return the number of model (objective) function evaluations
-         */
-        int getEvaluations();
-
-        /**
-         * Get the number of times the algorithm iterated in order to produce this
-         * optimum. In general least squares it is common to have one {@link
-         * #getEvaluations() evaluation} per iterations.
-         *
-         * @return the number of iterations
-         */
-        int getIterations();
-    }
+    /**
+     * Get the number of times the algorithm iterated in order to produce this optimum. In general
+     * least squares it is common to have one {@link #getEvaluations() evaluation} per iterations.
+     *
+     * @return the number of iterations
+     */
+    int getIterations();
+  }
 }
