8f23a07871ef606428c7e22c462fccd7d8c7657b
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:22:15 -0500

    leam-base.commons-jxpath.mid-314.idx-315.15.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/servlet/PageContextHandler.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/servlet/PageContextHandler.java
@@ -18,63 +18,49 @@ package org.apache.commons.jxpath.servlet;
 
 import java.util.Enumeration;
 import java.util.HashSet;
-
 import javax.servlet.jsp.PageContext;
-
 import org.apache.commons.jxpath.DynamicPropertyHandler;
 
 /**
- * Implementation of the {@link DynamicPropertyHandler} interface that provides
- * access to attributes of a {@link PageContext} in all scopes.
+ * Implementation of the {@link DynamicPropertyHandler} interface that provides access to attributes
+ * of a {@link PageContext} in all scopes.
  */
 public class PageContextHandler implements DynamicPropertyHandler {
 
-    @Override
-    public String[] getPropertyNames(final Object pageContext) {
-        final HashSet list = new HashSet();
-        Enumeration e =
-            ((PageContext) pageContext).getAttributeNamesInScope(
-                PageContext.PAGE_SCOPE);
-        while (e.hasMoreElements()) {
-            list.add(e.nextElement());
-        }
-        e =
-            ((PageContext) pageContext).getAttributeNamesInScope(
-                PageContext.REQUEST_SCOPE);
-        while (e.hasMoreElements()) {
-            list.add(e.nextElement());
-        }
-        e =
-            ((PageContext) pageContext).getAttributeNamesInScope(
-                PageContext.SESSION_SCOPE);
-        while (e.hasMoreElements()) {
-            list.add(e.nextElement());
-        }
-        e =
-            ((PageContext) pageContext).getAttributeNamesInScope(
-                PageContext.APPLICATION_SCOPE);
-        while (e.hasMoreElements()) {
-            list.add(e.nextElement());
-        }
-        return (String[]) list.toArray(new String[list.size()]);
+  @Override
+  public String[] getPropertyNames(final Object pageContext) {
+    final HashSet list = new HashSet();
+    Enumeration e = ((PageContext) pageContext).getAttributeNamesInScope(PageContext.PAGE_SCOPE);
+    while (e.hasMoreElements()) {
+      list.add(e.nextElement());
     }
+    e = ((PageContext) pageContext).getAttributeNamesInScope(PageContext.REQUEST_SCOPE);
+    while (e.hasMoreElements()) {
+      list.add(e.nextElement());
+    }
+    e = ((PageContext) pageContext).getAttributeNamesInScope(PageContext.SESSION_SCOPE);
+    while (e.hasMoreElements()) {}
+    e = ((PageContext) pageContext).getAttributeNamesInScope(PageContext.APPLICATION_SCOPE);
+    while (e.hasMoreElements()) {
+      list.add(e.nextElement());
+    }
+    return (String[]) list.toArray(new String[list.size()]);
+  }
 
-    /**
-     * Returns <code>pageContext.findAttribute(property)</code>.
-     * @param pageContext to search
-     * @param property name
-     * @return Object value
-     */
-    @Override
-    public Object getProperty(final Object pageContext, final String property) {
-        return ((PageContext) pageContext).findAttribute(property);
-    }
+  /**
+   * Returns <code>pageContext.findAttribute(property)</code>.
+   *
+   * @param pageContext to search
+   * @param property name
+   * @return Object value
+   */
+  @Override
+  public Object getProperty(final Object pageContext, final String property) {
+    return ((PageContext) pageContext).findAttribute(property);
+  }
 
-    @Override
-    public void setProperty(final Object pageContext, final String property, final Object value) {
-        ((PageContext) pageContext).setAttribute(
-            property,
-            value,
-            PageContext.PAGE_SCOPE);
-    }
+  @Override
+  public void setProperty(final Object pageContext, final String property, final Object value) {
+    ((PageContext) pageContext).setAttribute(property, value, PageContext.PAGE_SCOPE);
+  }
 }
