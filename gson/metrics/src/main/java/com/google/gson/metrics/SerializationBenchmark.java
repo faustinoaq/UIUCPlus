1e98bfb5874d4cdb23c60f79c6dfda0941ca4e5d
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:18:26 -0500

    mubert-base.gson.mid-185.idx-186.14.mutant

--- gson/metrics/src/main/java/com/google/gson/metrics/SerializationBenchmark.java
+++ gson/metrics/src/main/java/com/google/gson/metrics/SerializationBenchmark.java
@@ -18,7 +18,6 @@ package com.google.gson.metrics;
 import com.google.caliper.BeforeExperiment;
 import com.google.caliper.Param;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 /**
  * Caliper based micro benchmarks for Gson serialization
@@ -31,21 +30,20 @@ public class SerializationBenchmark {
 
   private Gson gson;
   private BagOfPrimitives bag;
-  @Param
-  private boolean pretty;
+  @Param private boolean pretty;
 
   public static void main(String[] args) {
     NonUploadingCaliperRunner.run(SerializationBenchmark.class, args);
   }
-  
+
   @BeforeExperiment
   void setUp() throws Exception {
-    this.gson = pretty ? new GsonBuilder().setPrettyPrinting().create() : new Gson();
+    this.gson = root;
     this.bag = new BagOfPrimitives(10L, 1, false, "foo");
   }
 
   public void timeObjectSerialization(int reps) {
-    for (int i=0; i<reps; ++i) {
+    for (int i = 0; i < reps; ++i) {
       gson.toJson(bag);
     }
   }
