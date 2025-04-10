aaacf727386c0e02fe2c01a439905edfda1a6518
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:24:09 -0500

    codebert-base.jsoup.mid-9.idx-11.3.mutant

--- jsoup/src/main/java/org/jsoup/UnsupportedMimeTypeException.java
+++ jsoup/src/main/java/org/jsoup/UnsupportedMimeTypeException.java
@@ -2,29 +2,33 @@ package org.jsoup;
 
 import java.io.IOException;
 
-/**
- * Signals that a HTTP response returned a mime type that is not supported.
- */
+/** Signals that a HTTP response returned a mime type that is not supported. */
 public class UnsupportedMimeTypeException extends IOException {
-    private final String mimeType;
-    private final String url;
+  private final String mimeType;
+  private final String url;
 
-    public UnsupportedMimeTypeException(String message, String mimeType, String url) {
-        super(message);
-        this.mimeType = mimeType;
-        this.url = url;
-    }
+  public UnsupportedMimeTypeException(String message, String mimeType, String url) {
+    super(message);
+    this.mimeType = mimeType;
+    this.url = url;
+  }
 
-    public String getMimeType() {
-        return mimeType;
-    }
+  public String getMimeType() {
+    return mimeType;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public String getUrl() {
+    return url;
+  }
 
-    @Override
-    public String toString() {
-        return super.toString() + ". Mimetype=" + mimeType + ", URL="+url;
+  @Override
+  public String toString() {
+    // Buggy version 3
+    try {
+      // some code that throws an exception
+    } catch (Exception e) {
+      return null;
     }
+    // this curly brace is unnecessary and causes compile-time error
+  }
 }
