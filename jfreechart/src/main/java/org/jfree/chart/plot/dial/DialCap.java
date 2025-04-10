65c9a38cddda9416ed3124d51e9518889d019015
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:33:45 -0500

    Formatted main branch before NatGen-base.jfreechart.mid-3412.idx-2530.2.mutant injection

--- jfreechart/src/main/java/org/jfree/chart/plot/dial/DialCap.java
+++ jfreechart/src/main/java/org/jfree/chart/plot/dial/DialCap.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * ------------
@@ -47,289 +47,258 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-
+import org.jfree.chart.api.PublicCloneable;
+import org.jfree.chart.internal.Args;
 import org.jfree.chart.internal.HashUtils;
 import org.jfree.chart.internal.PaintUtils;
-import org.jfree.chart.internal.Args;
-import org.jfree.chart.api.PublicCloneable;
 import org.jfree.chart.internal.SerialUtils;
 
 /**
- * A regular dial layer that can be used to draw a cap over the center of
- * the dial (the base of the dial pointer(s)).
+ * A regular dial layer that can be used to draw a cap over the center of the dial (the base of the
+ * dial pointer(s)).
  */
-public class DialCap extends AbstractDialLayer implements DialLayer, Cloneable,
-        PublicCloneable, Serializable {
+public class DialCap extends AbstractDialLayer
+    implements DialLayer, Cloneable, PublicCloneable, Serializable {
 
-    /** For serialization. */
-    static final long serialVersionUID = -2929484264982524463L;
+  /** For serialization. */
+  static final long serialVersionUID = -2929484264982524463L;
 
-    /**
-     * The radius of the cap, as a percentage of the framing rectangle.
-     */
-    private double radius;
+  /** The radius of the cap, as a percentage of the framing rectangle. */
+  private double radius;
 
-    /**
-     * The fill paint.  This field is transient because it requires special
-     * handling for serialization.
-     */
-    private transient Paint fillPaint;
+  /**
+   * The fill paint. This field is transient because it requires special handling for serialization.
+   */
+  private transient Paint fillPaint;
 
-    /**
-     * The paint used to draw the cap outline (this should never be
-     * {@code null}).  This field is transient because it requires
-     * special handling for serialization.
-     */
-    private transient Paint outlinePaint;
+  /**
+   * The paint used to draw the cap outline (this should never be {@code null}). This field is
+   * transient because it requires special handling for serialization.
+   */
+  private transient Paint outlinePaint;
 
-    /**
-     * The stroke used to draw the cap outline (this should never be
-     * {@code null}).   This field is transient because it requires
-     * special handling for serialization.
-     */
-    private transient Stroke outlineStroke;
+  /**
+   * The stroke used to draw the cap outline (this should never be {@code null}). This field is
+   * transient because it requires special handling for serialization.
+   */
+  private transient Stroke outlineStroke;
 
-    /**
-     * Creates a new instance of {@code StandardDialBackground}.  The
-     * default background paint is {@code Color.WHITE}.
-     */
-    public DialCap() {
-        this.radius = 0.05;
-        this.fillPaint = Color.WHITE;
-        this.outlinePaint = Color.BLACK;
-        this.outlineStroke = new BasicStroke(2.0f);
+  /**
+   * Creates a new instance of {@code StandardDialBackground}. The default background paint is
+   * {@code Color.WHITE}.
+   */
+  public DialCap() {
+    this.radius = 0.05;
+    this.fillPaint = Color.WHITE;
+    this.outlinePaint = Color.BLACK;
+    this.outlineStroke = new BasicStroke(2.0f);
+  }
+
+  /**
+   * Returns the radius of the cap, as a percentage of the dial's framing rectangle.
+   *
+   * @return The radius.
+   * @see #setRadius(double)
+   */
+  public double getRadius() {
+    return this.radius;
+  }
+
+  /**
+   * Sets the radius of the cap, as a percentage of the dial's framing rectangle, and sends a {@link
+   * DialLayerChangeEvent} to all registered listeners.
+   *
+   * @param radius the radius (must be greater than zero).
+   * @see #getRadius()
+   */
+  public void setRadius(double radius) {
+    if (radius <= 0.0) {
+      throw new IllegalArgumentException("Requires radius > 0.0.");
     }
+    this.radius = radius;
+    notifyListeners(new DialLayerChangeEvent(this));
+  }
 
-    /**
-     * Returns the radius of the cap, as a percentage of the dial's framing
-     * rectangle.
-     *
-     * @return The radius.
-     *
-     * @see #setRadius(double)
-     */
-    public double getRadius() {
-        return this.radius;
+  /**
+   * Returns the paint used to fill the cap.
+   *
+   * @return The paint (never {@code null}).
+   * @see #setFillPaint(Paint)
+   */
+  public Paint getFillPaint() {
+    return this.fillPaint;
+  }
+
+  /**
+   * Sets the paint for the cap background and sends a {@link DialLayerChangeEvent} to all
+   * registered listeners.
+   *
+   * @param paint the paint ({@code null} not permitted).
+   * @see #getFillPaint()
+   */
+  public void setFillPaint(Paint paint) {
+    Args.nullNotPermitted(paint, "paint");
+    this.fillPaint = paint;
+    notifyListeners(new DialLayerChangeEvent(this));
+  }
+
+  /**
+   * Returns the paint used to draw the outline of the cap.
+   *
+   * @return The paint (never {@code null}).
+   * @see #setOutlinePaint(Paint)
+   */
+  public Paint getOutlinePaint() {
+    return this.outlinePaint;
+  }
+
+  /**
+   * Sets the paint used to draw the outline of the cap and sends a {@link DialLayerChangeEvent} to
+   * all registered listeners.
+   *
+   * @param paint the paint ({@code null} not permitted).
+   * @see #getOutlinePaint()
+   */
+  public void setOutlinePaint(Paint paint) {
+    Args.nullNotPermitted(paint, "paint");
+    this.outlinePaint = paint;
+    notifyListeners(new DialLayerChangeEvent(this));
+  }
+
+  /**
+   * Returns the stroke used to draw the outline of the cap.
+   *
+   * @return The stroke (never {@code null}).
+   * @see #setOutlineStroke(Stroke)
+   */
+  public Stroke getOutlineStroke() {
+    return this.outlineStroke;
+  }
+
+  /**
+   * Sets the stroke used to draw the outline of the cap and sends a {@link DialLayerChangeEvent} to
+   * all registered listeners.
+   *
+   * @param stroke the stroke ({@code null} not permitted).
+   * @see #getOutlineStroke()
+   */
+  public void setOutlineStroke(Stroke stroke) {
+    Args.nullNotPermitted(stroke, "stroke");
+    this.outlineStroke = stroke;
+    notifyListeners(new DialLayerChangeEvent(this));
+  }
+
+  /**
+   * Returns {@code true} to indicate that this layer should be clipped within the dial window.
+   *
+   * @return {@code true}.
+   */
+  @Override
+  public boolean isClippedToWindow() {
+    return true;
+  }
+
+  /**
+   * Draws the background to the specified graphics device. If the dial frame specifies a window,
+   * the clipping region will already have been set to this window before this method is called.
+   *
+   * @param g2 the graphics device ({@code null} not permitted).
+   * @param plot the plot (ignored here).
+   * @param frame the dial frame (ignored here).
+   * @param view the view rectangle ({@code null} not permitted).
+   */
+  @Override
+  public void draw(Graphics2D g2, DialPlot plot, Rectangle2D frame, Rectangle2D view) {
+
+    g2.setPaint(this.fillPaint);
+
+    Rectangle2D f = DialPlot.rectangleByRadius(frame, this.radius, this.radius);
+    Ellipse2D e = new Ellipse2D.Double(f.getX(), f.getY(), f.getWidth(), f.getHeight());
+    g2.fill(e);
+    g2.setPaint(this.outlinePaint);
+    g2.setStroke(this.outlineStroke);
+    g2.draw(e);
+  }
+
+  /**
+   * Tests this instance for equality with an arbitrary object.
+   *
+   * @param obj the object ({@code null} permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
-
-    /**
-     * Sets the radius of the cap, as a percentage of the dial's framing
-     * rectangle, and sends a {@link DialLayerChangeEvent} to all registered
-     * listeners.
-     *
-     * @param radius  the radius (must be greater than zero).
-     *
-     * @see #getRadius()
-     */
-    public void setRadius(double radius) {
-        if (radius <= 0.0) {
-            throw new IllegalArgumentException("Requires radius > 0.0.");
-        }
-        this.radius = radius;
-        notifyListeners(new DialLayerChangeEvent(this));
+    if (!(obj instanceof DialCap)) {
+      return false;
     }
-
-    /**
-     * Returns the paint used to fill the cap.
-     *
-     * @return The paint (never {@code null}).
-     *
-     * @see #setFillPaint(Paint)
-     */
-    public Paint getFillPaint() {
-        return this.fillPaint;
+    DialCap that = (DialCap) obj;
+    if (this.radius != that.radius) {
+      return false;
     }
-
-    /**
-     * Sets the paint for the cap background and sends a
-     * {@link DialLayerChangeEvent} to all registered listeners.
-     *
-     * @param paint  the paint ({@code null} not permitted).
-     *
-     * @see #getFillPaint()
-     */
-    public void setFillPaint(Paint paint) {
-        Args.nullNotPermitted(paint, "paint");
-        this.fillPaint = paint;
-        notifyListeners(new DialLayerChangeEvent(this));
+    if (!PaintUtils.equal(this.fillPaint, that.fillPaint)) {
+      return false;
     }
-
-    /**
-     * Returns the paint used to draw the outline of the cap.
-     *
-     * @return The paint (never {@code null}).
-     *
-     * @see #setOutlinePaint(Paint)
-     */
-    public Paint getOutlinePaint() {
-        return this.outlinePaint;
+    if (!PaintUtils.equal(this.outlinePaint, that.outlinePaint)) {
+      return false;
     }
-
-    /**
-     * Sets the paint used to draw the outline of the cap and sends a
-     * {@link DialLayerChangeEvent} to all registered listeners.
-     *
-     * @param paint  the paint ({@code null} not permitted).
-     *
-     * @see #getOutlinePaint()
-     */
-    public void setOutlinePaint(Paint paint) {
-        Args.nullNotPermitted(paint, "paint");
-        this.outlinePaint = paint;
-        notifyListeners(new DialLayerChangeEvent(this));
+    if (!this.outlineStroke.equals(that.outlineStroke)) {
+      return false;
     }
+    return super.equals(obj);
+  }
 
-    /**
-     * Returns the stroke used to draw the outline of the cap.
-     *
-     * @return The stroke (never {@code null}).
-     *
-     * @see #setOutlineStroke(Stroke)
-     */
-    public Stroke getOutlineStroke() {
-        return this.outlineStroke;
-    }
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return The hash code.
+   */
+  @Override
+  public int hashCode() {
+    int result = 193;
+    result = 37 * result + HashUtils.hashCodeForPaint(this.fillPaint);
+    result = 37 * result + HashUtils.hashCodeForPaint(this.outlinePaint);
+    result = 37 * result + this.outlineStroke.hashCode();
+    return result;
+  }
 
-    /**
-     * Sets the stroke used to draw the outline of the cap and sends a
-     * {@link DialLayerChangeEvent} to all registered listeners.
-     *
-     * @param stroke  the stroke ({@code null} not permitted).
-     *
-     * @see #getOutlineStroke()
-     */
-    public void setOutlineStroke(Stroke stroke) {
-        Args.nullNotPermitted(stroke, "stroke");
-        this.outlineStroke = stroke;
-        notifyListeners(new DialLayerChangeEvent(this));
-    }
+  /**
+   * Returns a clone of this instance.
+   *
+   * @return A clone.
+   * @throws CloneNotSupportedException if some attribute of the cap cannot be cloned.
+   */
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
 
-    /**
-     * Returns {@code true} to indicate that this layer should be
-     * clipped within the dial window.
-     *
-     * @return {@code true}.
-     */
-    @Override
-    public boolean isClippedToWindow() {
-        return true;
-    }
+  /**
+   * Provides serialization support.
+   *
+   * @param stream the output stream.
+   * @throws IOException if there is an I/O error.
+   */
+  private void writeObject(ObjectOutputStream stream) throws IOException {
+    stream.defaultWriteObject();
+    SerialUtils.writePaint(this.fillPaint, stream);
+    SerialUtils.writePaint(this.outlinePaint, stream);
+    SerialUtils.writeStroke(this.outlineStroke, stream);
+  }
 
-    /**
-     * Draws the background to the specified graphics device.  If the dial
-     * frame specifies a window, the clipping region will already have been
-     * set to this window before this method is called.
-     *
-     * @param g2  the graphics device ({@code null} not permitted).
-     * @param plot  the plot (ignored here).
-     * @param frame  the dial frame (ignored here).
-     * @param view  the view rectangle ({@code null} not permitted).
-     */
-    @Override
-    public void draw(Graphics2D g2, DialPlot plot, Rectangle2D frame,
-            Rectangle2D view) {
-
-        g2.setPaint(this.fillPaint);
-
-        Rectangle2D f = DialPlot.rectangleByRadius(frame, this.radius,
-                this.radius);
-        Ellipse2D e = new Ellipse2D.Double(f.getX(), f.getY(), f.getWidth(),
-                f.getHeight());
-        g2.fill(e);
-        g2.setPaint(this.outlinePaint);
-        g2.setStroke(this.outlineStroke);
-        g2.draw(e);
-
-    }
-
-    /**
-     * Tests this instance for equality with an arbitrary object.
-     *
-     * @param obj  the object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof DialCap)) {
-            return false;
-        }
-        DialCap that = (DialCap) obj;
-        if (this.radius != that.radius) {
-            return false;
-        }
-        if (!PaintUtils.equal(this.fillPaint, that.fillPaint)) {
-            return false;
-        }
-        if (!PaintUtils.equal(this.outlinePaint, that.outlinePaint)) {
-            return false;
-        }
-        if (!this.outlineStroke.equals(that.outlineStroke)) {
-            return false;
-        }
-        return super.equals(obj);
-    }
-
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return The hash code.
-     */
-    @Override
-    public int hashCode() {
-        int result = 193;
-        result = 37 * result + HashUtils.hashCodeForPaint(this.fillPaint);
-        result = 37 * result + HashUtils.hashCodeForPaint(
-                this.outlinePaint);
-        result = 37 * result + this.outlineStroke.hashCode();
-        return result;
-    }
-
-    /**
-     * Returns a clone of this instance.
-     *
-     * @return A clone.
-     *
-     * @throws CloneNotSupportedException if some attribute of the cap cannot
-     *     be cloned.
-     */
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    /**
-     * Provides serialization support.
-     *
-     * @param stream  the output stream.
-     *
-     * @throws IOException  if there is an I/O error.
-     */
-    private void writeObject(ObjectOutputStream stream) throws IOException {
-        stream.defaultWriteObject();
-        SerialUtils.writePaint(this.fillPaint, stream);
-        SerialUtils.writePaint(this.outlinePaint, stream);
-        SerialUtils.writeStroke(this.outlineStroke, stream);
-    }
-
-    /**
-     * Provides serialization support.
-     *
-     * @param stream  the input stream.
-     *
-     * @throws IOException  if there is an I/O error.
-     * @throws ClassNotFoundException  if there is a classpath problem.
-     */
-    private void readObject(ObjectInputStream stream)
-            throws IOException, ClassNotFoundException {
-        stream.defaultReadObject();
-        this.fillPaint = SerialUtils.readPaint(stream);
-        this.outlinePaint = SerialUtils.readPaint(stream);
-        this.outlineStroke = SerialUtils.readStroke(stream);
-    }
-
+  /**
+   * Provides serialization support.
+   *
+   * @param stream the input stream.
+   * @throws IOException if there is an I/O error.
+   * @throws ClassNotFoundException if there is a classpath problem.
+   */
+  private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+    stream.defaultReadObject();
+    this.fillPaint = SerialUtils.readPaint(stream);
+    this.outlinePaint = SerialUtils.readPaint(stream);
+    this.outlineStroke = SerialUtils.readStroke(stream);
+  }
 }
-
