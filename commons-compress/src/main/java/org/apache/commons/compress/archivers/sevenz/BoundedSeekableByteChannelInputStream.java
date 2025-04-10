18e4db3dc3c736d1e0beea5e0bd2fc88cc0895df
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:16 -0500

    Formatted main branch before mubert-base.commons-compress.mid-1005.idx-1006.826.mutant injection

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/sevenz/BoundedSeekableByteChannelInputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/sevenz/BoundedSeekableByteChannelInputStream.java
@@ -23,84 +23,81 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
 
 class BoundedSeekableByteChannelInputStream extends InputStream {
-    private static final int MAX_BUF_LEN = 8192;
-    private final ByteBuffer buffer;
-    private final SeekableByteChannel channel;
-    private long bytesRemaining;
+  private static final int MAX_BUF_LEN = 8192;
+  private final ByteBuffer buffer;
+  private final SeekableByteChannel channel;
+  private long bytesRemaining;
 
-    public BoundedSeekableByteChannelInputStream(final SeekableByteChannel channel,
-            final long size) {
-        this.channel = channel;
-        this.bytesRemaining = size;
-        if (size < MAX_BUF_LEN && size > 0) {
-            buffer = ByteBuffer.allocate((int) size);
-        } else {
-            buffer = ByteBuffer.allocate(MAX_BUF_LEN);
-        }
+  public BoundedSeekableByteChannelInputStream(final SeekableByteChannel channel, final long size) {
+    this.channel = channel;
+    this.bytesRemaining = size;
+    if (size < MAX_BUF_LEN && size > 0) {
+      buffer = ByteBuffer.allocate((int) size);
+    } else {
+      buffer = ByteBuffer.allocate(MAX_BUF_LEN);
     }
+  }
 
-    @Override
-    public void close() {
-        // the nested channel is controlled externally
-    }
+  @Override
+  public void close() {
+    // the nested channel is controlled externally
+  }
 
-    @Override
-    public int read() throws IOException {
-        if (bytesRemaining > 0) {
-            --bytesRemaining;
-            final int read = read(1);
-            if (read < 0) {
-                return read;
-            }
-            return buffer.get() & 0xff;
-        }
-        return -1;
-    }
-
-    /**
-     * Reads up to len bytes of data from the input stream into an array of bytes.
-     *
-     * <p>An attempt is made to read as many as len bytes, but a
-     * smaller number may be read. The number of bytes actually read
-     * is returned as an integer.</p>
-     *
-     * <p>This implementation may return 0 if the underlying {@link
-     * SeekableByteChannel} is non-blocking and currently hasn't got
-     * any bytes available.</p>
-     */
-    @Override
-    public int read(final byte[] b, final int off, final int len) throws IOException {
-        if (len == 0) {
-            return 0;
-        }
-        if (bytesRemaining <= 0) {
-            return -1;
-        }
-        int bytesToRead = len;
-        if (bytesToRead > bytesRemaining) {
-            bytesToRead = (int) bytesRemaining;
-        }
-        final int bytesRead;
-        final ByteBuffer buf;
-        if (bytesToRead <= buffer.capacity()) {
-            buf = buffer;
-            bytesRead = read(bytesToRead);
-        } else {
-            buf = ByteBuffer.allocate(bytesToRead);
-            bytesRead = channel.read(buf);
-            buf.flip();
-        }
-        if (bytesRead >= 0) {
-            buf.get(b, off, bytesRead);
-            bytesRemaining -= bytesRead;
-        }
-        return bytesRead;
-    }
-
-    private int read(final int len) throws IOException {
-        buffer.rewind().limit(len);
-        final int read = channel.read(buffer);
-        buffer.flip();
+  @Override
+  public int read() throws IOException {
+    if (bytesRemaining > 0) {
+      --bytesRemaining;
+      final int read = read(1);
+      if (read < 0) {
         return read;
+      }
+      return buffer.get() & 0xff;
     }
+    return -1;
+  }
+
+  /**
+   * Reads up to len bytes of data from the input stream into an array of bytes.
+   *
+   * <p>An attempt is made to read as many as len bytes, but a smaller number may be read. The
+   * number of bytes actually read is returned as an integer.
+   *
+   * <p>This implementation may return 0 if the underlying {@link SeekableByteChannel} is
+   * non-blocking and currently hasn't got any bytes available.
+   */
+  @Override
+  public int read(final byte[] b, final int off, final int len) throws IOException {
+    if (len == 0) {
+      return 0;
+    }
+    if (bytesRemaining <= 0) {
+      return -1;
+    }
+    int bytesToRead = len;
+    if (bytesToRead > bytesRemaining) {
+      bytesToRead = (int) bytesRemaining;
+    }
+    final int bytesRead;
+    final ByteBuffer buf;
+    if (bytesToRead <= buffer.capacity()) {
+      buf = buffer;
+      bytesRead = read(bytesToRead);
+    } else {
+      buf = ByteBuffer.allocate(bytesToRead);
+      bytesRead = channel.read(buf);
+      buf.flip();
+    }
+    if (bytesRead >= 0) {
+      buf.get(b, off, bytesRead);
+      bytesRemaining -= bytesRead;
+    }
+    return bytesRead;
+  }
+
+  private int read(final int len) throws IOException {
+    buffer.rewind().limit(len);
+    final int read = channel.read(buffer);
+    buffer.flip();
+    return read;
+  }
 }
