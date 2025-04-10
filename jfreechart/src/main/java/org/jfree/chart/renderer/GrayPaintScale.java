7d1b7658c95c83132a2ea6749cf2bbbc69c77649
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:18:53 -0500

    Formatted main branch before mubert-base.jfreechart.mid-1159.idx-1160.306.mutant injection

--- jfreechart/src/main/java/org/jfree/chart/renderer/GrayPaintScale.java
+++ jfreechart/src/main/java/org/jfree/chart/renderer/GrayPaintScale.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * -------------------
@@ -39,184 +39,161 @@ package org.jfree.chart.renderer;
 import java.awt.Color;
 import java.awt.Paint;
 import java.io.Serializable;
-
-import org.jfree.chart.internal.HashUtils;
 import org.jfree.chart.api.PublicCloneable;
+import org.jfree.chart.internal.HashUtils;
 
-/**
- * A paint scale that returns shades of gray.
- */
-public class GrayPaintScale
-        implements PaintScale, PublicCloneable, Serializable {
+/** A paint scale that returns shades of gray. */
+public class GrayPaintScale implements PaintScale, PublicCloneable, Serializable {
 
-    /** The lower bound. */
-    private double lowerBound;
+  /** The lower bound. */
+  private double lowerBound;
 
-    /** The upper bound. */
-    private double upperBound;
+  /** The upper bound. */
+  private double upperBound;
 
-    /**
-     * The alpha transparency (0-255).
-     */
-    private int alpha;
+  /** The alpha transparency (0-255). */
+  private int alpha;
 
-    /**
-     * Creates a new {@code GrayPaintScale} instance with default values.
-     */
-    public GrayPaintScale() {
-        this(0.0, 1.0);
+  /** Creates a new {@code GrayPaintScale} instance with default values. */
+  public GrayPaintScale() {
+    this(0.0, 1.0);
+  }
+
+  /**
+   * Creates a new paint scale for values in the specified range.
+   *
+   * @param lowerBound the lower bound.
+   * @param upperBound the upper bound.
+   * @throws IllegalArgumentException if {@code lowerBound} is not less than {@code upperBound}.
+   */
+  public GrayPaintScale(double lowerBound, double upperBound) {
+    this(lowerBound, upperBound, 255);
+  }
+
+  /**
+   * Creates a new paint scale for values in the specified range.
+   *
+   * @param lowerBound the lower bound.
+   * @param upperBound the upper bound.
+   * @param alpha the alpha transparency (0-255).
+   * @throws IllegalArgumentException if {@code lowerBound} is not less than {@code upperBound}, or
+   *     {@code alpha} is not in the range 0 to 255.
+   */
+  public GrayPaintScale(double lowerBound, double upperBound, int alpha) {
+    if (lowerBound >= upperBound) {
+      throw new IllegalArgumentException("Requires lowerBound < upperBound.");
     }
-
-    /**
-     * Creates a new paint scale for values in the specified range.
-     *
-     * @param lowerBound  the lower bound.
-     * @param upperBound  the upper bound.
-     *
-     * @throws IllegalArgumentException if {@code lowerBound} is not
-     *       less than {@code upperBound}.
-     */
-    public GrayPaintScale(double lowerBound, double upperBound) {
-        this(lowerBound, upperBound, 255);
+    if (alpha < 0 || alpha > 255) {
+      throw new IllegalArgumentException("Requires alpha in the range 0 to 255.");
     }
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+    this.alpha = alpha;
+  }
 
-    /**
-     * Creates a new paint scale for values in the specified range.
-     *
-     * @param lowerBound  the lower bound.
-     * @param upperBound  the upper bound.
-     * @param alpha  the alpha transparency (0-255).
-     *
-     * @throws IllegalArgumentException if {@code lowerBound} is not
-     *       less than {@code upperBound}, or {@code alpha} is not in
-     *       the range 0 to 255.
-     */
-    public GrayPaintScale(double lowerBound, double upperBound, int alpha) {
-        if (lowerBound >= upperBound) {
-            throw new IllegalArgumentException(
-                    "Requires lowerBound < upperBound.");
-        }
-        if (alpha < 0 || alpha > 255) {
-            throw new IllegalArgumentException(
-                    "Requires alpha in the range 0 to 255.");
+  /**
+   * Returns the lower bound.
+   *
+   * @return The lower bound.
+   * @see #getUpperBound()
+   */
+  @Override
+  public double getLowerBound() {
+    return this.lowerBound;
+  }
 
-        }
-        this.lowerBound = lowerBound;
-        this.upperBound = upperBound;
-        this.alpha = alpha;
+  /**
+   * Returns the upper bound.
+   *
+   * @return The upper bound.
+   * @see #getLowerBound()
+   */
+  @Override
+  public double getUpperBound() {
+    return this.upperBound;
+  }
+
+  /**
+   * Returns the alpha transparency that was specified in the constructor.
+   *
+   * @return The alpha transparency (in the range 0 to 255).
+   */
+  public int getAlpha() {
+    return this.alpha;
+  }
+
+  /**
+   * Returns a paint for the specified value.
+   *
+   * @param value the value (must be within the range specified by the lower and upper bounds for
+   *     the scale).
+   * @return A paint for the specified value.
+   */
+  @Override
+  public Paint getPaint(double value) {
+    double v = Math.max(value, this.lowerBound);
+    v = Math.min(v, this.upperBound);
+    int g = (int) ((v - this.lowerBound) / (this.upperBound - this.lowerBound) * 255.0);
+    // FIXME:  it probably makes sense to allocate an array of 256 Colors
+    // and lazily populate this array...
+    return new Color(g, g, g, this.alpha);
+  }
+
+  /**
+   * Tests this {@code GrayPaintScale} instance for equality with an arbitrary object. This method
+   * returns {@code true} if and only if:
+   *
+   * <ul>
+   *   <li>{@code obj} is not {@code null};
+   *   <li>{@code obj} is an instance of {@code GrayPaintScale};
+   * </ul>
+   *
+   * @param obj the object ({@code null} permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
-
-    /**
-     * Returns the lower bound.
-     *
-     * @return The lower bound.
-     *
-     * @see #getUpperBound()
-     */
-    @Override
-    public double getLowerBound() {
-        return this.lowerBound;
+    if (!(obj instanceof GrayPaintScale)) {
+      return false;
     }
-
-    /**
-     * Returns the upper bound.
-     *
-     * @return The upper bound.
-     *
-     * @see #getLowerBound()
-     */
-    @Override
-    public double getUpperBound() {
-        return this.upperBound;
+    GrayPaintScale that = (GrayPaintScale) obj;
+    if (this.lowerBound != that.lowerBound) {
+      return false;
     }
-
-    /**
-     * Returns the alpha transparency that was specified in the constructor.
-     * 
-     * @return The alpha transparency (in the range 0 to 255).
-     */
-    public int getAlpha() {
-        return this.alpha;
+    if (this.upperBound != that.upperBound) {
+      return false;
     }
-
-    /**
-     * Returns a paint for the specified value.
-     *
-     * @param value  the value (must be within the range specified by the
-     *         lower and upper bounds for the scale).
-     *
-     * @return A paint for the specified value.
-     */
-    @Override
-    public Paint getPaint(double value) {
-        double v = Math.max(value, this.lowerBound);
-        v = Math.min(v, this.upperBound);
-        int g = (int) ((v - this.lowerBound) / (this.upperBound
-                - this.lowerBound) * 255.0);
-        // FIXME:  it probably makes sense to allocate an array of 256 Colors
-        // and lazily populate this array...
-        return new Color(g, g, g, this.alpha);
+    if (this.alpha != that.alpha) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Tests this {@code GrayPaintScale} instance for equality with an
-     * arbitrary object.  This method returns {@code true} if and only
-     * if:
-     * <ul>
-     * <li>{@code obj} is not {@code null};</li>
-     * <li>{@code obj} is an instance of {@code GrayPaintScale};</li>
-     * </ul>
-     *
-     * @param obj  the object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof GrayPaintScale)) {
-            return false;
-        }
-        GrayPaintScale that = (GrayPaintScale) obj;
-        if (this.lowerBound != that.lowerBound) {
-            return false;
-        }
-        if (this.upperBound != that.upperBound) {
-            return false;
-        }
-        if (this.alpha != that.alpha) {
-            return false;
-        }
-        return true;
-    }
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = HashUtils.hashCode(hash, this.lowerBound);
+    hash = HashUtils.hashCode(hash, this.upperBound);
+    hash = 43 * hash + this.alpha;
+    return hash;
+  }
 
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = HashUtils.hashCode(hash, this.lowerBound);
-        hash = HashUtils.hashCode(hash, this.upperBound);
-        hash = 43 * hash + this.alpha;
-        return hash;
-    }
-
-    /**
-     * Returns a clone of this {@code GrayPaintScale} instance.
-     *
-     * @return A clone.
-     *
-     * @throws CloneNotSupportedException if there is a problem cloning this
-     *     instance.
-     */
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
+  /**
+   * Returns a clone of this {@code GrayPaintScale} instance.
+   *
+   * @return A clone.
+   * @throws CloneNotSupportedException if there is a problem cloning this instance.
+   */
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
 }
