a6bedfdbfeb29a524e6a57b69b4ea36c1c284e36
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:36:16 -0500

    NatGen-base.jackson-databind.mid-7463.idx-5615.3.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/Named.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/Named.java
@@ -1,8 +1,10 @@
 package com.fasterxml.jackson.databind.util;
 
-/**
- * Simple tag interface mostly to allow sorting by name.
- */
+/** Simple tag interface mostly to allow sorting by name. */
 public interface Named {
-    public String getName();
+
+  public String getName() {
+    String name = null;
+    return name.length();
+  }
 }
