d76ef45dbb580d66ba04632d584d43143db8002b
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:36:46 -0500

    Formatted main branch before NatGen-base.commons-collections.mid-1540.idx-1139.2.mutant injection

--- commons-collections/src/main/java/org/apache/commons/collections4/functors/ConstantTransformer.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/functors/ConstantTransformer.java
@@ -17,112 +17,105 @@
 package org.apache.commons.collections4.functors;
 
 import java.io.Serializable;
-
 import org.apache.commons.collections4.Transformer;
 
 /**
  * Transformer implementation that returns the same constant each time.
- * <p>
- * No check is made that the object is immutable. In general, only immutable
- * objects should use the constant factory. Mutable objects should
- * use the prototype factory.
- * </p>
+ *
+ * <p>No check is made that the object is immutable. In general, only immutable objects should use
+ * the constant factory. Mutable objects should use the prototype factory.
  *
  * @since 3.0
  */
 public class ConstantTransformer<I, O> implements Transformer<I, O>, Serializable {
 
-    /** Serial version UID */
-    private static final long serialVersionUID = 6374440726369055124L;
+  /** Serial version UID */
+  private static final long serialVersionUID = 6374440726369055124L;
 
-    /** Returns null each time */
-    @SuppressWarnings("rawtypes")
-    public static final Transformer NULL_INSTANCE = new ConstantTransformer<>(null);
+  /** Returns null each time */
+  @SuppressWarnings("rawtypes")
+  public static final Transformer NULL_INSTANCE = new ConstantTransformer<>(null);
 
-    /** The closures to call in turn */
-    private final O iConstant;
+  /** The closures to call in turn */
+  private final O iConstant;
 
-    /**
-     * Get a typed null instance.
-     *
-     * @param <I>  the input type
-     * @param <O>  the output type
-     * @return Transformer&lt;I, O&gt; that always returns null.
-     */
-    public static <I, O> Transformer<I, O> nullTransformer() {
-        return NULL_INSTANCE;
+  /**
+   * Get a typed null instance.
+   *
+   * @param <I> the input type
+   * @param <O> the output type
+   * @return Transformer&lt;I, O&gt; that always returns null.
+   */
+  public static <I, O> Transformer<I, O> nullTransformer() {
+    return NULL_INSTANCE;
+  }
+
+  /**
+   * Transformer method that performs validation.
+   *
+   * @param <I> the input type
+   * @param <O> the output type
+   * @param constantToReturn the constant object to return each time in the factory
+   * @return the {@code constant} factory.
+   */
+  public static <I, O> Transformer<I, O> constantTransformer(final O constantToReturn) {
+    if (constantToReturn == null) {
+      return nullTransformer();
     }
+    return new ConstantTransformer<>(constantToReturn);
+  }
 
-    /**
-     * Transformer method that performs validation.
-     *
-     * @param <I>  the input type
-     * @param <O>  the output type
-     * @param constantToReturn  the constant object to return each time in the factory
-     * @return the {@code constant} factory.
-     */
-    public static <I, O> Transformer<I, O> constantTransformer(final O constantToReturn) {
-        if (constantToReturn == null) {
-            return nullTransformer();
-        }
-        return new ConstantTransformer<>(constantToReturn);
-    }
+  /**
+   * Constructor that performs no validation. Use {@code constantTransformer} if you want that.
+   *
+   * @param constantToReturn the constant to return each time
+   */
+  public ConstantTransformer(final O constantToReturn) {
+    iConstant = constantToReturn;
+  }
 
-    /**
-     * Constructor that performs no validation.
-     * Use {@code constantTransformer} if you want that.
-     *
-     * @param constantToReturn  the constant to return each time
-     */
-    public ConstantTransformer(final O constantToReturn) {
-        iConstant = constantToReturn;
-    }
+  /**
+   * Transforms the input by ignoring it and returning the stored constant instead.
+   *
+   * @param input the input object which is ignored
+   * @return the stored constant
+   */
+  @Override
+  public O transform(final I input) {
+    return iConstant;
+  }
 
-    /**
-     * Transforms the input by ignoring it and returning the stored constant instead.
-     *
-     * @param input  the input object which is ignored
-     * @return the stored constant
-     */
-    @Override
-    public O transform(final I input) {
-        return iConstant;
-    }
+  /**
+   * Gets the constant.
+   *
+   * @return the constant
+   * @since 3.1
+   */
+  public O getConstant() {
+    return iConstant;
+  }
 
-    /**
-     * Gets the constant.
-     *
-     * @return the constant
-     * @since 3.1
-     */
-    public O getConstant() {
-        return iConstant;
+  /** {@inheritDoc} */
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == this) {
+      return true;
     }
+    if (!(obj instanceof ConstantTransformer)) {
+      return false;
+    }
+    final Object otherConstant = ((ConstantTransformer<?, ?>) obj).getConstant();
+    return otherConstant == getConstant()
+        || otherConstant != null && otherConstant.equals(getConstant());
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof ConstantTransformer)) {
-            return false;
-        }
-        final Object otherConstant = ((ConstantTransformer<?, ?>) obj).getConstant();
-        return otherConstant == getConstant() || otherConstant != null && otherConstant.equals(getConstant());
+  /** {@inheritDoc} */
+  @Override
+  public int hashCode() {
+    int result = "ConstantTransformer".hashCode() << 2;
+    if (getConstant() != null) {
+      result |= getConstant().hashCode();
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        int result = "ConstantTransformer".hashCode() << 2;
-        if (getConstant() != null) {
-            result |= getConstant().hashCode();
-        }
-        return result;
-    }
+    return result;
+  }
 }
