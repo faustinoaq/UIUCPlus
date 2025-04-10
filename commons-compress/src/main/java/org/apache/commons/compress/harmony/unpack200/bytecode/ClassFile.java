a42082c0807651c722ef16c40798047a4424db2e
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:27:55 -0500

    Formatted main branch before codebert-base.commons-compress.mid-3555.idx-2881.1.mutant injection

--- commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/ClassFile.java
+++ commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/ClassFile.java
@@ -19,56 +19,55 @@ package org.apache.commons.compress.harmony.unpack200.bytecode;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-/**
- * ClassFile is used to represent and write out Java class files.
- */
+/** ClassFile is used to represent and write out Java class files. */
 public class ClassFile {
 
-    private static final int MAGIC = 0xCAFEBABE;
+  private static final int MAGIC = 0xCAFEBABE;
 
-    public int major;
-    public int minor;
-    public ClassConstantPool pool = new ClassConstantPool();
-    public int accessFlags;
-    public int thisClass;
-    public int superClass;
-    public int[] interfaces;
-    public ClassFileEntry[] fields;
-    public ClassFileEntry[] methods;
-    public Attribute[] attributes;
+  public int major;
+  public int minor;
+  public ClassConstantPool pool = new ClassConstantPool();
+  public int accessFlags;
+  public int thisClass;
+  public int superClass;
+  public int[] interfaces;
+  public ClassFileEntry[] fields;
+  public ClassFileEntry[] methods;
+  public Attribute[] attributes;
 
-    public void write(final DataOutputStream dos) throws IOException {
-        dos.writeInt(MAGIC);
-        dos.writeShort(minor);
-        dos.writeShort(major);
-        dos.writeShort(pool.size() + 1);
-        for (int i = 1; i <= pool.size(); i++) {
-            ConstantPoolEntry entry;
-            (entry = (ConstantPoolEntry) pool.get(i)).doWrite(dos);
-            // Doubles and longs take up two spaces in the pool, but only one
-            // gets written
-            if (entry.getTag() == ConstantPoolEntry.CP_Double || entry.getTag() == ConstantPoolEntry.CP_Long) {
-                i++;
-            }
-        }
-        dos.writeShort(accessFlags);
-        dos.writeShort(thisClass);
-        dos.writeShort(superClass);
-        dos.writeShort(interfaces.length);
-        for (final int element : interfaces) {
-            dos.writeShort(element);
-        }
-        dos.writeShort(fields.length);
-        for (final ClassFileEntry field : fields) {
-            field.write(dos);
-        }
-        dos.writeShort(methods.length);
-        for (final ClassFileEntry method : methods) {
-            method.write(dos);
-        }
-        dos.writeShort(attributes.length);
-        for (final Attribute attribute : attributes) {
-            attribute.write(dos);
-        }
+  public void write(final DataOutputStream dos) throws IOException {
+    dos.writeInt(MAGIC);
+    dos.writeShort(minor);
+    dos.writeShort(major);
+    dos.writeShort(pool.size() + 1);
+    for (int i = 1; i <= pool.size(); i++) {
+      ConstantPoolEntry entry;
+      (entry = (ConstantPoolEntry) pool.get(i)).doWrite(dos);
+      // Doubles and longs take up two spaces in the pool, but only one
+      // gets written
+      if (entry.getTag() == ConstantPoolEntry.CP_Double
+          || entry.getTag() == ConstantPoolEntry.CP_Long) {
+        i++;
+      }
     }
+    dos.writeShort(accessFlags);
+    dos.writeShort(thisClass);
+    dos.writeShort(superClass);
+    dos.writeShort(interfaces.length);
+    for (final int element : interfaces) {
+      dos.writeShort(element);
+    }
+    dos.writeShort(fields.length);
+    for (final ClassFileEntry field : fields) {
+      field.write(dos);
+    }
+    dos.writeShort(methods.length);
+    for (final ClassFileEntry method : methods) {
+      method.write(dos);
+    }
+    dos.writeShort(attributes.length);
+    for (final Attribute attribute : attributes) {
+      attribute.write(dos);
+    }
+  }
 }
