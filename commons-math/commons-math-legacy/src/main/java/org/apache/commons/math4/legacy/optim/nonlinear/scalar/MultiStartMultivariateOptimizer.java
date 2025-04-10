f10e048975e59b7feb9ec7a629d9fde19ddcd81a
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:19:15 -0500

    Formatted main branch before mubert-base.commons-math.mid-1986.idx-1987.8.mutant injection

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/optim/nonlinear/scalar/MultiStartMultivariateOptimizer.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/optim/nonlinear/scalar/MultiStartMultivariateOptimizer.java
@@ -21,89 +21,81 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Supplier;
-
 import org.apache.commons.math4.legacy.optim.BaseMultiStartMultivariateOptimizer;
 import org.apache.commons.math4.legacy.optim.PointValuePair;
 
 /**
  * Multi-start optimizer.
  *
- * This class wraps an optimizer in order to use it several times in
- * turn with different starting points (trying to avoid being trapped
- * in a local extremum when looking for a global one).
+ * <p>This class wraps an optimizer in order to use it several times in turn with different starting
+ * points (trying to avoid being trapped in a local extremum when looking for a global one).
  *
  * @since 3.0
  */
 public class MultiStartMultivariateOptimizer
     extends BaseMultiStartMultivariateOptimizer<PointValuePair> {
-    /** Underlying optimizer. */
-    private final MultivariateOptimizer optimizer;
-    /** Found optima. */
-    private final List<PointValuePair> optima = new ArrayList<>();
+  /** Underlying optimizer. */
+  private final MultivariateOptimizer optimizer;
 
-    /**
-     * Create a multi-start optimizer from a single-start optimizer.
-     *
-     * @param optimizer Single-start optimizer to wrap.
-     * @param starts Number of starts to perform.
-     * If {@code starts == 1}, the result will be same as if {@code optimizer}
-     * is called directly.
-     * @param generator Generator to use for restarts.
-     * @throws org.apache.commons.math4.legacy.exception.NullArgumentException NullArgumentException if {@code optimizer} or {@code generator}
-     * is {@code null}.
-     * @throws org.apache.commons.math4.legacy.exception.NotStrictlyPositiveException NotStrictlyPositiveException if {@code starts < 1}.
-     */
-    public MultiStartMultivariateOptimizer(final MultivariateOptimizer optimizer,
-                                           final int starts,
-                                           final Supplier<double[]> generator) {
-        super(optimizer, starts, generator);
-        this.optimizer = optimizer;
-    }
+  /** Found optima. */
+  private final List<PointValuePair> optima = new ArrayList<>();
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public PointValuePair[] getOptima() {
-        Collections.sort(optima, getPairComparator());
-        return optima.toArray(new PointValuePair[0]);
-    }
+  /**
+   * Create a multi-start optimizer from a single-start optimizer.
+   *
+   * @param optimizer Single-start optimizer to wrap.
+   * @param starts Number of starts to perform. If {@code starts == 1}, the result will be same as
+   *     if {@code optimizer} is called directly.
+   * @param generator Generator to use for restarts.
+   * @throws org.apache.commons.math4.legacy.exception.NullArgumentException NullArgumentException
+   *     if {@code optimizer} or {@code generator} is {@code null}.
+   * @throws org.apache.commons.math4.legacy.exception.NotStrictlyPositiveException
+   *     NotStrictlyPositiveException if {@code starts < 1}.
+   */
+  public MultiStartMultivariateOptimizer(
+      final MultivariateOptimizer optimizer, final int starts, final Supplier<double[]> generator) {
+    super(optimizer, starts, generator);
+    this.optimizer = optimizer;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void store(PointValuePair optimum) {
-        optima.add(optimum);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public PointValuePair[] getOptima() {
+    Collections.sort(optima, getPairComparator());
+    return optima.toArray(new PointValuePair[0]);
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void clear() {
-        optima.clear();
-    }
+  /** {@inheritDoc} */
+  @Override
+  protected void store(PointValuePair optimum) {
+    optima.add(optimum);
+  }
 
-    /**
-     * @return a comparator for sorting the optima.
-     */
-    private Comparator<PointValuePair> getPairComparator() {
-        return new Comparator<PointValuePair>() {
-            /** {@inheritDoc} */
-            @Override
-            public int compare(final PointValuePair o1,
-                               final PointValuePair o2) {
-                if (o1 == null) {
-                    return (o2 == null) ? 0 : 1;
-                } else if (o2 == null) {
-                    return -1;
-                }
-                final double v1 = o1.getValue();
-                final double v2 = o2.getValue();
-                return (optimizer.getGoalType() == GoalType.MINIMIZE) ?
-                    Double.compare(v1, v2) : Double.compare(v2, v1);
-            }
-        };
-    }
+  /** {@inheritDoc} */
+  @Override
+  protected void clear() {
+    optima.clear();
+  }
+
+  /**
+   * @return a comparator for sorting the optima.
+   */
+  private Comparator<PointValuePair> getPairComparator() {
+    return new Comparator<PointValuePair>() {
+      /** {@inheritDoc} */
+      @Override
+      public int compare(final PointValuePair o1, final PointValuePair o2) {
+        if (o1 == null) {
+          return (o2 == null) ? 0 : 1;
+        } else if (o2 == null) {
+          return -1;
+        }
+        final double v1 = o1.getValue();
+        final double v2 = o2.getValue();
+        return (optimizer.getGoalType() == GoalType.MINIMIZE)
+            ? Double.compare(v1, v2)
+            : Double.compare(v2, v1);
+      }
+    };
+  }
 }
