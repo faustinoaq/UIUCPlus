da7cd0e84510e695ee4d18fcfdd7ec1879d15789
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:18:29 -0500

    Formatted main branch before mubert-base.commons-compress.mid-903.idx-904.21.mutant injection

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/sevenz/AES256Options.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/sevenz/AES256Options.java
@@ -20,7 +20,6 @@ package org.apache.commons.compress.archivers.sevenz;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
@@ -34,76 +33,82 @@ import javax.crypto.spec.SecretKeySpec;
  */
 class AES256Options {
 
-    static final String ALGORITHM = "AES";
+  static final String ALGORITHM = "AES";
 
-    static final String TRANSFORMATION = "AES/CBC/NoPadding";
+  static final String TRANSFORMATION = "AES/CBC/NoPadding";
 
-    static SecretKeySpec newSecretKeySpec(final byte[] bytes) {
-        return new SecretKeySpec(bytes, ALGORITHM);
+  static SecretKeySpec newSecretKeySpec(final byte[] bytes) {
+    return new SecretKeySpec(bytes, ALGORITHM);
+  }
+
+  private static byte[] randomBytes(final int size) {
+    final byte[] bytes = new byte[size];
+    try {
+      SecureRandom.getInstanceStrong().nextBytes(bytes);
+    } catch (final NoSuchAlgorithmException e) {
+      throw new IllegalStateException(
+          "No strong secure random available to generate strong AES key", e);
     }
-    private static byte[] randomBytes(final int size) {
-        final byte[] bytes = new byte[size];
-        try {
-            SecureRandom.getInstanceStrong().nextBytes(bytes);
-        } catch (final NoSuchAlgorithmException e) {
-            throw new IllegalStateException("No strong secure random available to generate strong AES key", e);
-        }
-        return bytes;
+    return bytes;
+  }
+
+  private final byte[] salt;
+  private final byte[] iv;
+
+  private final int numCyclesPower;
+
+  private final Cipher cipher;
+
+  /**
+   * @param password password used for encryption
+   */
+  public AES256Options(final char[] password) {
+    this(password, new byte[0], randomBytes(16), 19);
+  }
+
+  /**
+   * @param password password used for encryption
+   * @param salt for password hash salting (enforce password security)
+   * @param iv Initialization Vector (IV) used by cipher algorithm
+   * @param numCyclesPower another password security enforcer parameter that controls the cycles of
+   *     password hashing. More the this number is high, more security you'll have but also high CPU
+   *     usage
+   */
+  public AES256Options(
+      final char[] password, final byte[] salt, final byte[] iv, final int numCyclesPower) {
+    this.salt = salt;
+    this.iv = iv;
+    this.numCyclesPower = numCyclesPower;
+
+    // NOTE: for security purposes, password is wrapped in a Cipher as soon as possible to not stay
+    // in memory
+    final byte[] aesKeyBytes = AES256SHA256Decoder.sha256Password(password, numCyclesPower, salt);
+    final SecretKey aesKey = newSecretKeySpec(aesKeyBytes);
+
+    try {
+      cipher = Cipher.getInstance(TRANSFORMATION);
+      cipher.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(iv));
+    } catch (final GeneralSecurityException generalSecurityException) {
+      throw new IllegalStateException(
+          "Encryption error (do you have the JCE Unlimited Strength Jurisdiction Policy Files"
+              + " installed?)",
+          generalSecurityException);
     }
-    private final byte[] salt;
-    private final byte[] iv;
+  }
 
-    private final int numCyclesPower;
+  Cipher getCipher() {
+    return cipher;
+  }
 
-    private final Cipher cipher;
+  byte[] getIv() {
+    return iv;
+  }
 
-    /**
-     * @param password password used for encryption
-     */
-    public AES256Options(final char[] password) {
-        this(password, new byte[0], randomBytes(16), 19);
-    }
+  int getNumCyclesPower() {
+    return numCyclesPower;
+  }
 
-    /**
-     * @param password password used for encryption
-     * @param salt for password hash salting (enforce password security)
-     * @param iv Initialization Vector (IV) used by cipher algorithm
-     * @param numCyclesPower another password security enforcer parameter that controls the cycles of password hashing. More the
-     *                       this number is high, more security you'll have but also high CPU usage
-     */
-    public AES256Options(final char[] password, final byte[] salt, final byte[] iv, final int numCyclesPower) {
-        this.salt = salt;
-        this.iv = iv;
-        this.numCyclesPower = numCyclesPower;
-
-        // NOTE: for security purposes, password is wrapped in a Cipher as soon as possible to not stay in memory
-        final byte[] aesKeyBytes = AES256SHA256Decoder.sha256Password(password, numCyclesPower, salt);
-        final SecretKey aesKey = newSecretKeySpec(aesKeyBytes);
-
-        try {
-            cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(iv));
-        } catch (final GeneralSecurityException generalSecurityException) {
-            throw new IllegalStateException(
-                "Encryption error (do you have the JCE Unlimited Strength Jurisdiction Policy Files installed?)",
-                generalSecurityException
-            );
-        }
-    }
-
-    Cipher getCipher() {
-        return cipher;
-    }
-
-    byte[] getIv() {
-        return iv;
-    }
-
-    int getNumCyclesPower() {
-        return numCyclesPower;
-    }
-
-    byte[] getSalt() {
-        return salt;
-    }
+  byte[] getSalt() {
+    return salt;
+  }
 }
