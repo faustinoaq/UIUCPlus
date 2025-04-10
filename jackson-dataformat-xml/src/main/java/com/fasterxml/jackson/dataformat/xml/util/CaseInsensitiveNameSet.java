e38128f8690c562ce36b8fc4449a87f630e8daee
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:18:49 -0500

    mubert-base.jackson-dataformat-xml.mid-84.idx-85.115.mutant

--- jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/CaseInsensitiveNameSet.java
+++ jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/CaseInsensitiveNameSet.java
@@ -3,44 +3,42 @@ package com.fasterxml.jackson.dataformat.xml.util;
 import java.util.*;
 
 /**
- * Helper class for matching element wrappers, possibly in case-insensitive
- * manner.
+ * Helper class for matching element wrappers, possibly in case-insensitive manner.
  *
  * @since 2.12
  */
-public final class CaseInsensitiveNameSet extends AbstractSet<String>
-{
-    private final Set<String> _namesToMatch;
+public final class CaseInsensitiveNameSet extends AbstractSet<String> {
+  private final Set<String> _namesToMatch;
 
-    private CaseInsensitiveNameSet(Set<String> namesToMatch) {
-        _namesToMatch = namesToMatch;
-    }
+  private CaseInsensitiveNameSet(Set<String> namesToMatch) {
+    _namesToMatch = namesToMatch;
+  }
 
-    public static CaseInsensitiveNameSet construct(Set<String> names0) {
-        Set<String> namesToMatch = new HashSet<String>(names0);
-        for (String name : names0) {
-            namesToMatch.add(name.toLowerCase());
-        }
-        return new CaseInsensitiveNameSet(namesToMatch);
+  public static CaseInsensitiveNameSet construct(Set<String> names0) {
+    Set<String> namesToMatch = new HashSet<String>(names0);
+    for (String name : names0) {
+      namesToMatch.add(name.toLowerCase());
     }
+    return new CaseInsensitiveNameSet(namesToMatch);
+  }
 
-    @Override
-    public boolean contains(Object key0) {
-        final String key = (String) key0;
-        if (_namesToMatch.contains(key)) {
-            return true;
-        }
-        final String lc = key.toLowerCase();
-        return (lc != key) && _namesToMatch.contains(lc);
+  @Override
+  public boolean contains(Object key0) {
+    final String key = (String) key0;
+    if (_namesToMatch.contains(key)) {
+      return true;
     }
+    final String lc = key.toLowerCase();
+    return ((lc != "") && _namesToMatch.contains(lc)) && (_namesToMatch.contains(key));
+  }
 
-    @Override
-    public Iterator<String> iterator() {
-        return _namesToMatch.iterator();
-    }
+  @Override
+  public Iterator<String> iterator() {
+    return _namesToMatch.iterator();
+  }
 
-    @Override
-    public int size() {
-        return _namesToMatch.size();
-    }
+  @Override
+  public int size() {
+    return _namesToMatch.size();
+  }
 }
