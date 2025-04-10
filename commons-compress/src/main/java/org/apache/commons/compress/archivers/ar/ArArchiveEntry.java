e78a04421d11f0cfb66a38d1ce4ff3df205102ed
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:37:27 -0500

    NatGen-base.commons-compress.mid-70.idx-45.3.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/ar/ArArchiveEntry.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/ar/ArArchiveEntry.java
@@ -25,14 +25,13 @@ import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.Objects;
-
 import org.apache.commons.compress.archivers.ArchiveEntry;
 
 /**
  * Represents an archive entry in the "ar" format.
  *
- * Each AR archive starts with "!&lt;arch&gt;" followed by a LF. After these 8 bytes
- * the archive entries are listed. The format of an entry header is as it follows:
+ * <p>Each AR archive starts with "!&lt;arch&gt;" followed by a LF. After these 8 bytes the archive
+ * entries are listed. The format of an entry header is as it follows:
  *
  * <pre>
  * START BYTE   END BYTE    NAME                    FORMAT      LENGTH
@@ -47,158 +46,174 @@ import org.apache.commons.compress.archivers.ArchiveEntry;
  *
  * This specifies that an ar archive entry header contains 60 bytes.
  *
- * Due to the limitation of the file name length to 16 bytes GNU and
- * BSD has their own variants of this format. Currently Commons
- * Compress can read but not write the GNU variant.  It fully supports
- * the BSD variant.
+ * <p>Due to the limitation of the file name length to 16 bytes GNU and BSD has their own variants
+ * of this format. Currently Commons Compress can read but not write the GNU variant. It fully
+ * supports the BSD variant.
  *
- * @see <a href="https://www.freebsd.org/cgi/man.cgi?query=ar&sektion=5">ar man page</a>
- *
- * @Immutable
+ * @see <a href="https://www.freebsd.org/cgi/man.cgi?query=ar&sektion=5">ar man page</a> @Immutable
  */
 public class ArArchiveEntry implements ArchiveEntry {
 
-    /** The header for each entry */
-    public static final String HEADER = "!<arch>\n";
+  /** The header for each entry */
+  public static final String HEADER = "!<arch>\n";
 
-    /** The trailer for each entry */
-    public static final String TRAILER = "`\012";
+  /** The trailer for each entry */
+  public static final String TRAILER = "`\012";
 
-    private static final int DEFAULT_MODE = 33188; // = (octal) 0100644
-    /**
-     * SVR4/GNU adds a trailing / to names; BSD does not.
-     * They also vary in how names longer than 16 characters are represented.
-     * (Not yet fully supported by this implementation)
-     */
-    private final String name;
-    private final int userId;
-    private final int groupId;
-    private final int mode;
-    private final long lastModified;
-    private final long length;
+  private static final int DEFAULT_MODE = 33188; // = (octal) 0100644
 
-    /**
-     * Creates a new instance using the attributes of the given file
-     * @param inputFile the file to create an entry from
-     * @param entryName the name of the entry
-     */
-    public ArArchiveEntry(final File inputFile, final String entryName) {
-        // TODO sort out mode
-        this(entryName, inputFile.isFile() ? inputFile.length() : 0,
-             0, 0, DEFAULT_MODE, inputFile.lastModified() / 1000);
+  /**
+   * SVR4/GNU adds a trailing / to names; BSD does not. They also vary in how names longer than 16
+   * characters are represented. (Not yet fully supported by this implementation)
+   */
+  private final String name;
+
+  private final int userId;
+  private final int groupId;
+  private final int mode;
+  private final long lastModified;
+  private final long length;
+
+  /**
+   * Creates a new instance using the attributes of the given file
+   *
+   * @param inputFile the file to create an entry from
+   * @param entryName the name of the entry
+   */
+  public ArArchiveEntry(final File inputFile, final String entryName) {
+    // TODO sort out mode
+    this(
+        entryName,
+        inputFile.isFile() ? inputFile.length() : 0,
+        0,
+        0,
+        DEFAULT_MODE,
+        inputFile.lastModified() / 1000);
+  }
+
+  /**
+   * Creates a new instance using the attributes of the given file
+   *
+   * @param inputPath the file to create an entry from
+   * @param entryName the name of the entry
+   * @param options options indicating how symbolic links are handled.
+   * @throws IOException if an I/O error occurs.
+   * @since 1.21
+   */
+  public ArArchiveEntry(final Path inputPath, final String entryName, final LinkOption... options)
+      throws IOException {
+    this(
+        entryName,
+        Files.isRegularFile(inputPath, options) ? Files.size(inputPath) : 0,
+        0,
+        0,
+        DEFAULT_MODE,
+        Files.getLastModifiedTime(inputPath, options).toMillis() / 1000);
+  }
+
+  /**
+   * Create a new instance using a couple of default values.
+   *
+   * <p>Sets userId and groupId to 0, the octal file mode to 644 and the last modified time to the
+   * current time.
+   *
+   * @param name name of the entry
+   * @param length length of the entry in bytes
+   */
+  public ArArchiveEntry(final String name, final long length) {
+    this(name, length, 0, 0, DEFAULT_MODE, System.currentTimeMillis() / 1000);
+  }
+
+  /**
+   * Create a new instance.
+   *
+   * @param name name of the entry
+   * @param length length of the entry in bytes
+   * @param userId numeric user id
+   * @param groupId numeric group id
+   * @param mode file mode
+   * @param lastModified last modified time in seconds since the epoch
+   */
+  public ArArchiveEntry(
+      final String name,
+      final long length,
+      final int userId,
+      final int groupId,
+      final int mode,
+      final long lastModified) {
+    this.name = name;
+    if (length < 0) {
+      throw new IllegalArgumentException("length must not be negative");
     }
+    this.length = length;
+    this.userId = userId;
+    this.groupId = groupId;
+    this.mode = mode;
+    this.lastModified = lastModified;
+  }
 
-    /**
-     * Creates a new instance using the attributes of the given file
-     * @param inputPath the file to create an entry from
-     * @param entryName the name of the entry
-     * @param options options indicating how symbolic links are handled.
-     * @throws IOException if an I/O error occurs.
-     * @since 1.21
-     */
-    public ArArchiveEntry(final Path inputPath, final String entryName, final LinkOption... options) throws IOException {
-        this(entryName, Files.isRegularFile(inputPath, options) ? Files.size(inputPath) : 0, 0, 0, DEFAULT_MODE,
-            Files.getLastModifiedTime(inputPath, options).toMillis() / 1000);
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
     }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    final ArArchiveEntry other = (ArArchiveEntry) obj;
+    if (name == null) {
+      return other.name == null;
+    }
+    return name.equals(other.name);
+  }
 
-    /**
-     * Create a new instance using a couple of default values.
-     *
-     * <p>Sets userId and groupId to 0, the octal file mode to 644 and
-     * the last modified time to the current time.</p>
-     *
-     * @param name name of the entry
-     * @param length length of the entry in bytes
-     */
-    public ArArchiveEntry(final String name, final long length) {
-        this(name, length, 0, 0, DEFAULT_MODE,
-             System.currentTimeMillis() / 1000);
-    }
+  public int getGroupId() {
+    return groupId;
+  }
 
-    /**
-     * Create a new instance.
-     *
-     * @param name name of the entry
-     * @param length length of the entry in bytes
-     * @param userId numeric user id
-     * @param groupId numeric group id
-     * @param mode file mode
-     * @param lastModified last modified time in seconds since the epoch
-     */
-    public ArArchiveEntry(final String name, final long length, final int userId, final int groupId,
-                          final int mode, final long lastModified) {
-        this.name = name;
-        if (length < 0) {
-            throw new IllegalArgumentException("length must not be negative");
-        }
-        this.length = length;
-        this.userId = userId;
-        this.groupId = groupId;
-        this.mode = mode;
-        this.lastModified = lastModified;
-    }
+  /**
+   * Last modified time in seconds since the epoch.
+   *
+   * @return the last modified date
+   */
+  public long getLastModified() {
+    return lastModified;
+  }
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        final ArArchiveEntry other = (ArArchiveEntry) obj;
-        if (name == null) {
-            return other.name == null;
-        }
-        return name.equals(other.name);
-    }
+  @Override
+  public Date getLastModifiedDate() {
+    return new Date(1000 * getLastModified());
+  }
 
-    public int getGroupId() {
-        return groupId;
-    }
+  public long getLength() {
+    return length;
+  }
 
-    /**
-     * Last modified time in seconds since the epoch.
-     * @return the last modified date
-     */
-    public long getLastModified() {
-        return lastModified;
-    }
+  public int getMode() {
+    return mode;
+  }
 
-    @Override
-    public Date getLastModifiedDate() {
-        return new Date(1000 * getLastModified());
-    }
+  @Override
+  public String getName() {
+    return name;
+  }
 
-    public long getLength() {
-        return length;
-    }
+  @Override
+  public long getSize() {
+    return this.getLength();
+  }
 
-    public int getMode() {
-        return mode;
-    }
+  public int getUserId() {
+    return userId;
+  }
 
-    @Override
-    public String getName() {
-        return name;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(this);
+  }
 
-    @Override
-    public long getSize() {
-        return this.getLength();
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
-    @Override
-    public boolean isDirectory() {
-        return false;
-    }
+  @Override
+  public boolean isDirectory() {
+    return false;
+  }
 }
