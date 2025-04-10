6dd5bfab2ce8843d3667bcb1d2ae1f32b86592dd
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:36:17 -0500

    Formatted main branch before NatGen-base.jackson-databind.mid-7463.idx-5615.3.mutant injection

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/Named.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/Named.java
@@ -1,8 +1,6 @@
 package com.fasterxml.jackson.databind.util;
 
-/**
- * Simple tag interface mostly to allow sorting by name.
- */
+/** Simple tag interface mostly to allow sorting by name. */
 public interface Named {
-    public String getName();
+  public String getName();
 }
