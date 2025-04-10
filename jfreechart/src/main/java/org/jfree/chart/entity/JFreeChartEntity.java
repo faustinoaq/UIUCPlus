a866733f57cbfaf7e70a3cae626eb5204669e273
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:10 -0500

    Formatted main branch before mubert-base.jfreechart.mid-842.idx-843.906.mutant injection

--- jfreechart/src/main/java/org/jfree/chart/entity/JFreeChartEntity.java
+++ jfreechart/src/main/java/org/jfree/chart/entity/JFreeChartEntity.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * ---------------------
@@ -41,167 +41,155 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Objects;
-
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.internal.HashUtils;
 import org.jfree.chart.internal.Args;
+import org.jfree.chart.internal.HashUtils;
 import org.jfree.chart.internal.SerialUtils;
 
-/**
- * A class that captures information about an entire chart.
- */
+/** A class that captures information about an entire chart. */
 public class JFreeChartEntity extends ChartEntity {
 
-    /** For serialization. */
-    private static final long serialVersionUID = -4445994133561919083L;
-            //same as for ChartEntity!
+  /** For serialization. */
+  private static final long serialVersionUID = -4445994133561919083L;
 
-    /** The chart. */
-    private final JFreeChart chart;
+  // same as for ChartEntity!
 
-    /**
-     * Creates a new chart entity.
-     *
-     * @param area  the area ({@code null} not permitted).
-     * @param chart  the chart ({@code null} not permitted).
-     */
-    public JFreeChartEntity(Shape area, JFreeChart chart) {
-        // defer argument checks...
-        this(area, chart, null);
+  /** The chart. */
+  private final JFreeChart chart;
+
+  /**
+   * Creates a new chart entity.
+   *
+   * @param area the area ({@code null} not permitted).
+   * @param chart the chart ({@code null} not permitted).
+   */
+  public JFreeChartEntity(Shape area, JFreeChart chart) {
+    // defer argument checks...
+    this(area, chart, null);
+  }
+
+  /**
+   * Creates a new chart entity.
+   *
+   * @param area the area ({@code null} not permitted).
+   * @param chart the chart ({@code null} not permitted).
+   * @param toolTipText the tool tip text ({@code null} permitted).
+   */
+  public JFreeChartEntity(Shape area, JFreeChart chart, String toolTipText) {
+    // defer argument checks...
+    this(area, chart, toolTipText, null);
+  }
+
+  /**
+   * Creates a new chart entity.
+   *
+   * @param area the area ({@code null} not permitted).
+   * @param chart the chart ({@code null} not permitted).
+   * @param toolTipText the tool tip text ({@code null} permitted).
+   * @param urlText the URL text for HTML image maps ({@code null} permitted).
+   */
+  public JFreeChartEntity(Shape area, JFreeChart chart, String toolTipText, String urlText) {
+    super(area, toolTipText, urlText);
+    Args.nullNotPermitted(chart, "chart");
+    this.chart = chart;
+  }
+
+  /**
+   * Returns the chart that occupies the entity area.
+   *
+   * @return The chart (never {@code null}).
+   */
+  public JFreeChart getChart() {
+    return this.chart;
+  }
+
+  /**
+   * Returns a string representation of the chart entity, useful for debugging.
+   *
+   * @return A string.
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("JFreeChartEntity: ");
+    sb.append("tooltip = ");
+    sb.append(getToolTipText());
+    return sb.toString();
+  }
+
+  /**
+   * Tests the entity for equality with an arbitrary object.
+   *
+   * @param obj the object to test against ({@code null} permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
-
-    /**
-     * Creates a new chart entity.
-     *
-     * @param area  the area ({@code null} not permitted).
-     * @param chart  the chart ({@code null} not permitted).
-     * @param toolTipText  the tool tip text ({@code null} permitted).
-     */
-    public JFreeChartEntity(Shape area, JFreeChart chart, String toolTipText) {
-        // defer argument checks...
-        this(area, chart, toolTipText, null);
+    if (!(obj instanceof JFreeChartEntity)) {
+      return false;
     }
-
-    /**
-     * Creates a new chart entity.
-     *
-     * @param area  the area ({@code null} not permitted).
-     * @param chart  the chart ({@code null} not permitted).
-     * @param toolTipText  the tool tip text ({@code null} permitted).
-     * @param urlText  the URL text for HTML image maps ({@code null}
-     *                 permitted).
-     */
-    public JFreeChartEntity(Shape area, JFreeChart chart, String toolTipText,
-            String urlText) {
-        super(area, toolTipText, urlText);
-        Args.nullNotPermitted(chart, "chart");
-        this.chart = chart;
+    JFreeChartEntity that = (JFreeChartEntity) obj;
+    if (!getArea().equals(that.getArea())) {
+      return false;
     }
-
-    /**
-     * Returns the chart that occupies the entity area.
-     *
-     * @return The chart (never {@code null}).
-     */
-    public JFreeChart getChart() {
-        return this.chart;
+    if (!Objects.equals(getToolTipText(), that.getToolTipText())) {
+      return false;
     }
-
-    /**
-     * Returns a string representation of the chart entity, useful for
-     * debugging.
-     *
-     * @return A string.
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("JFreeChartEntity: ");
-        sb.append("tooltip = ");
-        sb.append(getToolTipText());
-        return sb.toString();
+    if (!Objects.equals(getURLText(), that.getURLText())) {
+      return false;
     }
-
-    /**
-     * Tests the entity for equality with an arbitrary object.
-     *
-     * @param obj  the object to test against ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof JFreeChartEntity)) {
-            return false;
-        }
-        JFreeChartEntity that = (JFreeChartEntity) obj;
-        if (!getArea().equals(that.getArea())) {
-            return false;
-        }
-        if (!Objects.equals(getToolTipText(), that.getToolTipText())) {
-            return false;
-        }
-        if (!Objects.equals(getURLText(), that.getURLText())) {
-            return false;
-        }
-        if (!(this.chart.equals(that.chart))) {
-            return false;
-        }
-        return true;
+    if (!(this.chart.equals(that.chart))) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int result = 39;
-        result = HashUtils.hashCode(result, getToolTipText());
-        result = HashUtils.hashCode(result, getURLText());
-        return result;
-    }
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int result = 39;
+    result = HashUtils.hashCode(result, getToolTipText());
+    result = HashUtils.hashCode(result, getURLText());
+    return result;
+  }
 
-    /**
-     * Returns a clone of the entity.
-     *
-     * @return A clone.
-     *
-     * @throws CloneNotSupportedException if there is a problem cloning the
-     *         entity.
-     */
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
+  /**
+   * Returns a clone of the entity.
+   *
+   * @return A clone.
+   * @throws CloneNotSupportedException if there is a problem cloning the entity.
+   */
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
 
-    /**
-     * Provides serialization support.
-     *
-     * @param stream  the output stream.
-     *
-     * @throws IOException  if there is an I/O error.
-     */
-    private void writeObject(ObjectOutputStream stream) throws IOException {
-        stream.defaultWriteObject();
-        SerialUtils.writeShape(getArea(), stream);
-     }
+  /**
+   * Provides serialization support.
+   *
+   * @param stream the output stream.
+   * @throws IOException if there is an I/O error.
+   */
+  private void writeObject(ObjectOutputStream stream) throws IOException {
+    stream.defaultWriteObject();
+    SerialUtils.writeShape(getArea(), stream);
+  }
 
-    /**
-     * Provides serialization support.
-     *
-     * @param stream  the input stream.
-     *
-     * @throws IOException  if there is an I/O error.
-     * @throws ClassNotFoundException  if there is a classpath problem.
-     */
-    private void readObject(ObjectInputStream stream)
-        throws IOException, ClassNotFoundException {
-        stream.defaultReadObject();
-        setArea(SerialUtils.readShape(stream));
-    }
-
+  /**
+   * Provides serialization support.
+   *
+   * @param stream the input stream.
+   * @throws IOException if there is an I/O error.
+   * @throws ClassNotFoundException if there is a classpath problem.
+   */
+  private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+    stream.defaultReadObject();
+    setArea(SerialUtils.readShape(stream));
+  }
 }
