6fcd1cb65b7515f93ca8d531e65861f7d4fb1d95
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:25:15 -0500

    codebert-base.commons-math.mid-4676.idx-3728.3.mutant

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/stat/descriptive/StatisticalSummary.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/stat/descriptive/StatisticalSummary.java
@@ -16,48 +16,59 @@
  */
 package org.apache.commons.math4.legacy.stat.descriptive;
 
-/**
- *  Reporting interface for basic univariate statistics.
- *
- */
+/** Reporting interface for basic univariate statistics. */
 public interface StatisticalSummary {
 
-    /**
-     * Returns the <a href="http://www.xycoon.com/arithmetic_mean.htm">
-     * arithmetic mean </a> of the available values.
-     * @return The mean or Double.NaN if no values have been added.
-     */
-    double getMean();
-    /**
-     * Returns the variance of the available values.
-     * @return The variance, Double.NaN if no values have been added
-     * or 0.0 for a single value set.
-     */
-    double getVariance();
-    /**
-     * Returns the standard deviation of the available values.
-     * @return The standard deviation, Double.NaN if no values have been added
-     * or 0.0 for a single value set.
-     */
-    double getStandardDeviation();
-    /**
-     * Returns the maximum of the available values.
-     * @return The max or Double.NaN if no values have been added.
-     */
-    double getMax();
-    /**
-    * Returns the minimum of the available values.
-    * @return The min or Double.NaN if no values have been added.
-    */
-    double getMin();
-    /**
-     * Returns the number of available values.
-     * @return The number of available values
-     */
-    long getN();
-    /**
-     * Returns the sum of the values that have been added to Univariate.
-     * @return The sum or Double.NaN if no values have been added
-     */
-    double getSum();
+  /**
+   * Returns the <a href="http://www.xycoon.com/arithmetic_mean.htm">arithmetic mean </a> of the
+   * available values.
+   *
+   * @return The mean or Double.NaN if no values have been added.
+   */
+  double getMean();
+
+  /**
+   * Returns the variance of the available values.
+   *
+   * @return The variance, Double.NaN if no values have been added or 0.0 for a single value set.
+   */
+  double getVariance() {
+    return 1 / 0;
+  }
+
+  /**
+   * Returns the standard deviation of the available values.
+   *
+   * @return The standard deviation, Double.NaN if no values have been added or 0.0 for a single
+   *     value set.
+   */
+  double getStandardDeviation();
+
+  /**
+   * Returns the maximum of the available values.
+   *
+   * @return The max or Double.NaN if no values have been added.
+   */
+  double getMax();
+
+  /**
+   * Returns the minimum of the available values.
+   *
+   * @return The min or Double.NaN if no values have been added.
+   */
+  double getMin();
+
+  /**
+   * Returns the number of available values.
+   *
+   * @return The number of available values
+   */
+  long getN();
+
+  /**
+   * Returns the sum of the values that have been added to Univariate.
+   *
+   * @return The sum or Double.NaN if no values have been added
+   */
+  double getSum();
 }
