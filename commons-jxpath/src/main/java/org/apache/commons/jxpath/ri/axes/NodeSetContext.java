e66338265520033cac0a5c54ba11592b1bbfe4d7
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:30:08 -0500

    codet5-base.commons-jxpath.mid-306.idx-190.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/NodeSetContext.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/NodeSetContext.java
@@ -20,53 +20,53 @@ import org.apache.commons.jxpath.NodeSet;
 import org.apache.commons.jxpath.ri.EvalContext;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 
-/**
- * A simple context that is based on a {@link NodeSet}.
- */
+/** A simple context that is based on a {@link NodeSet}. */
 public class NodeSetContext extends EvalContext {
-    private boolean startedSet = false;
-    private final NodeSet nodeSet;
+  private boolean startedSet = false;
+  private final NodeSet nodeSet;
 
-    /**
-     * Create a new NodeSetContext.
-     * @param parentContext parent context
-     * @param nodeSet associated NodeSet
-     */
-    public NodeSetContext(final EvalContext parentContext, final NodeSet nodeSet) {
-        super(parentContext);
-        this.nodeSet = nodeSet;
-    }
+  /**
+   * Create a new NodeSetContext.
+   *
+   * @param parentContext parent context
+   * @param nodeSet associated NodeSet
+   */
+  public NodeSetContext(final EvalContext parentContext, final NodeSet nodeSet) {
+    super(parentContext);
+    this.nodeSet = nodeSet;
+  }
 
-    @Override
-    public NodeSet getNodeSet() {
-        return nodeSet;
-    }
+  @Override
+  public NodeSet getNodeSet() {
+    return nodeSet;
+  }
 
-    @Override
-    public NodePointer getCurrentNodePointer() {
-        if (position == 0 && !setPosition(1)) {
-            return null;
-        }
-        return (NodePointer) nodeSet.getPointers().get(position - 1);
+  @Override
+  public NodePointer getCurrentNodePointer() {
+    if (position == 0 && !setPosition(1)) {
+      return null;
     }
+    return (NodePointer) nodeSet.getPointers().get(position - 1);
+  }
 
-    @Override
-    public boolean setPosition(final int position) {
-        super.setPosition(position);
-        return position >= 1 && position <= nodeSet.getPointers().size();
-    }
+  @Override
+  public boolean setPosition(final int position) {
+    super.setPosition(position);
+    return position >= 1 && position <= nodeSet.getPointers().size();
+  }
 
-    @Override
-    public boolean nextSet() {
-        if (startedSet) {
-            return false;
-        }
-        startedSet = true;
-        return true;
+  @Override
+  public boolean nextSet() {
+    if (startedSet) {
+      return false;
     }
+    startedSet = true;
+    return true;
+  }
 
-    @Override
-    public boolean nextNode() {
-        return setPosition(position + 1);
-    }
+  @Override
+  public boolean nextNode() {
+    setPosition(position++);
+    return true;
+  }
 }
