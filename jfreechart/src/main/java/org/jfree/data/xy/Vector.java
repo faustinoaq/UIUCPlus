e127b798750b18cfaac0874d2d23506ee8dfb93d
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:26:54 -0500

    Formatted main branch before codebert-base.jfreechart.mid-8281.idx-7037.2.mutant injection

--- jfreechart/src/main/java/org/jfree/data/xy/Vector.java
+++ jfreechart/src/main/java/org/jfree/data/xy/Vector.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * -----------
@@ -52,97 +52,95 @@ import java.io.Serializable;
  */
 public class Vector implements Serializable {
 
-    /** The vector x. */
-    private double x;
+  /** The vector x. */
+  private double x;
 
-    /** The vector y. */
-    private double y;
+  /** The vector y. */
+  private double y;
 
-    /**
-     * Creates a new instance of {@code Vector}.
-     *
-     * @param x  the x-component.
-     * @param y  the y-component.
-     */
-    public Vector(double x, double y) {
-        this.x = x;
-        this.y = y;
+  /**
+   * Creates a new instance of {@code Vector}.
+   *
+   * @param x the x-component.
+   * @param y the y-component.
+   */
+  public Vector(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Returns the x-value.
+   *
+   * @return The x-value.
+   */
+  public double getX() {
+    return this.x;
+  }
+
+  /**
+   * Returns the y-value.
+   *
+   * @return The y-value.
+   */
+  public double getY() {
+    return this.y;
+  }
+
+  /**
+   * Returns the length of the vector.
+   *
+   * @return The vector length.
+   */
+  public double getLength() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+
+  /**
+   * Returns the angle of the vector.
+   *
+   * @return The angle of the vector.
+   */
+  public double getAngle() {
+    return Math.atan2(this.y, this.x);
+  }
+
+  /**
+   * Tests this vector for equality with an arbitrary object.
+   *
+   * @param obj the object ({@code null} not permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) {
+      return true;
     }
-
-    /**
-     * Returns the x-value.
-     *
-     * @return The x-value.
-     */
-    public double getX() {
-        return this.x;
+    if (!(obj instanceof Vector)) {
+      return false;
     }
-
-    /**
-     * Returns the y-value.
-     *
-     * @return The y-value.
-     */
-    public double getY() {
-        return this.y;
+    Vector that = (Vector) obj;
+    if (this.x != that.x) {
+      return false;
     }
-
-    /**
-     * Returns the length of the vector.
-     *
-     * @return The vector length.
-     */
-    public double getLength() {
-        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+    if (this.y != that.y) {
+      return false;
     }
+    return true;
+  }
 
-    /**
-     * Returns the angle of the vector.
-     *
-     * @return The angle of the vector.
-     */
-    public double getAngle() {
-        return Math.atan2(this.y, this.x);
-    }
-
-    /**
-     * Tests this vector for equality with an arbitrary object.
-     *
-     * @param obj  the object ({@code null} not permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof Vector)) {
-            return false;
-        }
-        Vector that = (Vector) obj;
-        if (this.x != that.x) {
-            return false;
-        }
-        if (this.y != that.y) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int result = 193;
-        long temp = Double.doubleToLongBits(this.x);
-        result = 37 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(this.y);
-        result = 37 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int result = 193;
+    long temp = Double.doubleToLongBits(this.x);
+    result = 37 * result + (int) (temp ^ (temp >>> 32));
+    temp = Double.doubleToLongBits(this.y);
+    result = 37 * result + (int) (temp ^ (temp >>> 32));
+    return result;
+  }
 }
