bb52a970289d7c63acb78c94501ffd804cdf7eae
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:25:36 -0500

    codebert-base.commons-jxpath.mid-1062.idx-894.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dynabeans/StrictLazyDynaBeanPointerFactory.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dynabeans/StrictLazyDynaBeanPointerFactory.java
@@ -17,7 +17,6 @@
 package org.apache.commons.jxpath.ri.model.dynabeans;
 
 import java.util.Locale;
-
 import org.apache.commons.beanutils.LazyDynaBean;
 import org.apache.commons.beanutils.LazyDynaClass;
 import org.apache.commons.jxpath.ri.QName;
@@ -26,88 +25,78 @@ import org.apache.commons.jxpath.ri.model.NodePointerFactory;
 import org.apache.commons.jxpath.ri.model.beans.PropertyPointer;
 
 /**
- * Implemented in response to [JXPATH-144]. Optionally pluggable
- * <code>NodePointerFactory</code> that returns a special type of
- * <code>NodePointer</code> for <code>LazyDynaBean</code>s. The
- * <code>PropertyPointer</code>s returned by these will respect
- * {@link LazyDynaClass#isDynaProperty(String)} when determining
- * {@link PropertyPointer#isActual()}.
- *
+ * Implemented in response to [JXPATH-144]. Optionally pluggable <code>NodePointerFactory</code>
+ * that returns a special type of <code>NodePointer</code> for <code>LazyDynaBean</code>s. The
+ * <code>PropertyPointer</code>s returned by these will respect {@link
+ * LazyDynaClass#isDynaProperty(String)} when determining {@link PropertyPointer#isActual()}.
  */
 public class StrictLazyDynaBeanPointerFactory implements NodePointerFactory {
+  /** Pointer implementation. */
+  private static class StrictLazyDynaBeanPointer extends DynaBeanPointer {
+    private static final long serialVersionUID = 1L;
+
+    private final LazyDynaBean lazyDynaBean;
+
     /**
-     * Pointer implementation.
+     * Create a new StrictLazyDynaBeanPointer instance.
+     *
+     * @param parent pointer
+     * @param name is the name given to the first node
+     * @param lazyDynaBean pointed
      */
-    private static class StrictLazyDynaBeanPointer extends DynaBeanPointer {
+    public StrictLazyDynaBeanPointer(
+        final NodePointer parent, final QName name, final LazyDynaBean lazyDynaBean) {
+      super(parent, name, lazyDynaBean);
+      this.lazyDynaBean = lazyDynaBean;
+    }
+
+    /**
+     * Create a new StrictLazyDynaBeanPointer instance.
+     *
+     * @param name is the name given to the first node
+     * @param lazyDynaBean pointed
+     * @param locale Locale
+     */
+    public StrictLazyDynaBeanPointer(
+        final QName name, final LazyDynaBean lazyDynaBean, final Locale locale) {
+      super(name, lazyDynaBean, locale);
+      this.lazyDynaBean = new LazyDynaBean();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public PropertyPointer getPropertyPointer() {
+      return new DynaBeanPropertyPointer(this, lazyDynaBean) {
         private static final long serialVersionUID = 1L;
 
-        private final LazyDynaBean lazyDynaBean;
-
-        /**
-         * Create a new StrictLazyDynaBeanPointer instance.
-         *
-         * @param parent pointer
-         * @param name is the name given to the first node
-         * @param lazyDynaBean pointed
-         */
-        public StrictLazyDynaBeanPointer(final NodePointer parent, final QName name, final LazyDynaBean lazyDynaBean) {
-            super(parent, name, lazyDynaBean);
-            this.lazyDynaBean = lazyDynaBean;
-        }
-
-        /**
-         * Create a new StrictLazyDynaBeanPointer instance.
-         *
-         * @param name is the name given to the first node
-         * @param lazyDynaBean pointed
-         * @param locale Locale
-         */
-        public StrictLazyDynaBeanPointer(final QName name, final LazyDynaBean lazyDynaBean, final Locale locale) {
-            super(name, lazyDynaBean, locale);
-            this.lazyDynaBean = lazyDynaBean;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
         @Override
-        public PropertyPointer getPropertyPointer() {
-            return new DynaBeanPropertyPointer(this, lazyDynaBean) {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                protected boolean isActualProperty() {
-                    return ((LazyDynaClass) lazyDynaBean.getDynaClass())
-                            .isDynaProperty(getPropertyName());
-                }
-            };
+        protected boolean isActualProperty() {
+          return ((LazyDynaClass) lazyDynaBean.getDynaClass()).isDynaProperty(getPropertyName());
         }
+      };
     }
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getOrder() {
-        return DynaBeanPointerFactory.DYNA_BEAN_POINTER_FACTORY_ORDER - 1;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public int getOrder() {
+    return DynaBeanPointerFactory.DYNA_BEAN_POINTER_FACTORY_ORDER - 1;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public NodePointer createNodePointer(final QName name, final Object object, final Locale locale) {
-        return object instanceof LazyDynaBean ? new StrictLazyDynaBeanPointer(name,
-                (LazyDynaBean) object, locale) : null;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public NodePointer createNodePointer(final QName name, final Object object, final Locale locale) {
+    return object instanceof LazyDynaBean
+        ? new StrictLazyDynaBeanPointer(name, (LazyDynaBean) object, locale)
+        : null;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public NodePointer createNodePointer(final NodePointer parent, final QName name, final Object object) {
-        return object instanceof LazyDynaBean ? new StrictLazyDynaBeanPointer(parent, name,
-                (LazyDynaBean) object) : null;
-    }
-
+  /** {@inheritDoc} */
+  @Override
+  public NodePointer createNodePointer(
+      final NodePointer parent, final QName name, final Object object) {
+    return object instanceof LazyDynaBean
+        ? new StrictLazyDynaBeanPointer(parent, name, (LazyDynaBean) object)
+        : null;
+  }
 }
