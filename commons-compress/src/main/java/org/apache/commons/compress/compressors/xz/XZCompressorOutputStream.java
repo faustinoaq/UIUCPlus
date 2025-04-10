e1ae1f5f5f955264eedd183cc80859f3788afa26
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:33:57 -0500

    Formatted main branch before NatGen-base.commons-compress.mid-2371.idx-1760.2.mutant injection

--- commons-compress/src/main/java/org/apache/commons/compress/compressors/xz/XZCompressorOutputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/compressors/xz/XZCompressorOutputStream.java
@@ -20,82 +20,81 @@ package org.apache.commons.compress.compressors.xz;
 
 import java.io.IOException;
 import java.io.OutputStream;
-
 import org.apache.commons.compress.compressors.CompressorOutputStream;
 import org.tukaani.xz.LZMA2Options;
 import org.tukaani.xz.XZOutputStream;
 
 /**
  * XZ compressor.
+ *
  * @since 1.4
  */
 public class XZCompressorOutputStream extends CompressorOutputStream {
-    private final XZOutputStream out;
+  private final XZOutputStream out;
 
-    /**
-     * Creates a new XZ compressor using the default LZMA2 options.
-     * This is equivalent to {@code XZCompressorOutputStream(outputStream, 6)}.
-     * @param outputStream the stream to wrap
-     * @throws IOException on error
-     */
-    public XZCompressorOutputStream(final OutputStream outputStream)
-            throws IOException {
-        out = new XZOutputStream(outputStream, new LZMA2Options());
-    }
+  /**
+   * Creates a new XZ compressor using the default LZMA2 options. This is equivalent to {@code
+   * XZCompressorOutputStream(outputStream, 6)}.
+   *
+   * @param outputStream the stream to wrap
+   * @throws IOException on error
+   */
+  public XZCompressorOutputStream(final OutputStream outputStream) throws IOException {
+    out = new XZOutputStream(outputStream, new LZMA2Options());
+  }
 
-    /**
-     * Creates a new XZ compressor using the specified LZMA2 preset level.
-     * <p>
-     * The presets 0-3 are fast presets with medium compression.
-     * The presets 4-6 are fairly slow presets with high compression.
-     * The default preset is 6.
-     * <p>
-     * The presets 7-9 are like the preset 6 but use bigger dictionaries
-     * and have higher compressor and decompressor memory requirements.
-     * Unless the uncompressed size of the file exceeds 8&nbsp;MiB,
-     * 16&nbsp;MiB, or 32&nbsp;MiB, it is waste of memory to use the
-     * presets 7, 8, or 9, respectively.
-     * @param outputStream the stream to wrap
-     * @param preset the preset
-     * @throws IOException on error
-     */
-    public XZCompressorOutputStream(final OutputStream outputStream, final int preset)
-            throws IOException {
-        out = new XZOutputStream(outputStream, new LZMA2Options(preset));
-    }
+  /**
+   * Creates a new XZ compressor using the specified LZMA2 preset level.
+   *
+   * <p>The presets 0-3 are fast presets with medium compression. The presets 4-6 are fairly slow
+   * presets with high compression. The default preset is 6.
+   *
+   * <p>The presets 7-9 are like the preset 6 but use bigger dictionaries and have higher compressor
+   * and decompressor memory requirements. Unless the uncompressed size of the file exceeds
+   * 8&nbsp;MiB, 16&nbsp;MiB, or 32&nbsp;MiB, it is waste of memory to use the presets 7, 8, or 9,
+   * respectively.
+   *
+   * @param outputStream the stream to wrap
+   * @param preset the preset
+   * @throws IOException on error
+   */
+  public XZCompressorOutputStream(final OutputStream outputStream, final int preset)
+      throws IOException {
+    out = new XZOutputStream(outputStream, new LZMA2Options(preset));
+  }
 
-    @Override
-    public void close() throws IOException {
-        out.close();
-    }
+  @Override
+  public void close() throws IOException {
+    out.close();
+  }
 
-    /**
-     * Finishes compression without closing the underlying stream.
-     * No more data can be written to this stream after finishing.
-     * @throws IOException on error
-     */
-    public void finish() throws IOException {
-        out.finish();
-    }
+  /**
+   * Finishes compression without closing the underlying stream. No more data can be written to this
+   * stream after finishing.
+   *
+   * @throws IOException on error
+   */
+  public void finish() throws IOException {
+    out.finish();
+  }
 
-    /**
-     * Flushes the encoder and calls {@code outputStream.flush()}.
-     * All buffered pending data will then be decompressible from
-     * the output stream. Calling this function very often may increase
-     * the compressed file size a lot.
-     */
-    @Override
-    public void flush() throws IOException {
-        out.flush();
-    }
+  /**
+   * Flushes the encoder and calls {@code outputStream.flush()}. All buffered pending data will then
+   * be decompressible from the output stream. Calling this function very often may increase the
+   * compressed file size a lot.
+   */
+  @Override
+  public void flush() throws IOException {
+    out.flush();
+  }
 
-    @Override
-    public void write(final byte[] buf, final int off, final int len) throws IOException {
-        out.write(buf, off, len);
-    }
+  @Override
+  public void write(final byte[] buf, final int off, final int len) throws IOException {
+    out.write(buf, off, len);
+  }
 
-    @Override
-    public void write(final int b) throws IOException {
-        out.write(b);
-    }
+  @Override
+  public void write(final int b) throws IOException {
+    out.write(b);
+  }
 }
