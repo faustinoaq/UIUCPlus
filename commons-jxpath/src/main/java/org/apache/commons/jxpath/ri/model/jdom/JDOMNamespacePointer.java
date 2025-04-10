ec0af11e6cdd03a61559475900bb2e5f94306c55
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:30:00 -0500

    codet5-base.commons-jxpath.mid-1178.idx-884.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/jdom/JDOMNamespacePointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/jdom/JDOMNamespacePointer.java
@@ -19,117 +19,115 @@ package org.apache.commons.jxpath.ri.model.jdom;
 import org.apache.commons.jxpath.ri.QName;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 
-/**
- * Represents a namespace node.
- */
+/** Represents a namespace node. */
 public class JDOMNamespacePointer extends NodePointer {
-    private final String prefix;
-    private String namespaceURI;
+  private final String prefix;
+  private String namespaceURI;
 
-    private static final long serialVersionUID = 7935311686545862379L;
+  private static final long serialVersionUID = 7935311686545862379L;
 
-    /**
-     * Create a new JDOMNamespacePointer.
-     * @param parent parent pointer
-     * @param prefix ns prefix
-     */
-    public JDOMNamespacePointer(final NodePointer parent, final String prefix) {
-        super(parent);
-        this.prefix = prefix;
+  /**
+   * Create a new JDOMNamespacePointer.
+   *
+   * @param parent parent pointer
+   * @param prefix ns prefix
+   */
+  public JDOMNamespacePointer(final NodePointer parent, final String prefix) {
+    super(parent);
+    this.prefix = prefix;
+  }
+
+  /**
+   * Create a new JDOMNamespacePointer.
+   *
+   * @param parent parent pointer
+   * @param prefix ns prefix
+   * @param namespaceURI ns URI
+   */
+  public JDOMNamespacePointer(
+      final NodePointer parent, final String prefix, final String namespaceURI) {
+    super(parent);
+    this.prefix = prefix;
+    this.namespaceURI = namespaceURI;
+  }
+
+  @Override
+  public QName getName() {
+    return new QName(prefix);
+  }
+
+  @Override
+  public Object getBaseValue() {
+    return null;
+  }
+
+  @Override
+  public boolean isCollection() {
+    return false;
+  }
+
+  @Override
+  public int getLength() {
+    return 1;
+  }
+
+  @Override
+  public Object getImmediateNode() {
+    return getNamespaceURI();
+  }
+
+  @Override
+  public String getNamespaceURI() {
+    if (namespaceURI == null) {
+      namespaceURI = parent.getNamespaceURI(prefix);
     }
+    return namespaceURI;
+  }
 
-    /**
-     * Create a new JDOMNamespacePointer.
-     * @param parent parent pointer
-     * @param prefix ns prefix
-     * @param namespaceURI ns URI
-     */
-    public JDOMNamespacePointer(
-            final NodePointer parent,
-            final String prefix,
-            final String namespaceURI) {
-        super(parent);
-        this.prefix = prefix;
-        this.namespaceURI = namespaceURI;
-    }
+  @Override
+  public boolean isLeaf() {
+    return true;
+  }
 
-    @Override
-    public QName getName() {
-        return new QName(prefix);
-    }
+  /**
+   * Throws UnsupportedOperationException.
+   *
+   * @param value Object value to set
+   */
+  @Override
+  public void setValue(final Object value) {
+    throw new UnsupportedOperationException("Cannot modify a namespace");
+  }
 
-    @Override
-    public Object getBaseValue() {
-        return null;
+  @Override
+  public String asPath() {
+    final StringBuffer buffer = new StringBuffer();
+    if (parent != null) {
+      buffer.append(parent.asPath());
+      buffer.append('/');
+    } else {
+      buffer.deleteCharAt(buffer.length() - 1);
     }
+    buffer.append("namespace::");
+    buffer.append(prefix);
+    return buffer.toString();
+  }
 
-    @Override
-    public boolean isCollection() {
-        return false;
-    }
+  @Override
+  public int hashCode() {
+    return prefix.hashCode();
+  }
 
-    @Override
-    public int getLength() {
-        return 1;
-    }
+  @Override
+  public boolean equals(final Object object) {
+    return object == this
+        || object instanceof JDOMNamespacePointer
+            && prefix.equals(((JDOMNamespacePointer) object).prefix);
+  }
 
-    @Override
-    public Object getImmediateNode() {
-        return getNamespaceURI();
-    }
-
-    @Override
-    public String getNamespaceURI() {
-        if (namespaceURI == null) {
-            namespaceURI = parent.getNamespaceURI(prefix);
-        }
-        return namespaceURI;
-    }
-
-    @Override
-    public boolean isLeaf() {
-        return true;
-    }
-
-    /**
-     * Throws UnsupportedOperationException.
-     * @param value Object value to set
-     */
-    @Override
-    public void setValue(final Object value) {
-        throw new UnsupportedOperationException("Cannot modify a namespace");
-    }
-
-    @Override
-    public String asPath() {
-        final StringBuffer buffer = new StringBuffer();
-        if (parent != null) {
-            buffer.append(parent.asPath());
-            if (buffer.length() == 0
-                || buffer.charAt(buffer.length() - 1) != '/') {
-                buffer.append('/');
-            }
-        }
-        buffer.append("namespace::");
-        buffer.append(prefix);
-        return buffer.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        return prefix.hashCode();
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        return object == this || object instanceof JDOMNamespacePointer && prefix.equals(((JDOMNamespacePointer) object).prefix);
-    }
-
-    @Override
-    public int compareChildNodePointers(
-        final NodePointer pointer1,
-        final NodePointer pointer2) {
-        // Won't happen - namespaces don't have children
-        return 0;
-    }
+  @Override
+  public int compareChildNodePointers(final NodePointer pointer1, final NodePointer pointer2) {
+    // Won't happen - namespaces don't have children
+    return 0;
+  }
 }
