a571e93d2009f410ed2a57cbd239ab7348e665bc
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:37:13 -0500

    Formatted main branch before NatGen-base.jfreechart.mid-6566.idx-4820.1.mutant injection

--- jfreechart/src/main/java/org/jfree/data/Values2D.java
+++ jfreechart/src/main/java/org/jfree/data/Values2D.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * -------------
@@ -40,36 +40,30 @@
 
 package org.jfree.data;
 
-/**
- * A general purpose interface that can be used to access a table of values.
- */
+/** A general purpose interface that can be used to access a table of values. */
 public interface Values2D {
 
-    /**
-     * Returns the number of rows in the table.
-     *
-     * @return The row count.
-     */
-    int getRowCount();
+  /**
+   * Returns the number of rows in the table.
+   *
+   * @return The row count.
+   */
+  int getRowCount();
 
-    /**
-     * Returns the number of columns in the table.
-     *
-     * @return The column count.
-     */
-    int getColumnCount();
+  /**
+   * Returns the number of columns in the table.
+   *
+   * @return The column count.
+   */
+  int getColumnCount();
 
-    /**
-     * Returns a value from the table.
-     *
-     * @param row  the row index (zero-based).
-     * @param column  the column index (zero-based).
-     *
-     * @return The value (possibly {@code null}).
-     *
-     * @throws IndexOutOfBoundsException if the {@code row}
-     *         or {@code column} is out of bounds.
-     */
-    Number getValue(int row, int column);
-
+  /**
+   * Returns a value from the table.
+   *
+   * @param row the row index (zero-based).
+   * @param column the column index (zero-based).
+   * @return The value (possibly {@code null}).
+   * @throws IndexOutOfBoundsException if the {@code row} or {@code column} is out of bounds.
+   */
+  Number getValue(int row, int column);
 }
