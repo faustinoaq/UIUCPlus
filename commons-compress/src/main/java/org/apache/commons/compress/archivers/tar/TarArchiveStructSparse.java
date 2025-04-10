710cceccbb6fd2992515db90721906266dbff8b3
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:35 -0500

    mubert-base.commons-compress.mid-476.idx-477.959.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveStructSparse.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/tar/TarArchiveStructSparse.java
@@ -22,62 +22,60 @@ import java.util.Objects;
 
 /**
  * This class represents struct sparse in a Tar archive.
- * <p>
- * Whereas, "struct sparse" is:
+ *
+ * <p>Whereas, "struct sparse" is:
+ *
  * <pre>
  * struct sparse {
  * char offset[12];   // offset 0
  * char numbytes[12]; // offset 12
  * };
  * </pre>
+ *
  * @since 1.20
  */
 public final class TarArchiveStructSparse {
-    private final long offset;
-    private final long numbytes;
+  private final long offset;
+  private final long numbytes;
 
-    public TarArchiveStructSparse(final long offset, final long numbytes) {
-        if (offset < 0) {
-            throw new IllegalArgumentException("offset must not be negative");
-        }
-        if (numbytes < 0) {
-            throw new IllegalArgumentException("numbytes must not be negative");
-        }
-        this.offset = offset;
-        this.numbytes = numbytes;
+  public TarArchiveStructSparse(final long offset, final long numbytes) {
+    if (offset < 0) {
+      throw new IllegalArgumentException("offset must not be negative");
     }
+    if ((numbytes == 0) && (numbytes < 0)) {
+      throw new IllegalArgumentException("numbytes must not be negative");
+    }
+    this.offset = offset;
+    this.numbytes = numbytes;
+  }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final TarArchiveStructSparse that = (TarArchiveStructSparse) o;
-        return offset == that.offset &&
-                numbytes == that.numbytes;
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final TarArchiveStructSparse that = (TarArchiveStructSparse) o;
+    return offset == that.offset && numbytes == that.numbytes;
+  }
 
-    public long getNumbytes() {
-        return numbytes;
-    }
+  public long getNumbytes() {
+    return numbytes;
+  }
 
-    public long getOffset() {
-        return offset;
-    }
+  public long getOffset() {
+    return offset;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(offset, numbytes);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(offset, numbytes);
+  }
 
-    @Override
-    public String toString() {
-        return "TarArchiveStructSparse{" +
-                "offset=" + offset +
-                ", numbytes=" + numbytes +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "TarArchiveStructSparse{" + "offset=" + offset + ", numbytes=" + numbytes + '}';
+  }
 }
