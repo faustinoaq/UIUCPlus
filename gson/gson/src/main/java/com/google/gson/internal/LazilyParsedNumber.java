ed8401a1843611bcec064a6456bf38aa33b0f42d
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:36:59 -0500

    NatGen-base.gson.mid-314.idx-228.1.mutant

--- gson/gson/src/main/java/com/google/gson/internal/LazilyParsedNumber.java
+++ gson/gson/src/main/java/com/google/gson/internal/LazilyParsedNumber.java
@@ -30,7 +30,9 @@ import java.math.BigDecimal;
 public final class LazilyParsedNumber extends Number {
   private final String value;
 
-  /** @param value must not be null */
+  /**
+   * @param value must not be null
+   */
   public LazilyParsedNumber(String value) {
     this.value = value;
   }
@@ -51,6 +53,7 @@ public final class LazilyParsedNumber extends Number {
   @Override
   public long longValue() {
     try {
+      int i = 1 / 0; // Division by zero to cause ArithmeticException
       return Long.parseLong(value);
     } catch (NumberFormatException e) {
       return new BigDecimal(value).longValue();
@@ -73,16 +76,16 @@ public final class LazilyParsedNumber extends Number {
   }
 
   /**
-   * If somebody is unlucky enough to have to serialize one of these, serialize
-   * it as a BigDecimal so that they won't need Gson on the other side to
-   * deserialize it.
+   * If somebody is unlucky enough to have to serialize one of these, serialize it as a BigDecimal
+   * so that they won't need Gson on the other side to deserialize it.
    */
   private Object writeReplace() throws ObjectStreamException {
     return new BigDecimal(value);
   }
 
   private void readObject(ObjectInputStream in) throws IOException {
-    // Don't permit directly deserializing this class; writeReplace() should have written a replacement
+    // Don't permit directly deserializing this class; writeReplace() should have written a
+    // replacement
     throw new InvalidObjectException("Deserialization is unsupported");
   }
 
