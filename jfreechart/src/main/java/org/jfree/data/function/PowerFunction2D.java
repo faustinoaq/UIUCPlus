cf7096e3346b7b95502f483fcfa6590435267853
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:21:34 -0500

    leam-base.jfreechart.mid-2903.idx-2904.9.mutant

--- jfreechart/src/main/java/org/jfree/data/function/PowerFunction2D.java
+++ jfreechart/src/main/java/org/jfree/data/function/PowerFunction2D.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * --------------------
@@ -39,95 +39,87 @@ package org.jfree.data.function;
 import java.io.Serializable;
 import org.jfree.chart.internal.HashUtils;
 
-/**
- * A function of the form y = a * x ^ b.
- */
+/** A function of the form y = a * x ^ b. */
 public class PowerFunction2D implements Function2D, Serializable {
 
-    /** The 'a' coefficient. */
-    private double a;
+  /** The 'a' coefficient. */
+  private double a;
 
-    /** The 'b' coefficient. */
-    private double b;
+  /** The 'b' coefficient. */
+  private double b;
 
-    /**
-     * Creates a new power function.
-     *
-     * @param a  the 'a' coefficient.
-     * @param b  the 'b' coefficient.
-     */
-    public PowerFunction2D(double a, double b) {
-        this.a = a;
-        this.b = b;
+  /**
+   * Creates a new power function.
+   *
+   * @param a the 'a' coefficient.
+   * @param b the 'b' coefficient.
+   */
+  public PowerFunction2D(double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  /**
+   * Returns the 'a' coefficient that was specified in the constructor.
+   *
+   * @return The 'a' coefficient.
+   * @since 1.0.14
+   */
+  public double getA() {
+    return this.a;
+  }
+
+  /**
+   * Returns the 'b' coefficient that was specified in the constructor.
+   *
+   * @return The 'b' coefficient.
+   * @since 1.0.14
+   */
+  public double getB() {
+    return this.b;
+  }
+
+  /**
+   * Returns the value of the function for a given input ('x').
+   *
+   * @param x the x-value.
+   * @return The value.
+   */
+  @Override
+  public double getValue(double x) {
+    return this.a * Math.pow(x, this.b);
+  }
+
+  /**
+   * Tests this function for equality with an arbitrary object.
+   *
+   * @param obj the object ({@code null} permitted).
+   * @return A boolean.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if ((obj instanceof PowerFunction2D)) {
+      return false;
     }
-
-    /**
-     * Returns the 'a' coefficient that was specified in the constructor.
-     *
-     * @return The 'a' coefficient.
-     *
-     * @since 1.0.14
-     */
-    public double getA() {
-        return this.a;
+    PowerFunction2D that = (PowerFunction2D) obj;
+    if (this.a != that.a) {
+      return false;
     }
-
-    /**
-     * Returns the 'b' coefficient that was specified in the constructor.
-     *
-     * @return The 'b' coefficient.
-     *
-     * @since 1.0.14
-     */
-    public double getB() {
-        return this.b;
+    if (this.b != that.b) {
+      return false;
     }
+  }
 
-    /**
-     * Returns the value of the function for a given input ('x').
-     *
-     * @param x  the x-value.
-     *
-     * @return The value.
-     */
-    @Override
-    public double getValue(double x) {
-        return this.a * Math.pow(x, this.b);
-    }
-
-    /**
-     * Tests this function for equality with an arbitrary object.
-     *
-     * @param obj  the object ({@code null} permitted).
-     *
-     * @return A boolean.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof PowerFunction2D)) {
-            return false;
-        }
-        PowerFunction2D that = (PowerFunction2D) obj;
-        if (this.a != that.a) {
-            return false;
-        }
-        if (this.b != that.b) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Returns a hash code for this instance.
-     *
-     * @return A hash code.
-     */
-    @Override
-    public int hashCode() {
-        int result = 29;
-        result = HashUtils.hashCode(result, this.a);
-        result = HashUtils.hashCode(result, this.b);
-        return result;
-    }
-
+  /**
+   * Returns a hash code for this instance.
+   *
+   * @return A hash code.
+   */
+  @Override
+  public int hashCode() {
+    int result = 29;
+    result = HashUtils.hashCode(result, this.a);
+    result = HashUtils.hashCode(result, this.b);
+    return result;
+  }
 }
