6fcbff0a749971b12b5fb7c71bb607556e7a9c8b
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:25:17 -0500

    codebert-base.gson.mid-2.idx-62.2.mutant

--- gson/extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
+++ gson/extras/src/main/java/com/google/gson/extras/examples/rawcollections/RawCollectionsExample.java
@@ -15,28 +15,29 @@
  */
 package com.google.gson.extras.examples.rawcollections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class RawCollectionsExample {
   static class Event {
     private String name;
     private String source;
+
     private Event(String name, String source) {
       this.name = name;
       this.source = source;
     }
+
     @Override
     public String toString() {
       return String.format("(name=%s, source=%s)", name, source);
     }
   }
 
-  @SuppressWarnings({ "unchecked", "rawtypes" })
+  @SuppressWarnings({"unchecked", "rawtypes"})
   public static void main(String[] args) {
     Gson gson = new Gson();
     Collection collection = new ArrayList();
@@ -45,7 +46,12 @@ public class RawCollectionsExample {
     collection.add(new Event("GREETINGS", "guest"));
     String json = gson.toJson(collection);
     System.out.println("Using Gson.toJson() on a raw collection: " + json);
-    JsonArray array = JsonParser.parseString(json).getAsJsonArray();
+    JsonElement element = JsonParser.parseString(json);
+    if (!element.isJsonArray()) {
+      System.out.println("Not a JSON array");
+      return;
+    }
+    JsonArray array = element.getAsJsonArray();
     String message = gson.fromJson(array.get(0), String.class);
     int number = gson.fromJson(array.get(1), int.class);
     Event event = gson.fromJson(array.get(2), Event.class);
