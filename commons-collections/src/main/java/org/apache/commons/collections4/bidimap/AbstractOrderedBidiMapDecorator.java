95e4864025a2306a4259c8531fea6c0e54707517
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:19:45 -0500

    Formatted main branch before leam-base.commons-collections.mid-2258.idx-2259.1.mutant injection

--- commons-collections/src/main/java/org/apache/commons/collections4/bidimap/AbstractOrderedBidiMapDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bidimap/AbstractOrderedBidiMapDecorator.java
@@ -20,75 +20,70 @@ import org.apache.commons.collections4.OrderedBidiMap;
 import org.apache.commons.collections4.OrderedMapIterator;
 
 /**
- * Provides a base decorator that enables additional functionality to be added
- * to an OrderedBidiMap via decoration.
- * <p>
- * Methods are forwarded directly to the decorated map.
- * </p>
- * <p>
- * This implementation does not perform any special processing with the map views.
- * Instead it simply returns the inverse from the wrapped map. This may be
- * undesirable, for example if you are trying to write a validating implementation
- * it would provide a loophole around the validation.
+ * Provides a base decorator that enables additional functionality to be added to an OrderedBidiMap
+ * via decoration.
+ *
+ * <p>Methods are forwarded directly to the decorated map.
+ *
+ * <p>This implementation does not perform any special processing with the map views. Instead it
+ * simply returns the inverse from the wrapped map. This may be undesirable, for example if you are
+ * trying to write a validating implementation it would provide a loophole around the validation.
  * But, you might want that loophole, so this class is kept simple.
- * </p>
  *
  * @param <K> the type of the keys in this map
  * @param <V> the type of the values in this map
  * @since 3.0
  */
-public abstract class AbstractOrderedBidiMapDecorator<K, V>
-        extends AbstractBidiMapDecorator<K, V>
-        implements OrderedBidiMap<K, V> {
+public abstract class AbstractOrderedBidiMapDecorator<K, V> extends AbstractBidiMapDecorator<K, V>
+    implements OrderedBidiMap<K, V> {
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param map  the map to decorate, must not be null
-     * @throws NullPointerException if the collection is null
-     */
-    protected AbstractOrderedBidiMapDecorator(final OrderedBidiMap<K, V> map) {
-        super(map);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param map the map to decorate, must not be null
+   * @throws NullPointerException if the collection is null
+   */
+  protected AbstractOrderedBidiMapDecorator(final OrderedBidiMap<K, V> map) {
+    super(map);
+  }
 
-    /**
-     * Gets the map being decorated.
-     *
-     * @return the decorated map
-     */
-    @Override
-    protected OrderedBidiMap<K, V> decorated() {
-        return (OrderedBidiMap<K, V>) super.decorated();
-    }
+  /**
+   * Gets the map being decorated.
+   *
+   * @return the decorated map
+   */
+  @Override
+  protected OrderedBidiMap<K, V> decorated() {
+    return (OrderedBidiMap<K, V>) super.decorated();
+  }
 
-    @Override
-    public OrderedMapIterator<K, V> mapIterator() {
-        return decorated().mapIterator();
-    }
+  @Override
+  public OrderedMapIterator<K, V> mapIterator() {
+    return decorated().mapIterator();
+  }
 
-    @Override
-    public K firstKey() {
-        return decorated().firstKey();
-    }
+  @Override
+  public K firstKey() {
+    return decorated().firstKey();
+  }
 
-    @Override
-    public K lastKey() {
-        return decorated().lastKey();
-    }
+  @Override
+  public K lastKey() {
+    return decorated().lastKey();
+  }
 
-    @Override
-    public K nextKey(final K key) {
-        return decorated().nextKey(key);
-    }
+  @Override
+  public K nextKey(final K key) {
+    return decorated().nextKey(key);
+  }
 
-    @Override
-    public K previousKey(final K key) {
-        return decorated().previousKey(key);
-    }
+  @Override
+  public K previousKey(final K key) {
+    return decorated().previousKey(key);
+  }
 
-    @Override
-    public OrderedBidiMap<V, K> inverseBidiMap() {
-        return decorated().inverseBidiMap();
-    }
-
+  @Override
+  public OrderedBidiMap<V, K> inverseBidiMap() {
+    return decorated().inverseBidiMap();
+  }
 }
