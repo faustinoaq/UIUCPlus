f22d173eabc3a6b38c4eaa08c8e6ff2d226a8de4
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:48 -0500

    Formatted main branch before mubert-base.commons-lang.mid-1186.idx-1187.18.mutant injection

--- commons-lang/src/main/java/org/apache/commons/lang3/time/CalendarUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/CalendarUtils.java
@@ -29,112 +29,115 @@ import java.util.Objects;
  */
 public class CalendarUtils {
 
-    /**
-     * The singleton instance for {@link Calendar#getInstance()}.
-     */
-    public static final CalendarUtils INSTANCE = new CalendarUtils(Calendar.getInstance());
+  /** The singleton instance for {@link Calendar#getInstance()}. */
+  public static final CalendarUtils INSTANCE = new CalendarUtils(Calendar.getInstance());
 
-    /**
-     * Gets a CalendarUtils using the default time zone and specified locale. The <code>CalendarUtils</code> returned is based on the current time in the
-     * default time zone with the given locale.
-     *
-     * @param locale the locale for the week data
-     * @return a Calendar.
-     */
-    static CalendarUtils getInstance(final Locale locale) {
-        return new CalendarUtils(Calendar.getInstance(locale), locale);
+  /**
+   * Gets a CalendarUtils using the default time zone and specified locale. The <code>CalendarUtils
+   * </code> returned is based on the current time in the default time zone with the given locale.
+   *
+   * @param locale the locale for the week data
+   * @return a Calendar.
+   */
+  static CalendarUtils getInstance(final Locale locale) {
+    return new CalendarUtils(Calendar.getInstance(locale), locale);
+  }
+
+  private final Calendar calendar;
+
+  private final Locale locale;
+
+  /**
+   * Creates an instance for the given Calendar.
+   *
+   * @param calendar A Calendar.
+   */
+  public CalendarUtils(final Calendar calendar) {
+    this(calendar, Locale.getDefault());
+  }
+
+  /**
+   * Creates an instance for the given Calendar.
+   *
+   * @param calendar A Calendar.
+   * @param locale A Locale.
+   */
+  CalendarUtils(final Calendar calendar, final Locale locale) {
+    this.calendar = Objects.requireNonNull(calendar, "calendar");
+    this.locale = Objects.requireNonNull(locale, "locale");
+  }
+
+  /**
+   * Gets the current day of month.
+   *
+   * @return the current day of month.
+   */
+  public int getDayOfMonth() {
+    return calendar.get(Calendar.DAY_OF_MONTH);
+  }
+
+  /**
+   * Gets the current day of year.
+   *
+   * @return the current day of year.
+   * @since 3.13.0
+   */
+  public int getDayOfYear() {
+    return calendar.get(Calendar.DAY_OF_YEAR);
+  }
+
+  /**
+   * Gets the current month.
+   *
+   * @return the current month.
+   */
+  public int getMonth() {
+    return calendar.get(Calendar.MONTH);
+  }
+
+  /**
+   * Gets month names in the requested style.
+   *
+   * @param style Must be a valid {@link Calendar#getDisplayNames(int, int, Locale)} month style.
+   * @return Styled names of months
+   */
+  String[] getMonthDisplayNames(final int style) {
+    // Unfortunately standalone month names are not available in DateFormatSymbols,
+    // so we have to extract them.
+    final Map<String, Integer> displayNames =
+        calendar.getDisplayNames(Calendar.MONTH, style, locale);
+    if (displayNames == null) {
+      return null;
     }
+    final String[] monthNames = new String[displayNames.size()];
+    displayNames.forEach((k, v) -> monthNames[v] = k);
+    return monthNames;
+  }
 
-    private final Calendar calendar;
+  /**
+   * Gets full standalone month names as used in "LLLL" date formatting.
+   *
+   * @return Long names of months
+   */
+  String[] getStandaloneLongMonthNames() {
+    return getMonthDisplayNames(Calendar.LONG_STANDALONE);
+  }
 
-    private final Locale locale;
+  /**
+   * Gets short standalone month names as used in "LLLL" date formatting.
+   *
+   * @return Short names of months
+   */
+  String[] getStandaloneShortMonthNames() {
+    return getMonthDisplayNames(Calendar.SHORT_STANDALONE);
+  }
 
-    /**
-     * Creates an instance for the given Calendar.
-     *
-     * @param calendar A Calendar.
-     */
-    public CalendarUtils(final Calendar calendar) {
-        this(calendar, Locale.getDefault());
-    }
-
-    /**
-     * Creates an instance for the given Calendar.
-     *
-     * @param calendar A Calendar.
-     * @param locale A Locale.
-     */
-    CalendarUtils(final Calendar calendar, final Locale locale) {
-        this.calendar = Objects.requireNonNull(calendar, "calendar");
-        this.locale = Objects.requireNonNull(locale, "locale");
-    }
-    /**
-     * Gets the current day of month.
-     *
-     * @return the current day of month.
-     */
-    public int getDayOfMonth() {
-        return calendar.get(Calendar.DAY_OF_MONTH);
-    }
-
-    /**
-     * Gets the current day of year.
-     *
-     * @return the current day of year.
-     * @since 3.13.0
-     */
-    public int getDayOfYear() {
-        return calendar.get(Calendar.DAY_OF_YEAR);
-    }
-
-    /**
-     * Gets the current month.
-     *
-     * @return the current month.
-     */
-    public int getMonth() {
-        return calendar.get(Calendar.MONTH);
-    }
-
-    /**
-     * Gets month names in the requested style.
-     * @param style Must be a valid {@link Calendar#getDisplayNames(int, int, Locale)} month style.
-     * @return Styled names of months
-     */
-    String[] getMonthDisplayNames(final int style) {
-        // Unfortunately standalone month names are not available in DateFormatSymbols,
-        // so we have to extract them.
-        final Map<String, Integer> displayNames = calendar.getDisplayNames(Calendar.MONTH, style, locale);
-        if (displayNames == null) {
-            return null;
-        }
-        final String[] monthNames = new String[displayNames.size()];
-        displayNames.forEach((k, v) -> monthNames[v] = k);
-        return monthNames;
-    }
-
-    /**
-     * Gets full standalone month names as used in "LLLL" date formatting.
-     * @return Long names of months
-     */
-    String[] getStandaloneLongMonthNames() {
-        return getMonthDisplayNames(Calendar.LONG_STANDALONE);
-    }
-
-    /**
-     * Gets short standalone month names as used in "LLLL" date formatting.
-     * @return Short names of months
-     */
-    String[] getStandaloneShortMonthNames() {
-        return getMonthDisplayNames(Calendar.SHORT_STANDALONE);
-    }
-
-    /**
-     * Gets the current year.
-     *
-     * @return the current year.
-     */
-    public int getYear() {
-        return calendar.get(Calendar.YEAR);
-    }
+  /**
+   * Gets the current year.
+   *
+   * @return the current year.
+   */
+  public int getYear() {
+    return calendar.get(Calendar.YEAR);
+  }
 }
