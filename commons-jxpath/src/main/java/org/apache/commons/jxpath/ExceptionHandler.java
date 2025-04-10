2501bed19e0ab26291d0f5386ff7936dc248d081
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:31:21 -0500

    Formatted main branch before codet5-base.commons-jxpath.mid-4.idx-5.2.mutant injection

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ExceptionHandler.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ExceptionHandler.java
@@ -17,14 +17,16 @@
 package org.apache.commons.jxpath;
 
 /**
- * Exception handler interface.  Actually handles Throwables.
+ * Exception handler interface. Actually handles Throwables.
+ *
  * @since 1.4
  */
 public interface ExceptionHandler {
-    /**
-     * Handle an encountered Throwable.
-     * @param t to handle
-     * @param ptr specific context
-     */
-    void handle(Throwable t, Pointer ptr);
+  /**
+   * Handle an encountered Throwable.
+   *
+   * @param t to handle
+   * @param ptr specific context
+   */
+  void handle(Throwable t, Pointer ptr);
 }
