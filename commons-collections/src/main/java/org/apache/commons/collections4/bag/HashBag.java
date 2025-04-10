a9a6223608d76a2228d93a446c71e57aaba8b78f
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:19:26 -0500

    Formatted main branch before leam-base.commons-collections.mid-3672.idx-3673.18.mutant injection

--- commons-collections/src/main/java/org/apache/commons/collections4/bag/HashBag.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bag/HashBag.java
@@ -24,62 +24,57 @@ import java.util.Collection;
 import java.util.HashMap;
 
 /**
- * Implements {@code Bag}, using a {@link HashMap} to provide the
- * data storage. This is the standard implementation of a bag.
- * <p>
- * A {@code Bag} stores each object in the collection together with a
- * count of occurrences. Extra methods on the interface allow multiple copies
- * of an object to be added or removed at once. It is important to read the
- * interface javadoc carefully as several methods violate the
- * {@link Collection} interface specification.
- * </p>
+ * Implements {@code Bag}, using a {@link HashMap} to provide the data storage. This is the standard
+ * implementation of a bag.
+ *
+ * <p>A {@code Bag} stores each object in the collection together with a count of occurrences. Extra
+ * methods on the interface allow multiple copies of an object to be added or removed at once. It is
+ * important to read the interface javadoc carefully as several methods violate the {@link
+ * Collection} interface specification.
  *
  * @param <E> the type of elements in this bag
  * @since 3.0 (previously in main package v2.0)
  */
 public class HashBag<E> extends AbstractMapBag<E> implements Serializable {
 
-    /** Serial version lock */
-    private static final long serialVersionUID = -6561115435802554013L;
+  /** Serial version lock */
+  private static final long serialVersionUID = -6561115435802554013L;
 
-    /**
-     * Constructs an empty {@link HashBag}.
-     */
-    public HashBag() {
-        super(new HashMap<>());
-    }
+  /** Constructs an empty {@link HashBag}. */
+  public HashBag() {
+    super(new HashMap<>());
+  }
 
-    /**
-     * Constructs a bag containing all the members of the given collection.
-     *
-     * @param coll  a collection to copy into this bag
-     */
-    public HashBag(final Collection<? extends E> coll) {
-        this();
-        addAll(coll);
-    }
+  /**
+   * Constructs a bag containing all the members of the given collection.
+   *
+   * @param coll a collection to copy into this bag
+   */
+  public HashBag(final Collection<? extends E> coll) {
+    this();
+    addAll(coll);
+  }
 
-    /**
-     * Write the bag out using a custom routine.
-     *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
-     */
-    private void writeObject(final ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
-        super.doWriteObject(out);
-    }
+  /**
+   * Write the bag out using a custom routine.
+   *
+   * @param out the output stream
+   * @throws IOException if an error occurs while writing to the stream
+   */
+  private void writeObject(final ObjectOutputStream out) throws IOException {
+    out.defaultWriteObject();
+    super.doWriteObject(out);
+  }
 
-    /**
-     * Read the bag in using a custom routine.
-     *
-     * @param in  the input stream
-     * @throws IOException if an error occurs while reading from the stream
-     * @throws ClassNotFoundException if an object read from the stream can not be loaded
-     */
-    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        super.doReadObject(new HashMap<>(), in);
-    }
-
+  /**
+   * Read the bag in using a custom routine.
+   *
+   * @param in the input stream
+   * @throws IOException if an error occurs while reading from the stream
+   * @throws ClassNotFoundException if an object read from the stream can not be loaded
+   */
+  private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    super.doReadObject(new HashMap<>(), in);
+  }
 }
