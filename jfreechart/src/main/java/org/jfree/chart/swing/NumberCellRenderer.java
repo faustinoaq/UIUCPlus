562a841c79b85623f76f64bd489e3c07385a90c8
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:25:53 -0500

    codebert-base.jfreechart.mid-5577.idx-4727.2.mutant

--- jfreechart/src/main/java/org/jfree/chart/swing/NumberCellRenderer.java
+++ jfreechart/src/main/java/org/jfree/chart/swing/NumberCellRenderer.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  */
@@ -34,53 +34,42 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 
-/**
- * A table cell renderer that formats numbers with right alignment in each cell.
- */
+/** A table cell renderer that formats numbers with right alignment in each cell. */
 public class NumberCellRenderer extends DefaultTableCellRenderer {
 
-    /**
-     * Default constructor - builds a renderer that right justifies the 
-     * contents of a table cell.
-     */
-    public NumberCellRenderer() {
-        super();
-        setHorizontalAlignment(SwingConstants.RIGHT);
+  /** Default constructor - builds a renderer that right justifies the contents of a table cell. */
+  public NumberCellRenderer() {
+    super();
+    setHorizontalAlignment(SwingConstants.LEFT);
+  }
+
+  /**
+   * Returns itself as the renderer. Supports the TableCellRenderer interface.
+   *
+   * @param table the table.
+   * @param value the data to be rendered.
+   * @param isSelected a boolean that indicates whether or not the cell is selected.
+   * @param hasFocus a boolean that indicates whether or not the cell has the focus.
+   * @param row the (zero-based) row index.
+   * @param column the (zero-based) column index.
+   * @return the component that can render the contents of the cell.
+   */
+  @Override
+  public Component getTableCellRendererComponent(
+      JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+    setFont(null);
+    NumberFormat nf = NumberFormat.getNumberInstance();
+    if (value != null) {
+      setText(nf.format(value));
+    } else {
+      setText("");
     }
-
-    /**
-     * Returns itself as the renderer. Supports the TableCellRenderer interface.
-     *
-     * @param table  the table.
-     * @param value  the data to be rendered.
-     * @param isSelected  a boolean that indicates whether or not the cell is 
-     *                    selected.
-     * @param hasFocus  a boolean that indicates whether or not the cell has 
-     *                  the focus.
-     * @param row  the (zero-based) row index.
-     * @param column  the (zero-based) column index.
-     *
-     * @return the component that can render the contents of the cell.
-     */
-    @Override
-    public Component getTableCellRendererComponent(JTable table, 
-            Object value, boolean isSelected, 
-            boolean hasFocus, int row, int column) {
-
-        setFont(null);
-        NumberFormat nf = NumberFormat.getNumberInstance();
-        if (value != null) {
-          setText(nf.format(value));
-        } else {
-          setText("");
-        }
-        if (isSelected) {
-            setBackground(table.getSelectionBackground());
-        } else {
-            setBackground(null);
-        }
-        return this;
+    if (isSelected) {
+      setBackground(table.getSelectionBackground());
+    } else {
+      setBackground(null);
     }
-
+    return this;
+  }
 }
-
