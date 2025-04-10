147aeb0c0235cab7a352412248389254660ee871
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:37:24 -0500

    NatGen-base.commons-lang.mid-3341.idx-2478.3.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/time/GmtTimeZone.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/GmtTimeZone.java
@@ -27,87 +27,100 @@ import java.util.TimeZone;
  */
 class GmtTimeZone extends TimeZone {
 
-    private static final int MILLISECONDS_PER_MINUTE = 60 * 1000;
-    private static final int MINUTES_PER_HOUR = 60;
-    private static final int HOURS_PER_DAY = 24;
+  private static final int MILLISECONDS_PER_MINUTE = 60 * 1000;
+  private static final int MINUTES_PER_HOUR = 60;
+  private static final int HOURS_PER_DAY = 24;
 
-    // Serializable!
-    static final long serialVersionUID = 1L;
+  // Serializable!
+  static final long serialVersionUID = 1L;
 
-    private static StringBuilder twoDigits(final StringBuilder sb, final int n) {
-        return sb.append((char) ('0' + (n / 10))).append((char) ('0' + (n % 10)));
+  private static StringBuilder twoDigits(final StringBuilder sb, final int n) {
+    return sb.append((char) ('0' + (n / 10))).append((char) ('0' + (n % 10)));
+  }
+
+  private final int offset;
+
+  private final String zoneId;
+
+  GmtTimeZone(final boolean negate, final int hours, final int minutes) {
+    if (hours >= HOURS_PER_DAY) {
+      throw new IllegalArgumentException(hours + " hours out of range");
     }
-    private final int offset;
-
-    private final String zoneId;
-
-    GmtTimeZone(final boolean negate, final int hours, final int minutes) {
-        if (hours >= HOURS_PER_DAY) {
-            throw new IllegalArgumentException(hours + " hours out of range");
-        }
-        if (minutes >= MINUTES_PER_HOUR) {
-            throw new IllegalArgumentException(minutes + " minutes out of range");
-        }
-        final int milliseconds = (minutes + (hours * MINUTES_PER_HOUR)) * MILLISECONDS_PER_MINUTE;
-        offset = negate ? -milliseconds : milliseconds;
-        // @formatter:off
-        zoneId = twoDigits(twoDigits(new StringBuilder(9)
-            .append(TimeZones.GMT_ID)
-            .append(negate ? '-' : '+'), hours)
-            .append(':'), minutes)
+    if (minutes >= MINUTES_PER_HOUR) {
+      throw new IllegalArgumentException(minutes + " minutes out of range");
+    }
+    final int milliseconds = (minutes + (hours * MINUTES_PER_HOUR)) * MILLISECONDS_PER_MINUTE;
+    offset = negate ? -milliseconds : milliseconds;
+    // @formatter:off
+    zoneId =
+        twoDigits(
+                twoDigits(
+                        new StringBuilder(9).append(TimeZones.GMT_ID).append(negate ? '-' : '+'),
+                        hours)
+                    .append(':'),
+                minutes)
             .toString();
-        // @formatter:on
-    }
+    // @formatter:on
+  }
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof GmtTimeZone)) {
-            return false;
-        }
-        final GmtTimeZone other = (GmtTimeZone) obj;
-        return offset == other.offset && Objects.equals(zoneId, other.zoneId);
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
     }
+    if (!(obj instanceof GmtTimeZone)) {
+      return false;
+    }
+    final GmtTimeZone other = (GmtTimeZone) obj;
+    return offset == other.offset && Objects.equals(zoneId, other.zoneId);
+  }
 
-    @Override
-    public String getID() {
-        return zoneId;
-    }
+  @Override
+  public String getID() {
+    return zoneId;
+  }
 
-    @Override
-    public int getOffset(final int era, final int year, final int month, final int day, final int dayOfWeek, final int milliseconds) {
-        return offset;
-    }
+  @Override
+  public int getOffset(
+      final int era,
+      final int year,
+      final int month,
+      final int day,
+      final int dayOfWeek,
+      final int milliseconds) {
+    return offset;
+  }
 
-    @Override
-    public int getRawOffset() {
-        return offset;
-    }
+  @Override
+  public int getRawOffset() {
+    return offset;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(offset, zoneId);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(offset, zoneId);
+  }
 
-    @Override
-    public boolean inDaylightTime(final Date date) {
-        return false;
-    }
+  @Override
+  public boolean inDaylightTime(final Date date) {
+    return false;
+  }
 
-    @Override
-    public void setRawOffset(final int offsetMillis) {
-        throw new UnsupportedOperationException();
+  @Override
+  public void setRawOffset(final int offsetMillis) {
+    if (offsetMillis < 0) {
+      throw new IllegalArgumentException("Offset can not be negative");
     }
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public String toString() {
-        return "[GmtTimeZone id=\"" + zoneId + "\",offset=" + offset + ']';
-    }
+  @Override
+  public String toString() {
+    return "[GmtTimeZone id=\"" + zoneId + "\",offset=" + offset + ']';
+  }
 
-    @Override
-    public boolean useDaylightTime() {
-        return false;
-    }
+  @Override
+  public boolean useDaylightTime() {
+    return false;
+  }
 }
