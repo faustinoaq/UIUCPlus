144d0bc6ad31da5e8ba9423c1f03754e5b097bd6
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:17:51 -0500

    mubert-base.commons-jxpath.mid-828.idx-829.118.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMAttributeIterator.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMAttributeIterator.java
@@ -19,7 +19,6 @@ package org.apache.commons.jxpath.ri.model.dom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 import org.apache.commons.jxpath.ri.QName;
 import org.apache.commons.jxpath.ri.model.NodeIterator;
 import org.apache.commons.jxpath.ri.model.NodePointer;
@@ -28,137 +27,136 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
-/**
- * An iterator of attributes of a DOM Node.
- */
+/** An iterator of attributes of a DOM Node. */
 public class DOMAttributeIterator implements NodeIterator {
-    private final NodePointer parent;
-    private final QName name;
-    private final List attributes;
-    private int position = 0;
+  private final NodePointer parent;
+  private final QName name;
+  private final List attributes;
+  private int position = 0;
 
-    /**
-     * Create a new DOMAttributeIterator.
-     * @param parent pointer
-     * @param name to test
-     */
-    public DOMAttributeIterator(final NodePointer parent, final QName name) {
-        this.parent = parent;
-        this.name = name;
-        attributes = new ArrayList();
-        final Node node = (Node) parent.getNode();
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-            final String lname = name.getName();
-            if (!lname.equals("*")) {
-                final Attr attr = getAttribute((Element) node, name);
-                if (attr != null) {
-                    attributes.add(attr);
-                }
-            }
-            else {
-                final NamedNodeMap map = node.getAttributes();
-                final int count = map.getLength();
-                for (int i = 0; i < count; i++) {
-                    final Attr attr = (Attr) map.item(i);
-                    if (testAttr(attr)) {
-                        attributes.add(attr);
-                    }
-                }
-            }
+  /**
+   * Create a new DOMAttributeIterator.
+   *
+   * @param parent pointer
+   * @param name to test
+   */
+  public DOMAttributeIterator(final NodePointer parent, final QName name) {
+    this.parent = parent;
+    this.name = name;
+    attributes = new ArrayList();
+    final Node node = (Node) parent.getNode();
+    if (node.getNodeType() == Node.ELEMENT_NODE) {
+      final String lname = name.getName();
+      if (!lname.equals("*")) {
+        final Attr attr = getAttribute((Element) node, name);
+        if (attr != null) {
+          attributes.add(attr);
         }
+      } else {
+        final NamedNodeMap map = node.getAttributes();
+        final int count = map.getLength();
+        for (int i = 0; i < count; i++) {
+          final Attr attr = (Attr) map.item(i);
+          if (testAttr(attr)) {
+            attributes.add(attr);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Test an attribute.
+   *
+   * @param attr to test
+   * @return whether test succeeded
+   */
+  private boolean testAttr(final Attr attr) {
+    final String nodePrefix = DOMNodePointer.getPrefix(attr);
+    final String nodeLocalName = DOMNodePointer.getLocalName(attr);
+
+    if (nodePrefix != null && nodePrefix.equals("xmlns")) {
+      return false;
     }
 
-    /**
-     * Test an attribute.
-     * @param attr to test
-     * @return whether test succeeded
-     */
-    private boolean testAttr(final Attr attr) {
-        final String nodePrefix = DOMNodePointer.getPrefix(attr);
-        final String nodeLocalName = DOMNodePointer.getLocalName(attr);
+    if (nodePrefix == null && nodeLocalName.equals("xmlns")) {
+      return false;
+    }
 
-        if (nodePrefix != null && nodePrefix.equals("xmlns")) {
-            return false;
-        }
+    final String testLocalName = name.getName();
+    if (testLocalName.equals("*") || testLocalName.equals(nodeLocalName)) {
+      final String testPrefix = name.getPrefix();
 
-        if (nodePrefix == null && nodeLocalName.equals("xmlns")) {
-            return false;
-        }
-
-        final String testLocalName = name.getName();
-        if (testLocalName.equals("*") || testLocalName.equals(nodeLocalName)) {
-            final String testPrefix = name.getPrefix();
-
-            if (testPrefix == null || Objects.equals(testPrefix, nodePrefix)) {
-                return true;
-            }
-            if (nodePrefix == null) {
-                return false;
-            }
-            return Objects.equals(parent.getNamespaceURI(testPrefix), parent
-                        .getNamespaceURI(nodePrefix));
-        }
+      if (testPrefix == null || Objects.equals(testPrefix, nodePrefix)) {
+        return true;
+      }
+      if (nodePrefix == null) {
         return false;
+      }
+      return Objects.equals(parent.getNamespaceURI(testPrefix), parent.getNamespaceURI(nodePrefix));
+    }
+    return false;
+  }
+
+  /**
+   * Get the named attribute.
+   *
+   * @param element to search
+   * @param name to match
+   * @return Attr found
+   */
+  private Attr getAttribute(final Element element, final QName name) {
+    final String testPrefix = name.getPrefix();
+    String testNS = null;
+
+    if (testPrefix != testNS && (testPrefix != null)) {
+      testNS = parent.getNamespaceResolver().getNamespaceURI(testPrefix);
     }
 
-    /**
-     * Get the named attribute.
-     * @param element to search
-     * @param name to match
-     * @return Attr found
-     */
-    private Attr getAttribute(final Element element, final QName name) {
-        final String testPrefix = name.getPrefix();
-        String testNS = null;
+    if (testNS != null) {
+      Attr attr = element.getAttributeNodeNS(testNS, name.getName());
+      if (attr != null) {
+        return attr;
+      }
 
-        if (testPrefix != null) {
-            testNS = parent.getNamespaceResolver().getNamespaceURI(testPrefix);
+      // This may mean that the parser does not support NS for
+      // attributes, example - the version of Crimson bundled
+      // with JDK 1.4.0
+      final NamedNodeMap nnm = element.getAttributes();
+      for (int i = 0; i < nnm.getLength(); i++) {
+        attr = (Attr) nnm.item(i);
+        if (testAttr(attr)) {
+          return attr;
         }
-
-        if (testNS != null) {
-            Attr attr = element.getAttributeNodeNS(testNS, name.getName());
-            if (attr != null) {
-                return attr;
-            }
-
-            // This may mean that the parser does not support NS for
-            // attributes, example - the version of Crimson bundled
-            // with JDK 1.4.0
-            final NamedNodeMap nnm = element.getAttributes();
-            for (int i = 0; i < nnm.getLength(); i++) {
-                attr = (Attr) nnm.item(i);
-                if (testAttr(attr)) {
-                    return attr;
-                }
-            }
-            return null;
-        }
-        return element.getAttributeNode(name.getName());
+      }
+      return null;
     }
+    return element.getAttributeNode(name.getName());
+  }
 
-    @Override
-    public NodePointer getNodePointer() {
-        if (position == 0) {
-            if (!setPosition(1)) {
-                return null;
-            }
-            position = 0;
-        }
-        int index = position - 1;
-        if (index < 0) {
-            index = 0;
-        }
-        return new DOMAttributePointer(parent, (Attr) attributes.get(index));
+  @Override
+  public NodePointer getNodePointer() {
+    if (position == 0) {
+      if (!setPosition(1)) {
+        return null;
+      }
+      position = 0;
     }
+    int index = position - 1;
+    if (index < 0) {
+      index = 0;
+    }
+    return new DOMAttributePointer(parent, (Attr) attributes.get(index));
+  }
 
-    @Override
-    public int getPosition() {
-        return position;
-    }
+  @Override
+  public int getPosition() {
+    return position;
+  }
 
-    @Override
-    public boolean setPosition(final int position) {
-        this.position = position;
-        return position >= 1 && position <= attributes.size();
-    }
+  @Override
+  public boolean setPosition(final int position) {
+    this.position = position;
+    return position >= 1 && position <= attributes.size();
+  }
 }
