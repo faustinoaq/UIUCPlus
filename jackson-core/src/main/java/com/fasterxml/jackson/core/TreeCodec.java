b66704786487cbd8ae83e8df062156b65fbefb54
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:33:44 -0500

    NatGen-base.jackson-core.mid-496.idx-374.3.mutant

--- jackson-core/src/main/java/com/fasterxml/jackson/core/TreeCodec.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/TreeCodec.java
@@ -3,36 +3,47 @@ package com.fasterxml.jackson.core;
 import java.io.IOException;
 
 /**
- * Interface that defines objects that can read and write
- * {@link TreeNode} instances using Streaming API.
+ * Interface that defines objects that can read and write {@link TreeNode} instances using Streaming
+ * API.
  *
  * @since 2.3
  */
-public abstract class TreeCodec
-{
-    public abstract <T extends TreeNode> T readTree(JsonParser p) throws IOException, JsonProcessingException;
-    public abstract void writeTree(JsonGenerator g, TreeNode tree) throws IOException, JsonProcessingException;
+public abstract class TreeCodec {
+  public abstract <T extends TreeNode> T readTree(JsonParser p)
+      throws IOException, JsonProcessingException;
 
-    /**
-     * @return Node that represents "missing" node during traversal: something
-     *   referenced but that does not exist in content model
-     *
-     * @since 2.10
-     */
-    public TreeNode missingNode() {
-        return null;
+  public abstract void writeTree(JsonGenerator g, TreeNode tree) throws JsonProcessingException {
+    try {
+      StringWriter sw = new StringWriter();
+      JsonGenerator g1 = Json.createGenerator(sw);
+      g1.writeStartObject();
+      tree.toJson(g1);
+      g1.writeEndObject();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * @return Node that represents explicit {@code null} value in content
-     *
-     * @since 2.10
-     */
-    public TreeNode nullNode() {
-        return null;
-    }
+  /**
+   * @return Node that represents "missing" node during traversal: something referenced but that
+   *     does not exist in content model
+   * @since 2.10
+   */
+  public TreeNode missingNode() {
+    return null;
+  }
 
-    public abstract TreeNode createArrayNode();
-    public abstract TreeNode createObjectNode();
-    public abstract JsonParser treeAsTokens(TreeNode node);
+  /**
+   * @return Node that represents explicit {@code null} value in content
+   * @since 2.10
+   */
+  public TreeNode nullNode() {
+    return null;
+  }
+
+  public abstract TreeNode createArrayNode();
+
+  public abstract TreeNode createObjectNode();
+
+  public abstract JsonParser treeAsTokens(TreeNode node);
 }
