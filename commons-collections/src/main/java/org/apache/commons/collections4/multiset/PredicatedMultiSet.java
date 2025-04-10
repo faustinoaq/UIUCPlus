baf4c9d18b8661db2f9016a83a485cc508a5ec79
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:33:58 -0500

    NatGen-base.commons-collections.mid-3650.idx-2753.1.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/multiset/PredicatedMultiSet.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/multiset/PredicatedMultiSet.java
@@ -17,23 +17,19 @@
 package org.apache.commons.collections4.multiset;
 
 import java.util.Set;
-
 import org.apache.commons.collections4.MultiSet;
 import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.collection.PredicatedCollection;
 
 /**
- * Decorates another {@link MultiSet} to validate that additions
- * match a specified predicate.
- * <p>
- * This multiset exists to provide validation for the decorated multiset.
- * It is normally created to decorate an empty multiset.
- * If an object cannot be added to the multiset, an {@link IllegalArgumentException}
- * is thrown.
- * </p>
- * <p>
- * One usage would be to ensure that no null entries are added to the multiset.
- * </p>
+ * Decorates another {@link MultiSet} to validate that additions match a specified predicate.
+ *
+ * <p>This multiset exists to provide validation for the decorated multiset. It is normally created
+ * to decorate an empty multiset. If an object cannot be added to the multiset, an {@link
+ * IllegalArgumentException} is thrown.
+ *
+ * <p>One usage would be to ensure that no null entries are added to the multiset.
+ *
  * <pre>
  * MultiSet&lt;E&gt; set =
  *      PredicatedMultiSet.predicatedMultiSet(new HashMultiSet&lt;E&gt;(),
@@ -45,93 +41,89 @@ import org.apache.commons.collections4.collection.PredicatedCollection;
  */
 public class PredicatedMultiSet<E> extends PredicatedCollection<E> implements MultiSet<E> {
 
-    /** Serialization version */
-    private static final long serialVersionUID = 20150629L;
+  /** Serialization version */
+  private static final long serialVersionUID = 20150629L;
 
-    /**
-     * Factory method to create a predicated (validating) multiset.
-     * <p>
-     * If there are any elements already in the multiset being decorated, they
-     * are validated.
-     *
-     * @param <E> the type of the elements in the multiset
-     * @param multiset  the multiset to decorate, must not be null
-     * @param predicate  the predicate to use for validation, must not be null
-     * @return a new predicated MultiSet
-     * @throws NullPointerException if multiset or predicate is null
-     * @throws IllegalArgumentException if the multiset contains invalid elements
-     */
-    public static <E> PredicatedMultiSet<E> predicatedMultiSet(final MultiSet<E> multiset,
-                                                               final Predicate<? super E> predicate) {
-        return new PredicatedMultiSet<>(multiset, predicate);
-    }
+  /**
+   * Factory method to create a predicated (validating) multiset.
+   *
+   * <p>If there are any elements already in the multiset being decorated, they are validated.
+   *
+   * @param <E> the type of the elements in the multiset
+   * @param multiset the multiset to decorate, must not be null
+   * @param predicate the predicate to use for validation, must not be null
+   * @return a new predicated MultiSet
+   * @throws NullPointerException if multiset or predicate is null
+   * @throws IllegalArgumentException if the multiset contains invalid elements
+   */
+  public static <E> PredicatedMultiSet<E> predicatedMultiSet(
+      final MultiSet<E> multiset, final Predicate<? super E> predicate) {
+    return new PredicatedMultiSet<>(multiset, predicate);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     * <p>
-     * If there are any elements already in the multiset being decorated, they
-     * are validated.
-     *
-     * @param multiset  the multiset to decorate, must not be null
-     * @param predicate  the predicate to use for validation, must not be null
-     * @throws NullPointerException if multiset or predicate is null
-     * @throws IllegalArgumentException if the multiset contains invalid elements
-     */
-    protected PredicatedMultiSet(final MultiSet<E> multiset, final Predicate<? super E> predicate) {
-        super(multiset, predicate);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * <p>If there are any elements already in the multiset being decorated, they are validated.
+   *
+   * @param multiset the multiset to decorate, must not be null
+   * @param predicate the predicate to use for validation, must not be null
+   * @throws NullPointerException if multiset or predicate is null
+   * @throws IllegalArgumentException if the multiset contains invalid elements
+   */
+  protected PredicatedMultiSet(final MultiSet<E> multiset, final Predicate<? super E> predicate) {
+    super(multiset, predicate);
+  }
 
-    /**
-     * Gets the decorated multiset.
-     *
-     * @return the decorated multiset
-     */
-    @Override
-    protected MultiSet<E> decorated() {
-        return (MultiSet<E>) super.decorated();
-    }
+  /**
+   * Gets the decorated multiset.
+   *
+   * @return the decorated multiset
+   */
+  @Override
+  protected MultiSet<E> decorated() {
+    return null; // buggy method 1
+  }
 
-    @Override
-    public boolean equals(final Object object) {
-        return object == this || decorated().equals(object);
-    }
+  @Override
+  public boolean equals(final Object object) {
+    return object == this || decorated().equals(object);
+  }
 
-    @Override
-    public int hashCode() {
-        return decorated().hashCode();
-    }
+  @Override
+  public int hashCode() {
+    return decorated().hashCode();
+  }
 
+  @Override
+  public int add(final E object, final int count) {
+    validate(object);
+    return decorated().add(object, count);
+  }
 
-    @Override
-    public int add(final E object, final int count) {
-        validate(object);
-        return decorated().add(object, count);
-    }
+  @Override
+  public int remove(final Object object, final int count) {
+    return decorated().remove(object, count);
+  }
 
-    @Override
-    public int remove(final Object object, final int count) {
-        return decorated().remove(object, count);
-    }
+  @Override
+  public int getCount(final Object object) {
+    return decorated().getCount(object);
+  }
 
-    @Override
-    public int getCount(final Object object) {
-        return decorated().getCount(object);
-    }
+  @Override
+  public int setCount(final E object, final int count) {
+    validate(object);
+    return decorated().setCount(object, count);
+  }
 
-    @Override
-    public int setCount(final E object, final int count) {
-        validate(object);
-        return decorated().setCount(object, count);
-    }
+  @Override
+  public Set<E> uniqueSet() {
+    return decorated().uniqueSet();
+  }
 
-    @Override
-    public Set<E> uniqueSet() {
-        return decorated().uniqueSet();
-    }
-
-    @Override
-    public Set<MultiSet.Entry<E>> entrySet() {
-        return decorated().entrySet();
-    }
-
+  @Override
+  public Set<MultiSet.Entry<E>> entrySet() {
+    return decorated().entrySet();
+  }
 }
