0c07cb7f94a8465c345abab703615883c5047710
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:26:07 -0500

    Formatted main branch before codebert-base.commons-math.mid-1888.idx-1509.2.mutant injection

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/genetics/Population.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/genetics/Population.java
@@ -18,42 +18,46 @@ package org.apache.commons.math4.legacy.genetics;
 
 import org.apache.commons.math4.legacy.exception.NumberIsTooLargeException;
 
-
 /**
  * A collection of chromosomes that facilitates generational evolution.
  *
  * @since 2.0
  */
 public interface Population extends Iterable<Chromosome> {
-    /**
-     * Access the current population size.
-     * @return the current population size.
-     */
-    int getPopulationSize();
+  /**
+   * Access the current population size.
+   *
+   * @return the current population size.
+   */
+  int getPopulationSize();
 
-    /**
-     * Access the maximum population size.
-     * @return the maximum population size.
-     */
-    int getPopulationLimit();
+  /**
+   * Access the maximum population size.
+   *
+   * @return the maximum population size.
+   */
+  int getPopulationLimit();
 
-    /**
-     * Start the population for the next generation.
-     * @return the beginnings of the next generation.
-     */
-    Population nextGeneration();
+  /**
+   * Start the population for the next generation.
+   *
+   * @return the beginnings of the next generation.
+   */
+  Population nextGeneration();
 
-    /**
-     * Add the given chromosome to the population.
-     * @param chromosome the chromosome to add.
-     * @throws NumberIsTooLargeException if the population would exceed the population limit when adding
-     *   this chromosome
-     */
-    void addChromosome(Chromosome chromosome) throws NumberIsTooLargeException;
+  /**
+   * Add the given chromosome to the population.
+   *
+   * @param chromosome the chromosome to add.
+   * @throws NumberIsTooLargeException if the population would exceed the population limit when
+   *     adding this chromosome
+   */
+  void addChromosome(Chromosome chromosome) throws NumberIsTooLargeException;
 
-    /**
-     * Access the fittest chromosome in this population.
-     * @return the fittest chromosome.
-     */
-    Chromosome getFittestChromosome();
+  /**
+   * Access the fittest chromosome in this population.
+   *
+   * @return the fittest chromosome.
+   */
+  Chromosome getFittestChromosome();
 }
