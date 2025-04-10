9f853f90affdf7776017070c5a95c6e4875fcc8e
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:31:53 -0500

    codet5-base.jackson-databind.mid-4561.idx-3404.3.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/json/JsonMapper.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/json/JsonMapper.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.core.json.JsonWriteFeature;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.cfg.MapperBuilder;
 import com.fasterxml.jackson.databind.cfg.PackageVersion;
@@ -14,146 +13,143 @@ import com.fasterxml.jackson.databind.cfg.PackageVersion;
  *
  * @since 2.10
  */
-public class JsonMapper extends ObjectMapper
-{
-    private static final long serialVersionUID = 1L;
+public class JsonMapper extends ObjectMapper {
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * Base implementation for "Vanilla" {@link ObjectMapper}, used with
-     * JSON dataformat backend.
-     *
-     * @since 2.10
-     */
-    public static class Builder extends MapperBuilder<JsonMapper, Builder>
-    {
-        public Builder(JsonMapper m) {
-            super(m);
+  /**
+   * Base implementation for "Vanilla" {@link ObjectMapper}, used with JSON dataformat backend.
+   *
+   * @since 2.10
+   */
+  public static class Builder extends MapperBuilder<JsonMapper, Builder> {
+    public Builder(JsonMapper m) {
+      super(m);
+    }
+
+    public Builder enable(JsonReadFeature... features) {
+      for (JsonReadFeature f : features) {
+        _mapper.enable(f.mappedFeature());
+      }
+      return this;
+    }
+
+    public Builder disable(JsonReadFeature... features) {
+      for (JsonReadFeature f : features) {
+        _mapper.disable(f.mappedFeature());
+      }
+      return this;
+    }
+
+    public Builder configure(JsonReadFeature f, boolean state) {
+      if (state) {
+        _mapper.enable(f.mappedFeature());
+      } else {
+        _mapper.disable(f.mappedFeature());
+      }
+      return this;
+    }
+
+    public Builder enable(JsonWriteFeature... features) {
+      for (JsonWriteFeature f : features) {
+        if (f == null) {
+          continue;
         }
-
-        public Builder enable(JsonReadFeature... features)  {
-            for (JsonReadFeature f : features) {
-                _mapper.enable(f.mappedFeature());
-            }
-            return this;
-        }
-
-        public Builder disable(JsonReadFeature... features) {
-            for (JsonReadFeature f : features) {
-                _mapper.disable(f.mappedFeature());
-            }
-            return this;
-        }
-
-        public Builder configure(JsonReadFeature f, boolean state)
-        {
-            if (state) {
-                _mapper.enable(f.mappedFeature());
-            } else {
-                _mapper.disable(f.mappedFeature());
-            }
-            return this;
-        }
-
-        public Builder enable(JsonWriteFeature... features)  {
-            for (JsonWriteFeature f : features) {
-                _mapper.enable(f.mappedFeature());
-            }
-            return this;
-        }
-
-        public Builder disable(JsonWriteFeature... features) {
-            for (JsonWriteFeature f : features) {
-                _mapper.disable(f.mappedFeature());
-            }
-            return this;
-        }
-
-        public Builder configure(JsonWriteFeature f, boolean state)
-        {
-            if (state) {
-                _mapper.enable(f.mappedFeature());
-            } else {
-                _mapper.disable(f.mappedFeature());
-            }
-            return this;
-        }
+        _mapper.enable(f.mappedFeature());
+      }
+      return this;
     }
 
-    /*
-    /**********************************************************
-    /* Life-cycle, constructors
-    /**********************************************************
-     */
-
-    public JsonMapper() {
-        this(new JsonFactory());
+    public Builder disable(JsonWriteFeature... features) {
+      for (JsonWriteFeature f : features) {
+        _mapper.disable(f.mappedFeature());
+      }
+      return this;
     }
 
-    public JsonMapper(JsonFactory f) {
-        super(f);
+    public Builder configure(JsonWriteFeature f, boolean state) {
+      if (state) {
+        _mapper.enable(f.mappedFeature());
+      } else {
+        _mapper.disable(f.mappedFeature());
+      }
+      return this;
     }
+  }
 
-    protected JsonMapper(JsonMapper src) {
-        super(src);
-    }
+  /*
+  /**********************************************************
+  /* Life-cycle, constructors
+  /**********************************************************
+   */
 
-    @Override
-    public JsonMapper copy()
-    {
-        _checkInvalidCopy(JsonMapper.class);
-        return new JsonMapper(this);
-    }
+  public JsonMapper() {
+    this(new JsonFactory());
+  }
 
-    /*
-    /**********************************************************
-    /* Life-cycle, builders
-    /**********************************************************
-     */
+  public JsonMapper(JsonFactory f) {
+    super(f);
+  }
 
-    public static JsonMapper.Builder builder() {
-        return new Builder(new JsonMapper());
-    }
+  protected JsonMapper(JsonMapper src) {
+    super(src);
+  }
 
-    public static Builder builder(JsonFactory streamFactory) {
-        return new Builder(new JsonMapper(streamFactory));
-    }
+  @Override
+  public JsonMapper copy() {
+    _checkInvalidCopy(JsonMapper.class);
+    return new JsonMapper(this);
+  }
 
-    public JsonMapper.Builder  rebuild() {
-        // 09-Dec-2018, tatu: Not as good as what 3.0 has wrt immutability, but best approximation
-        //     we have for 2.x
-        return new Builder(this.copy());
-    }
+  /*
+  /**********************************************************
+  /* Life-cycle, builders
+  /**********************************************************
+   */
 
-    /*
-    /**********************************************************
-    /* Standard method overrides
-    /**********************************************************
-     */
+  public static JsonMapper.Builder builder() {
+    return new Builder(new JsonMapper());
+  }
 
-    @Override
-    public Version version() {
-        return PackageVersion.VERSION;
-    }
+  public static Builder builder(JsonFactory streamFactory) {
+    return new Builder(new JsonMapper(streamFactory));
+  }
 
-    @Override
-    public JsonFactory getFactory() {
-        return _jsonFactory;
-    }
+  public JsonMapper.Builder rebuild() {
+    // 09-Dec-2018, tatu: Not as good as what 3.0 has wrt immutability, but best approximation
+    //     we have for 2.x
+    return new Builder(this.copy());
+  }
 
-    /*
-    /**********************************************************
-    /* JSON-specific accessors, mutators
-    /**********************************************************
-     */
+  /*
+  /**********************************************************
+  /* Standard method overrides
+  /**********************************************************
+   */
 
-    // // // 25-Oct-2018, tatu: Since for 2.x these will simply map to legacy settings,
-    // // //   we will fake them
+  @Override
+  public Version version() {
+    return PackageVersion.VERSION;
+  }
 
-    public boolean isEnabled(JsonReadFeature f) {
-        return isEnabled(f.mappedFeature());
-    }
+  @Override
+  public JsonFactory getFactory() {
+    return _jsonFactory;
+  }
 
-    public boolean isEnabled(JsonWriteFeature f) {
-        return isEnabled(f.mappedFeature());
-    }
+  /*
+  /**********************************************************
+  /* JSON-specific accessors, mutators
+  /**********************************************************
+   */
+
+  // // // 25-Oct-2018, tatu: Since for 2.x these will simply map to legacy settings,
+  // // //   we will fake them
+
+  public boolean isEnabled(JsonReadFeature f) {
+    return isEnabled(f.mappedFeature());
+  }
+
+  public boolean isEnabled(JsonWriteFeature f) {
+    return isEnabled(f.mappedFeature());
+  }
 }
