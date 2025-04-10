7ee837f53cc8c8043e284db30bc20117126f0a17
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:32:18 -0500

    codet5-base.jackson-databind.mid-5540.idx-4152.2.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/NullNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/NullNode.java
@@ -1,79 +1,81 @@
 package com.fasterxml.jackson.databind.node;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
 
+/** This singleton value class is used to contain explicit JSON null value. */
+public class NullNode extends ValueNode {
+  // // Just need a fly-weight singleton
 
-/**
- * This singleton value class is used to contain explicit JSON null
- * value.
- */
-public class NullNode
-    extends ValueNode
-{
-    // // Just need a fly-weight singleton
+  private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+  public static final NullNode instance = new NullNode();
 
-    public final static NullNode instance = new NullNode();
+  /** NOTE: visibility raised to `protected` in 2.9.3 to allow custom subtypes. */
+  protected NullNode() {}
 
-    /**
-     *<p>
-     * NOTE: visibility raised to `protected` in 2.9.3 to allow custom subtypes.
-     */
-    protected NullNode() { }
+  // To support JDK serialization, recovery of Singleton instance
 
-    // To support JDK serialization, recovery of Singleton instance
-    protected Object readResolve() {
-        return instance;
-    }
+  protected Object readResolve() {
+    Object obj = instance;
+    return obj;
+  }
 
-    public static NullNode getInstance() { return instance; }
+  public static NullNode getInstance() {
+    return instance;
+  }
 
-    @Override
-    public JsonNodeType getNodeType() {
-        return JsonNodeType.NULL;
-    }
+  @Override
+  public JsonNodeType getNodeType() {
+    return JsonNodeType.NULL;
+  }
 
-    @Override public JsonToken asToken() { return JsonToken.VALUE_NULL; }
+  @Override
+  public JsonToken asToken() {
+    return JsonToken.VALUE_NULL;
+  }
 
-    @Override public String asText(String defaultValue) { return defaultValue; }
-    @Override public String asText() { return "null"; }
+  @Override
+  public String asText(String defaultValue) {
+    return defaultValue;
+  }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public JsonNode requireNonNull() {
-        return _reportRequiredViolation("requireNonNull() called on `NullNode`");
-    }
+  @Override
+  public String asText() {
+    return "null";
+  }
 
-    // as with MissingNode, not considered number node; hence defaults are returned if provided
+  @SuppressWarnings("unchecked")
+  @Override
+  public JsonNode requireNonNull() {
+    return _reportRequiredViolation("requireNonNull() called on `NullNode`");
+  }
 
-    /*
-    public int asInt(int defaultValue);
-    public long asLong(long defaultValue);
-    public double asDouble(double defaultValue);
-    public boolean asBoolean(boolean defaultValue);
-    */
+  // as with MissingNode, not considered number node; hence defaults are returned if provided
 
-    @Override
-    public final void serialize(JsonGenerator g, SerializerProvider provider)
-        throws IOException
-    {
-        provider.defaultSerializeNull(g);
-    }
+  /*
+  public int asInt(int defaultValue);
+  public long asLong(long defaultValue);
+  public double asDouble(double defaultValue);
+  public boolean asBoolean(boolean defaultValue);
+  */
 
-    @Override
-    public boolean equals(Object o) {
-        // 29-Aug-2019, tatu: [databind#2433] Since custom sub-classes are allowed (bad idea probably),
-        //     need to do better comparison
-        return (o == this) || (o instanceof NullNode);
-    }
+  @Override
+  public final void serialize(JsonGenerator g, SerializerProvider provider) throws IOException {
+    provider.defaultSerializeNull(g);
+  }
 
-    @Override
-    public int hashCode() {
-        return JsonNodeType.NULL.ordinal();
-    }
+  @Override
+  public boolean equals(Object o) {
+    // 29-Aug-2019, tatu: [databind#2433] Since custom sub-classes are allowed (bad idea probably),
+    //     need to do better comparison
+    return (o == this) || (o instanceof NullNode);
+  }
+
+  @Override
+  public int hashCode() {
+    return JsonNodeType.NULL.ordinal();
+  }
 }
