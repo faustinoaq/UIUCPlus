91d7758f897968d67bd6ee706e85ee292adb312a
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:32:24 -0500

    Formatted main branch before codet5-base.commons-collections.mid-667.idx-457.1.mutant injection

--- commons-collections/src/main/java/org/apache/commons/collections4/bag/AbstractSortedBagDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bag/AbstractSortedBagDecorator.java
@@ -17,65 +17,61 @@
 package org.apache.commons.collections4.bag;
 
 import java.util.Comparator;
-
 import org.apache.commons.collections4.SortedBag;
 
 /**
  * Decorates another {@code SortedBag} to provide additional behavior.
- * <p>
- * Methods are forwarded directly to the decorated bag.
- * </p>
+ *
+ * <p>Methods are forwarded directly to the decorated bag.
  *
  * @param <E> the type of elements in this bag
  * @since 3.0
  */
-public abstract class AbstractSortedBagDecorator<E>
-        extends AbstractBagDecorator<E> implements SortedBag<E> {
+public abstract class AbstractSortedBagDecorator<E> extends AbstractBagDecorator<E>
+    implements SortedBag<E> {
 
-    /** Serialization version */
-    private static final long serialVersionUID = -8223473624050467718L;
+  /** Serialization version */
+  private static final long serialVersionUID = -8223473624050467718L;
 
-    /**
-     * Constructor only used in deserialization, do not use otherwise.
-     * @since 3.1
-     */
-    protected AbstractSortedBagDecorator() {
-    }
+  /**
+   * Constructor only used in deserialization, do not use otherwise.
+   *
+   * @since 3.1
+   */
+  protected AbstractSortedBagDecorator() {}
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param bag  the bag to decorate, must not be null
-     * @throws NullPointerException if bag is null
-     */
-    protected AbstractSortedBagDecorator(final SortedBag<E> bag) {
-        super(bag);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param bag the bag to decorate, must not be null
+   * @throws NullPointerException if bag is null
+   */
+  protected AbstractSortedBagDecorator(final SortedBag<E> bag) {
+    super(bag);
+  }
 
-    /**
-     * Gets the bag being decorated.
-     *
-     * @return the decorated bag
-     */
-    @Override
-    protected SortedBag<E> decorated() {
-        return (SortedBag<E>) super.decorated();
-    }
+  /**
+   * Gets the bag being decorated.
+   *
+   * @return the decorated bag
+   */
+  @Override
+  protected SortedBag<E> decorated() {
+    return (SortedBag<E>) super.decorated();
+  }
 
+  @Override
+  public E first() {
+    return decorated().first();
+  }
 
-    @Override
-    public E first() {
-        return decorated().first();
-    }
+  @Override
+  public E last() {
+    return decorated().last();
+  }
 
-    @Override
-    public E last() {
-        return decorated().last();
-    }
-
-    @Override
-    public Comparator<? super E> comparator() {
-        return decorated().comparator();
-    }
-
+  @Override
+  public Comparator<? super E> comparator() {
+    return decorated().comparator();
+  }
 }
