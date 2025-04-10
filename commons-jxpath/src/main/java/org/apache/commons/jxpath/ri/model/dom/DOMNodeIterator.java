8d823d1506715932f6a91ab7f89912da41c93fe7
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:18:48 -0500

    Formatted main branch before mubert-base.commons-jxpath.mid-746.idx-747.301.mutant injection

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMNodeIterator.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dom/DOMNodeIterator.java
@@ -21,141 +21,135 @@ import org.apache.commons.jxpath.ri.model.NodeIterator;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 import org.w3c.dom.Node;
 
-/**
- * An iterator of children of a DOM Node.
- */
+/** An iterator of children of a DOM Node. */
 public class DOMNodeIterator implements NodeIterator {
-    private final NodePointer parent;
-    private final NodeTest nodeTest;
-    private final Node node;
-    private Node child = null;
-    private final boolean reverse;
-    private int position = 0;
+  private final NodePointer parent;
+  private final NodeTest nodeTest;
+  private final Node node;
+  private Node child = null;
+  private final boolean reverse;
+  private int position = 0;
 
-    /**
-     * Create a new DOMNodeIterator.
-     * @param parent parent pointer
-     * @param nodeTest test
-     * @param reverse whether to iterate in reverse
-     * @param startWith starting pointer
-     */
-    public DOMNodeIterator(
-        final NodePointer parent,
-        final NodeTest nodeTest,
-        final boolean reverse,
-        final NodePointer startWith) {
-        this.parent = parent;
-        this.node = (Node) parent.getNode();
-        if (startWith != null) {
-            this.child = (Node) startWith.getNode();
-        }
-        this.nodeTest = nodeTest;
-        this.reverse = reverse;
+  /**
+   * Create a new DOMNodeIterator.
+   *
+   * @param parent parent pointer
+   * @param nodeTest test
+   * @param reverse whether to iterate in reverse
+   * @param startWith starting pointer
+   */
+  public DOMNodeIterator(
+      final NodePointer parent,
+      final NodeTest nodeTest,
+      final boolean reverse,
+      final NodePointer startWith) {
+    this.parent = parent;
+    this.node = (Node) parent.getNode();
+    if (startWith != null) {
+      this.child = (Node) startWith.getNode();
     }
+    this.nodeTest = nodeTest;
+    this.reverse = reverse;
+  }
 
-    @Override
-    public NodePointer getNodePointer() {
-        if (position == 0) {
-            setPosition(1);
-        }
-        return child == null ? null : new DOMNodePointer(parent, child);
+  @Override
+  public NodePointer getNodePointer() {
+    if (position == 0) {
+      setPosition(1);
     }
+    return child == null ? null : new DOMNodePointer(parent, child);
+  }
 
-    @Override
-    public int getPosition() {
-        return position;
-    }
+  @Override
+  public int getPosition() {
+    return position;
+  }
 
-    @Override
-    public boolean setPosition(final int position) {
-        while (this.position < position) {
-            if (!next()) {
-                return false;
-            }
-        }
-        while (this.position > position) {
-            if (!previous()) {
-                return false;
-            }
-        }
-        return true;
+  @Override
+  public boolean setPosition(final int position) {
+    while (this.position < position) {
+      if (!next()) {
+        return false;
+      }
     }
+    while (this.position > position) {
+      if (!previous()) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    /**
-     * Set the previous position.
-     * @return whether valid
-     */
-    private boolean previous() {
-        position--;
-        if (!reverse) {
-            if (position == 0) {
-                child = null;
-            }
-            else if (child == null) {
-                child = node.getLastChild();
-            }
-            else {
-                child = child.getPreviousSibling();
-            }
-            while (child != null && !testChild()) {
-                child = child.getPreviousSibling();
-            }
-        }
-        else {
-            child = child.getNextSibling();
-            while (child != null && !testChild()) {
-                child = child.getNextSibling();
-            }
-        }
-        return child != null;
+  /**
+   * Set the previous position.
+   *
+   * @return whether valid
+   */
+  private boolean previous() {
+    position--;
+    if (!reverse) {
+      if (position == 0) {
+        child = null;
+      } else if (child == null) {
+        child = node.getLastChild();
+      } else {
+        child = child.getPreviousSibling();
+      }
+      while (child != null && !testChild()) {
+        child = child.getPreviousSibling();
+      }
+    } else {
+      child = child.getNextSibling();
+      while (child != null && !testChild()) {
+        child = child.getNextSibling();
+      }
     }
+    return child != null;
+  }
 
-    /**
-     * Set the next position.
-     * @return whether valid
-     */
-    private boolean next() {
-        position++;
-        if (!reverse) {
-            if (position == 1) {
-                if (child == null) {
-                    child = node.getFirstChild();
-                }
-                else {
-                    child = child.getNextSibling();
-                }
-            }
-            else {
-                child = child.getNextSibling();
-            }
-            while (child != null && !testChild()) {
-                child = child.getNextSibling();
-            }
+  /**
+   * Set the next position.
+   *
+   * @return whether valid
+   */
+  private boolean next() {
+    position++;
+    if (!reverse) {
+      if (position == 1) {
+        if (child == null) {
+          child = node.getFirstChild();
+        } else {
+          child = child.getNextSibling();
         }
-        else {
-            if (position == 1) {
-                if (child == null) {
-                    child = node.getLastChild();
-                }
-                else {
-                    child = child.getPreviousSibling();
-                }
-            }
-            else {
-                child = child.getPreviousSibling();
-            }
-            while (child != null && !testChild()) {
-                child = child.getPreviousSibling();
-            }
+      } else {
+        child = child.getNextSibling();
+      }
+      while (child != null && !testChild()) {
+        child = child.getNextSibling();
+      }
+    } else {
+      if (position == 1) {
+        if (child == null) {
+          child = node.getLastChild();
+        } else {
+          child = child.getPreviousSibling();
         }
-        return child != null;
+      } else {
+        child = child.getPreviousSibling();
+      }
+      while (child != null && !testChild()) {
+        child = child.getPreviousSibling();
+      }
     }
+    return child != null;
+  }
 
-    /**
-     * Test child.
-     * @return result of the test
-     */
-    private boolean testChild() {
-        return DOMNodePointer.testNode(child, nodeTest);
-    }
+  /**
+   * Test child.
+   *
+   * @return result of the test
+   */
+  private boolean testChild() {
+    return DOMNodePointer.testNode(child, nodeTest);
+  }
 }
