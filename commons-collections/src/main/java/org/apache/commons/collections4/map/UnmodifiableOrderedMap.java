0876d1f6c029027793b022de2c17b1bb770eb2f3
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:27:08 -0500

    Formatted main branch before codebert-base.commons-collections.mid-3389.idx-2786.1.mutant injection

--- commons-collections/src/main/java/org/apache/commons/collections4/map/UnmodifiableOrderedMap.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/map/UnmodifiableOrderedMap.java
@@ -23,7 +23,6 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-
 import org.apache.commons.collections4.OrderedMap;
 import org.apache.commons.collections4.OrderedMapIterator;
 import org.apache.commons.collections4.Unmodifiable;
@@ -33,121 +32,119 @@ import org.apache.commons.collections4.set.UnmodifiableSet;
 
 /**
  * Decorates another {@code OrderedMap} to ensure it can't be altered.
- * <p>
- * This class is Serializable from Commons Collections 3.1.
- * </p>
- * <p>
- * Attempts to modify it will result in an UnsupportedOperationException.
- * </p>
+ *
+ * <p>This class is Serializable from Commons Collections 3.1.
+ *
+ * <p>Attempts to modify it will result in an UnsupportedOperationException.
  *
  * @param <K> the type of the keys in this map
  * @param <V> the type of the values in this map
  * @since 3.0
  */
-public final class UnmodifiableOrderedMap<K, V> extends AbstractOrderedMapDecorator<K, V> implements
-        Unmodifiable, Serializable {
+public final class UnmodifiableOrderedMap<K, V> extends AbstractOrderedMapDecorator<K, V>
+    implements Unmodifiable, Serializable {
 
-    /** Serialization version */
-    private static final long serialVersionUID = 8136428161720526266L;
+  /** Serialization version */
+  private static final long serialVersionUID = 8136428161720526266L;
 
-    /**
-     * Factory method to create an unmodifiable sorted map.
-     *
-     * @param <K>  the key type
-     * @param <V>  the value type
-     * @param map  the map to decorate, must not be null
-     * @return a new ordered map
-     * @throws NullPointerException if map is null
-     * @since 4.0
-     */
-    public static <K, V> OrderedMap<K, V> unmodifiableOrderedMap(final OrderedMap<? extends K, ? extends V> map) {
-        if (map instanceof Unmodifiable) {
-            @SuppressWarnings("unchecked") // safe to upcast
-            final OrderedMap<K, V> tmpMap = (OrderedMap<K, V>) map;
-            return tmpMap;
-        }
-        return new UnmodifiableOrderedMap<>(map);
+  /**
+   * Factory method to create an unmodifiable sorted map.
+   *
+   * @param <K> the key type
+   * @param <V> the value type
+   * @param map the map to decorate, must not be null
+   * @return a new ordered map
+   * @throws NullPointerException if map is null
+   * @since 4.0
+   */
+  public static <K, V> OrderedMap<K, V> unmodifiableOrderedMap(
+      final OrderedMap<? extends K, ? extends V> map) {
+    if (map instanceof Unmodifiable) {
+      @SuppressWarnings("unchecked") // safe to upcast
+      final OrderedMap<K, V> tmpMap = (OrderedMap<K, V>) map;
+      return tmpMap;
     }
+    return new UnmodifiableOrderedMap<>(map);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param map  the map to decorate, must not be null
-     * @throws NullPointerException if map is null
-     */
-    @SuppressWarnings("unchecked") // safe to upcast
-    private UnmodifiableOrderedMap(final OrderedMap<? extends K, ? extends V> map) {
-        super((OrderedMap<K, V>) map);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param map the map to decorate, must not be null
+   * @throws NullPointerException if map is null
+   */
+  @SuppressWarnings("unchecked") // safe to upcast
+  private UnmodifiableOrderedMap(final OrderedMap<? extends K, ? extends V> map) {
+    super((OrderedMap<K, V>) map);
+  }
 
-    /**
-     * Write the map out using a custom routine.
-     *
-     * @param out  the output stream
-     * @throws IOException if an error occurs while writing to the stream
-     * @since 3.1
-     */
-    private void writeObject(final ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
-        out.writeObject(map);
-    }
+  /**
+   * Write the map out using a custom routine.
+   *
+   * @param out the output stream
+   * @throws IOException if an error occurs while writing to the stream
+   * @since 3.1
+   */
+  private void writeObject(final ObjectOutputStream out) throws IOException {
+    out.defaultWriteObject();
+    out.writeObject(map);
+  }
 
-    /**
-     * Read the map in using a custom routine.
-     *
-     * @param in  the input stream
-     * @throws IOException if an error occurs while reading from the stream
-     * @throws ClassNotFoundException if an object read from the stream can not be loaded
-     * @since 3.1
-     */
-    @SuppressWarnings("unchecked") // (1) should only fail if input stream is incorrect
-    private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        map = (Map<K, V>) in.readObject(); // (1)
-    }
+  /**
+   * Read the map in using a custom routine.
+   *
+   * @param in the input stream
+   * @throws IOException if an error occurs while reading from the stream
+   * @throws ClassNotFoundException if an object read from the stream can not be loaded
+   * @since 3.1
+   */
+  @SuppressWarnings("unchecked") // (1) should only fail if input stream is incorrect
+  private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    map = (Map<K, V>) in.readObject(); // (1)
+  }
 
-    @Override
-    public OrderedMapIterator<K, V> mapIterator() {
-        final OrderedMapIterator<K, V> it = decorated().mapIterator();
-        return UnmodifiableOrderedMapIterator.unmodifiableOrderedMapIterator(it);
-    }
+  @Override
+  public OrderedMapIterator<K, V> mapIterator() {
+    final OrderedMapIterator<K, V> it = decorated().mapIterator();
+    return UnmodifiableOrderedMapIterator.unmodifiableOrderedMapIterator(it);
+  }
 
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public V put(final K key, final V value) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public V put(final K key, final V value) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public V remove(final Object key) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public V remove(final Object key) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public Set<Map.Entry<K, V>> entrySet() {
-        final Set<Map.Entry<K, V>> set = super.entrySet();
-        return UnmodifiableEntrySet.unmodifiableEntrySet(set);
-    }
+  @Override
+  public Set<Map.Entry<K, V>> entrySet() {
+    final Set<Map.Entry<K, V>> set = super.entrySet();
+    return UnmodifiableEntrySet.unmodifiableEntrySet(set);
+  }
 
-    @Override
-    public Set<K> keySet() {
-        final Set<K> set = super.keySet();
-        return UnmodifiableSet.unmodifiableSet(set);
-    }
+  @Override
+  public Set<K> keySet() {
+    final Set<K> set = super.keySet();
+    return UnmodifiableSet.unmodifiableSet(set);
+  }
 
-    @Override
-    public Collection<V> values() {
-        final Collection<V> coll = super.values();
-        return UnmodifiableCollection.unmodifiableCollection(coll);
-    }
-
+  @Override
+  public Collection<V> values() {
+    final Collection<V> coll = super.values();
+    return UnmodifiableCollection.unmodifiableCollection(coll);
+  }
 }
