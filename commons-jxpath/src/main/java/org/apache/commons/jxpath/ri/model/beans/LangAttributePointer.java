5a62479602f43e07416f2de2ed690b09945b7dff
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:34:49 -0500

    Formatted main branch before NatGen-base.commons-jxpath.mid-750.idx-532.3.mutant injection

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/LangAttributePointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/beans/LangAttributePointer.java
@@ -21,102 +21,100 @@ import org.apache.commons.jxpath.ri.compiler.NodeTest;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 
 /**
- * A Pointer that points to the "lang" attribute of a JavaBean. The value
- * of the attribute is based on the locale supplied to it in the constructor.
+ * A Pointer that points to the "lang" attribute of a JavaBean. The value of the attribute is based
+ * on the locale supplied to it in the constructor.
  */
 public class LangAttributePointer extends NodePointer {
 
-    private static final long serialVersionUID = -8665319197100034134L;
+  private static final long serialVersionUID = -8665319197100034134L;
 
-    /**
-     * Create a new LangAttributePointer.
-     * @param parent parent pointer.
-     */
-    public LangAttributePointer(final NodePointer parent) {
-        super(parent);
-    }
+  /**
+   * Create a new LangAttributePointer.
+   *
+   * @param parent parent pointer.
+   */
+  public LangAttributePointer(final NodePointer parent) {
+    super(parent);
+  }
 
-    @Override
-    public QName getName() {
-        return new QName("xml", "lang");
-    }
+  @Override
+  public QName getName() {
+    return new QName("xml", "lang");
+  }
 
-    @Override
-    public String getNamespaceURI() {
-        return null;
-    }
+  @Override
+  public String getNamespaceURI() {
+    return null;
+  }
 
-    @Override
-    public boolean isCollection() {
-        return false;
-    }
+  @Override
+  public boolean isCollection() {
+    return false;
+  }
 
-    @Override
-    public int getLength() {
-        return 1;
-    }
+  @Override
+  public int getLength() {
+    return 1;
+  }
 
-    @Override
-    public Object getBaseValue() {
-        return parent.getLocale().toString().replace('_', '-');
-    }
+  @Override
+  public Object getBaseValue() {
+    return parent.getLocale().toString().replace('_', '-');
+  }
 
-    @Override
-    public Object getImmediateNode() {
-        return getBaseValue();
-    }
+  @Override
+  public Object getImmediateNode() {
+    return getBaseValue();
+  }
 
-    @Override
-    public boolean isLeaf() {
-        return true;
-    }
+  @Override
+  public boolean isLeaf() {
+    return true;
+  }
 
-    /**
-     * {@inheritDoc}
-     *
-     * Throws UnsupportedOperationException.
-     * @param value Object
-     */
-    @Override
-    public void setValue(final Object value) {
-        throw new UnsupportedOperationException(
-                "Cannot change locale using the 'lang' attribute");
-    }
+  /**
+   * {@inheritDoc}
+   *
+   * <p>Throws UnsupportedOperationException.
+   *
+   * @param value Object
+   */
+  @Override
+  public void setValue(final Object value) {
+    throw new UnsupportedOperationException("Cannot change locale using the 'lang' attribute");
+  }
 
-    @Override
-    public String asPath() {
-        final StringBuffer buffer = new StringBuffer();
-        if (parent != null) {
-            buffer.append(parent.asPath());
-            if (buffer.length() == 0
-                || buffer.charAt(buffer.length() - 1) != '/') {
-                buffer.append('/');
-            }
-        }
-        buffer.append("@xml:lang");
-        return buffer.toString();
+  @Override
+  public String asPath() {
+    final StringBuffer buffer = new StringBuffer();
+    if (parent != null) {
+      buffer.append(parent.asPath());
+      if (buffer.length() == 0 || buffer.charAt(buffer.length() - 1) != '/') {
+        buffer.append('/');
+      }
     }
+    buffer.append("@xml:lang");
+    return buffer.toString();
+  }
 
-    @Override
-    public int hashCode() {
-        return 0;
-    }
+  @Override
+  public int hashCode() {
+    return 0;
+  }
 
-    @Override
-    public boolean equals(final Object object) {
-        return object instanceof LangAttributePointer;
-    }
+  @Override
+  public boolean equals(final Object object) {
+    return object instanceof LangAttributePointer;
+  }
 
-    @Override
-    public boolean testNode(final NodeTest test) {
-        return false;
-    }
+  @Override
+  public boolean testNode(final NodeTest test) {
+    return false;
+  }
 
-    @Override
-    public int compareChildNodePointers(
-        final NodePointer pointer1,
-        final NodePointer pointer2) {
-        // Won't happen - lang attributes don't have children
-        return 0;
-    }
+  @Override
+  public int compareChildNodePointers(final NodePointer pointer1, final NodePointer pointer2) {
+    // Won't happen - lang attributes don't have children
+    return 0;
+  }
 }
