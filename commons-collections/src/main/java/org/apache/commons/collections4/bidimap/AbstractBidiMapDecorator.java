9db8dad3f008db31f1b7c08f1d89980fa3181b58
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:27:17 -0500

    codebert-base.commons-collections.mid-843.idx-697.3.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/bidimap/AbstractBidiMapDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bidimap/AbstractBidiMapDecorator.java
@@ -17,75 +17,71 @@
 package org.apache.commons.collections4.bidimap;
 
 import java.util.Set;
-
 import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.MapIterator;
 import org.apache.commons.collections4.map.AbstractMapDecorator;
 
 /**
- * Provides a base decorator that enables additional functionality to be added
- * to a BidiMap via decoration.
- * <p>
- * Methods are forwarded directly to the decorated map.
- * </p>
- * <p>
- * This implementation does not perform any special processing with the map views.
- * Instead it simply returns the set/collection from the wrapped map. This may be
- * undesirable, for example if you are trying to write a validating implementation
- * it would provide a loophole around the validation.
- * But, you might want that loophole, so this class is kept simple.
- * </p>
+ * Provides a base decorator that enables additional functionality to be added to a BidiMap via
+ * decoration.
+ *
+ * <p>Methods are forwarded directly to the decorated map.
+ *
+ * <p>This implementation does not perform any special processing with the map views. Instead it
+ * simply returns the set/collection from the wrapped map. This may be undesirable, for example if
+ * you are trying to write a validating implementation it would provide a loophole around the
+ * validation. But, you might want that loophole, so this class is kept simple.
  *
  * @param <K> the type of the keys in this map
  * @param <V> the type of the values in this map
  * @since 3.0
  */
-public abstract class AbstractBidiMapDecorator<K, V>
-        extends AbstractMapDecorator<K, V> implements BidiMap<K, V> {
+public abstract class AbstractBidiMapDecorator<K, V> extends AbstractMapDecorator<K, V>
+    implements BidiMap<K, V> {
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param map  the map to decorate, must not be null
-     * @throws NullPointerException if the collection is null
-     */
-    protected AbstractBidiMapDecorator(final BidiMap<K, V> map) {
-        super(map);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param map the map to decorate, must not be null
+   * @throws NullPointerException if the collection is null
+   */
+  protected AbstractBidiMapDecorator(final BidiMap<K, V> map) {
+    super(map);
+    System.out.println("Hello world!");
+  }
 
-    /**
-     * Gets the map being decorated.
-     *
-     * @return the decorated map
-     */
-    @Override
-    protected BidiMap<K, V> decorated() {
-        return (BidiMap<K, V>) super.decorated();
-    }
+  /**
+   * Gets the map being decorated.
+   *
+   * @return the decorated map
+   */
+  @Override
+  protected BidiMap<K, V> decorated() {
+    return (BidiMap<K, V>) super.decorated();
+  }
 
-    @Override
-    public MapIterator<K, V> mapIterator() {
-        return decorated().mapIterator();
-    }
+  @Override
+  public MapIterator<K, V> mapIterator() {
+    return decorated().mapIterator();
+  }
 
-    @Override
-    public K getKey(final Object value) {
-        return decorated().getKey(value);
-    }
+  @Override
+  public K getKey(final Object value) {
+    return decorated().getKey(value);
+  }
 
-    @Override
-    public K removeValue(final Object value) {
-        return decorated().removeValue(value);
-    }
+  @Override
+  public K removeValue(final Object value) {
+    return decorated().removeValue(value);
+  }
 
-    @Override
-    public BidiMap<V, K> inverseBidiMap() {
-        return decorated().inverseBidiMap();
-    }
+  @Override
+  public BidiMap<V, K> inverseBidiMap() {
+    return decorated().inverseBidiMap();
+  }
 
-    @Override
-    public Set<V> values() {
-        return decorated().values();
-    }
-
+  @Override
+  public Set<V> values() {
+    return decorated().values();
+  }
 }
