feac01a0bae1c812e21fb0284c71716af6f9e7d8
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:19:56 -0500

    Formatted main branch before leam-base.commons-math.mid-1873.idx-1874.24.mutant injection

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/stat/descriptive/rank/Max.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/stat/descriptive/rank/Max.java
@@ -16,148 +16,135 @@
  */
 package org.apache.commons.math4.legacy.stat.descriptive.rank;
 
+import org.apache.commons.math4.legacy.core.MathArrays;
 import org.apache.commons.math4.legacy.exception.MathIllegalArgumentException;
 import org.apache.commons.math4.legacy.exception.NullArgumentException;
 import org.apache.commons.math4.legacy.stat.descriptive.AbstractStorelessUnivariateStatistic;
-import org.apache.commons.math4.legacy.core.MathArrays;
 
 /**
  * Returns the maximum of the available values.
+ *
  * <ul>
- * <li>The result is <code>NaN</code> iff all values are <code>NaN</code>
- * (i.e. <code>NaN</code> values have no impact on the value of the statistic).</li>
- * <li>If any of the values equals <code>Double.POSITIVE_INFINITY</code>,
- * the result is <code>Double.POSITIVE_INFINITY.</code></li>
+ *   <li>The result is <code>NaN</code> iff all values are <code>NaN</code> (i.e. <code>NaN</code>
+ *       values have no impact on the value of the statistic).
+ *   <li>If any of the values equals <code>Double.POSITIVE_INFINITY</code>, the result is <code>
+ *       Double.POSITIVE_INFINITY.</code>
  * </ul>
-* <p>
- * <strong>Note that this implementation is not synchronized.</strong> If
- * multiple threads access an instance of this class concurrently, and at least
- * one of the threads invokes the <code>increment()</code> or
- * <code>clear()</code> method, it must be synchronized externally.</p>
+ *
+ * <p><strong>Note that this implementation is not synchronized.</strong> If multiple threads access
+ * an instance of this class concurrently, and at least one of the threads invokes the <code>
+ * increment()</code> or <code>clear()</code> method, it must be synchronized externally.
  */
 public class Max extends AbstractStorelessUnivariateStatistic {
-    /** Number of values that have been added. */
-    private long n;
+  /** Number of values that have been added. */
+  private long n;
 
-    /** Current value of the statistic. */
-    private double value;
+  /** Current value of the statistic. */
+  private double value;
 
-    /**
-     * Create a Max instance.
-     */
-    public Max() {
-        n = 0;
-        value = Double.NaN;
+  /** Create a Max instance. */
+  public Max() {
+    n = 0;
+    value = Double.NaN;
+  }
+
+  /**
+   * Copy constructor, creates a new {@code Max} identical to the {@code original}.
+   *
+   * @param original the {@code Max} instance to copy
+   * @throws NullArgumentException if original is null
+   */
+  public Max(Max original) throws NullArgumentException {
+    copy(original, this);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void increment(final double d) {
+    if (d > value || Double.isNaN(value)) {
+      value = d;
     }
+    n++;
+  }
 
-    /**
-     * Copy constructor, creates a new {@code Max} identical
-     * to the {@code original}.
-     *
-     * @param original the {@code Max} instance to copy
-     * @throws NullArgumentException if original is null
-     */
-    public Max(Max original) throws NullArgumentException {
-        copy(original, this);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void clear() {
+    value = Double.NaN;
+    n = 0;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void increment(final double d) {
-        if (d > value || Double.isNaN(value)) {
-            value = d;
+  /** {@inheritDoc} */
+  @Override
+  public double getResult() {
+    return value;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public long getN() {
+    return n;
+  }
+
+  /**
+   * Returns the maximum of the entries in the specified portion of the input array, or <code>
+   * Double.NaN</code> if the designated subarray is empty.
+   *
+   * <p>Throws <code>MathIllegalArgumentException</code> if the array is null or the array index
+   * parameters are not valid.
+   *
+   * <ul>
+   *   <li>The result is <code>NaN</code> iff all values are <code>NaN</code> (i.e. <code>NaN</code>
+   *       values have no impact on the value of the statistic).
+   *   <li>If any of the values equals <code>Double.POSITIVE_INFINITY</code>, the result is <code>
+   *       Double.POSITIVE_INFINITY.</code>
+   * </ul>
+   *
+   * @param values the input array
+   * @param begin index of the first array element to include
+   * @param length the number of elements to include
+   * @return the maximum of the values or Double.NaN if length = 0
+   * @throws MathIllegalArgumentException if the array is null or the array index parameters are not
+   *     valid
+   */
+  @Override
+  public double evaluate(final double[] values, final int begin, final int length)
+      throws MathIllegalArgumentException {
+
+    double max = Double.NaN;
+    if (MathArrays.verifyValues(values, begin, length)) {
+      max = values[begin];
+      for (int i = begin; i < begin + length; i++) {
+        if (!Double.isNaN(values[i])) {
+          max = (max > values[i]) ? max : values[i];
         }
-        n++;
+      }
     }
+    return max;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void clear() {
-        value = Double.NaN;
-        n = 0;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public Max copy() {
+    Max result = new Max();
+    // No try-catch or advertised exception because args are non-null
+    copy(this, result);
+    return result;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public double getResult() {
-        return value;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public long getN() {
-        return n;
-    }
-
-    /**
-     * Returns the maximum of the entries in the specified portion of
-     * the input array, or <code>Double.NaN</code> if the designated subarray
-     * is empty.
-     * <p>
-     * Throws <code>MathIllegalArgumentException</code> if the array is null or
-     * the array index parameters are not valid.</p>
-     * <ul>
-     * <li>The result is <code>NaN</code> iff all values are <code>NaN</code>
-     * (i.e. <code>NaN</code> values have no impact on the value of the statistic).</li>
-     * <li>If any of the values equals <code>Double.POSITIVE_INFINITY</code>,
-     * the result is <code>Double.POSITIVE_INFINITY.</code></li>
-     * </ul>
-     *
-     * @param values the input array
-     * @param begin index of the first array element to include
-     * @param length the number of elements to include
-     * @return the maximum of the values or Double.NaN if length = 0
-     * @throws MathIllegalArgumentException if the array is null or the array index
-     *  parameters are not valid
-     */
-    @Override
-    public double evaluate(final double[] values, final int begin, final int length)
-        throws MathIllegalArgumentException {
-
-        double max = Double.NaN;
-        if (MathArrays.verifyValues(values, begin, length)) {
-            max = values[begin];
-            for (int i = begin; i < begin + length; i++) {
-                if (!Double.isNaN(values[i])) {
-                    max = (max > values[i]) ? max : values[i];
-                }
-            }
-        }
-        return max;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Max copy() {
-        Max result = new Max();
-        // No try-catch or advertised exception because args are non-null
-        copy(this, result);
-        return result;
-    }
-
-    /**
-     * Copies source to dest.
-     * <p>Neither source nor dest can be null.</p>
-     *
-     * @param source Max to copy
-     * @param dest Max to copy to
-     * @throws NullArgumentException if either source or dest is null
-     */
-    public static void copy(Max source, Max dest)
-        throws NullArgumentException {
-        NullArgumentException.check(source);
-        NullArgumentException.check(dest);
-        dest.n = source.n;
-        dest.value = source.value;
-    }
+  /**
+   * Copies source to dest.
+   *
+   * <p>Neither source nor dest can be null.
+   *
+   * @param source Max to copy
+   * @param dest Max to copy to
+   * @throws NullArgumentException if either source or dest is null
+   */
+  public static void copy(Max source, Max dest) throws NullArgumentException {
+    NullArgumentException.check(source);
+    NullArgumentException.check(dest);
+    dest.n = source.n;
+    dest.value = source.value;
+  }
 }
