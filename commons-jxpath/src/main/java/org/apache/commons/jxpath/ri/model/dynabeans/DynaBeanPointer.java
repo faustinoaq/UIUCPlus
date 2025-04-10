f983bb5c454db0f66865bd1e9219582cfc160ebc
faustinoaq <faustinoaq@gmail.com>
2025-04-10 08:16:30 -0500

    Formatted main branch before mubert-base.commons-jxpath.mid-870.idx-871.400.mutant injection

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dynabeans/DynaBeanPointer.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/model/dynabeans/DynaBeanPointer.java
@@ -17,120 +17,121 @@
 package org.apache.commons.jxpath.ri.model.dynabeans;
 
 import java.util.Locale;
-
 import org.apache.commons.beanutils.DynaBean;
 import org.apache.commons.jxpath.ri.QName;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 import org.apache.commons.jxpath.ri.model.beans.PropertyOwnerPointer;
 import org.apache.commons.jxpath.ri.model.beans.PropertyPointer;
 
-
 /**
- * A Pointer that points to a {@link DynaBean}.  If the target DynaBean is Serializable,
- * so should this instance be.
+ * A Pointer that points to a {@link DynaBean}. If the target DynaBean is Serializable, so should
+ * this instance be.
  */
 public class DynaBeanPointer extends PropertyOwnerPointer {
-    private static final long serialVersionUID = -9135052498044877965L;
+  private static final long serialVersionUID = -9135052498044877965L;
 
-    private final QName name;
-    private final DynaBean dynaBean;
+  private final QName name;
+  private final DynaBean dynaBean;
 
-    /**
-     * Create a new DynaBeanPointer.
-     * @param name is the name given to the first node
-     * @param dynaBean pointed
-     * @param locale Locale
-     */
-    public DynaBeanPointer(final QName name, final DynaBean dynaBean, final Locale locale) {
-        super(null, locale);
-        this.name = name;
-        this.dynaBean = dynaBean;
+  /**
+   * Create a new DynaBeanPointer.
+   *
+   * @param name is the name given to the first node
+   * @param dynaBean pointed
+   * @param locale Locale
+   */
+  public DynaBeanPointer(final QName name, final DynaBean dynaBean, final Locale locale) {
+    super(null, locale);
+    this.name = name;
+    this.dynaBean = dynaBean;
+  }
+
+  /**
+   * Create a new DynaBeanPointer.
+   *
+   * @param parent pointer
+   * @param name is the name given to the first node
+   * @param dynaBean pointed
+   */
+  public DynaBeanPointer(final NodePointer parent, final QName name, final DynaBean dynaBean) {
+    super(parent);
+    this.name = name;
+    this.dynaBean = dynaBean;
+  }
+
+  @Override
+  public PropertyPointer getPropertyPointer() {
+    return new DynaBeanPropertyPointer(this, dynaBean);
+  }
+
+  @Override
+  public QName getName() {
+    return name;
+  }
+
+  @Override
+  public Object getBaseValue() {
+    return dynaBean;
+  }
+
+  @Override
+  public Object getImmediateNode() {
+    return dynaBean;
+  }
+
+  @Override
+  public boolean isCollection() {
+    return false;
+  }
+
+  @Override
+  public int getLength() {
+    return 1;
+  }
+
+  @Override
+  public boolean isLeaf() {
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return name == null ? 0 : name.hashCode();
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    if (object == this) {
+      return true;
     }
 
-    /**
-     * Create a new DynaBeanPointer.
-     * @param parent pointer
-     * @param name is the name given to the first node
-     * @param dynaBean pointed
-     */
-    public DynaBeanPointer(final NodePointer parent, final QName name, final DynaBean dynaBean) {
-        super(parent);
-        this.name = name;
-        this.dynaBean = dynaBean;
+    if (!(object instanceof DynaBeanPointer)) {
+      return false;
     }
 
-    @Override
-    public PropertyPointer getPropertyPointer() {
-        return new DynaBeanPropertyPointer(this, dynaBean);
+    final DynaBeanPointer other = (DynaBeanPointer) object;
+    if (!(equalObjects(parent, other.parent) && equalObjects(name, other.name))) {
+      return false;
     }
 
-    @Override
-    public QName getName() {
-        return name;
-    }
+    final int iThis = index == WHOLE_COLLECTION ? 0 : index;
+    final int iOther = other.index == WHOLE_COLLECTION ? 0 : other.index;
+    return iThis == iOther && dynaBean == other.dynaBean;
+  }
 
-    @Override
-    public Object getBaseValue() {
-        return dynaBean;
-    }
+  @Override
+  public String asPath() {
+    return parent == null ? "/" : super.asPath();
+  }
 
-    @Override
-    public Object getImmediateNode() {
-        return dynaBean;
-    }
-
-    @Override
-    public boolean isCollection() {
-        return false;
-    }
-
-    @Override
-    public int getLength() {
-        return 1;
-    }
-
-    @Override
-    public boolean isLeaf() {
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return name == null ? 0 : name.hashCode();
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (object == this) {
-            return true;
-        }
-
-        if (!(object instanceof DynaBeanPointer)) {
-            return false;
-        }
-
-        final DynaBeanPointer other = (DynaBeanPointer) object;
-        if (!(equalObjects(parent, other.parent) && equalObjects(name, other.name))) {
-            return false;
-        }
-
-        final int iThis = index == WHOLE_COLLECTION ? 0 : index;
-        final int iOther = other.index == WHOLE_COLLECTION ? 0 : other.index;
-        return iThis == iOther && dynaBean == other.dynaBean;
-    }
-
-    @Override
-    public String asPath() {
-        return parent == null ? "/" : super.asPath();
-    }
-
-    /**
-     * Learn whether two objects are == || .equals().
-     * @param o1 first object
-     * @param o2 second object
-     * @return boolean
-     */
-    private static boolean equalObjects(final Object o1, final Object o2) {
-        return o1 == o2 || o1 != null && o1.equals(o2);
-    }
+  /**
+   * Learn whether two objects are == || .equals().
+   *
+   * @param o1 first object
+   * @param o2 second object
+   * @return boolean
+   */
+  private static boolean equalObjects(final Object o1, final Object o2) {
+    return o1 == o2 || o1 != null && o1.equals(o2);
+  }
 }
